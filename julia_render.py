d194c7e71e4f36b421ab5da021f2b15216c389fe
Scott <scottimig@gmail.com>
2013-11-29 21:03:11 -0700

    initial commit

---
+++ julia_render.py
@@ -0,0 +1,42 @@
+import numpy as np
+from PIL import Image, ImageFilter, ImageEnhance, ImageOps
+from matplotlib.colors import hsv_to_rgb
+
+width, height = 900, 900
+x_range = (-0.95, 0.95)
+y_range = (-0.95, 0.95)
+c = complex(0.26, -0.04)
+max_iter = 350
+
+x = np.linspace(x_range[0], x_range[1], width)
+y = np.linspace(y_range[0], y_range[1], height)
+X, Y = np.meshgrid(x, y)
+Z = X + 1j * Y
+
+div_iter = np.zeros(Z.shape, dtype=int)
+mask = np.ones(Z.shape, dtype=bool)
+for i in range(max_iter):
+    Z[mask] = Z[mask] ** 2 + c
+    mask_new = np.abs(Z) <= 2
+    div_iter[mask & ~mask_new] = i
+    mask = mask_new
+
+with np.errstate(divide='ignore', invalid='ignore'):
+    smooth = div_iter + 1 - np.log(np.log2(np.abs(Z)))
+    smooth = np.nan_to_num(smooth)
+smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
+
+hsv = np.zeros((height, width, 3), dtype=float)
+hsv[..., 0] = (0.8 * smooth_norm + 0.1) % 1
+hsv[..., 1] = 0.98 - 0.2 * np.abs(np.sin(2 * np.pi * smooth_norm))
+hsv[..., 2] = smooth_norm ** 0.2
+
+rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
+img = Image.fromarray(rgb)
+
+img = ImageOps.posterize(img, 3)
+img = img.filter(ImageFilter.EDGE_ENHANCE_MORE)
+enhanced = ImageEnhance.Contrast(img).enhance(2.0)
+
+output_path = 'julia_output.jpg'
+enhanced.save(output_path) 
