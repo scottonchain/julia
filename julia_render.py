1e8b6bb73a0447cd9f28a229064c18e5b15a8975
Scott <scottimig@gmail.com>
2013-12-31 20:11:37 -0700

    save state

--- julia_render.py
+++ julia_render.py
@@ -1,54 +1,43 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy import ndimage
+from PIL import Image, ImageDraw, ImageEnhance
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 900, 900
-x_range = (-0.65, 0.65)
-y_range = (-0.57, 0.57)
-c = complex(-0.69, 0.3)
-max_iter = 500
+x_range = (-2.2, 1.2)
+y_range = (-2.0, 1.0)
+max_iter = 320
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
-Z = X + 1j * Y
-
-iteration = np.zeros(Z.shape, dtype=int)
-mask = np.ones(Z.shape, dtype=bool)
-
+C = X + 1j * Y
+Z = np.zeros_like(C)
+burning_ship = np.zeros(C.shape, dtype=int)
+mask = np.ones(C.shape, dtype=bool)
 for i in range(max_iter):
-    Z[mask] = Z[mask] ** 2 + c
+    Z[mask] = (np.abs(Z[mask].real) + 1j * np.abs(Z[mask].imag)) ** 2 + C[mask]
     mask_new = np.abs(Z) <= 2
-    iteration[mask & ~mask_new] = i
+    burning_ship[mask & ~mask_new] = i
     mask = mask_new
 
-# Apply scipy filters for enhancement
-iteration_smooth = ndimage.gaussian_filter(iteration.astype(float), sigma=0.5)
-iteration_enhanced = ndimage.uniform_filter(iteration_smooth, size=2)
-
-# Smooth coloring
-with np.errstate(divide='ignore', invalid='ignore'):
-    smooth = iteration_enhanced + 1 - np.log(np.log2(np.abs(Z)))
-    smooth = np.nan_to_num(smooth)
-smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
-
-# Bright palette
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.6 * smooth_norm + 0.3) % 1
-hsv[..., 1] = 0.95 - 0.1 * np.abs(np.sin(2 * np.pi * smooth_norm))
-hsv[..., 2] = smooth_norm ** 0.2
-
+hsv[..., 0] = (0.1 * burning_ship / max_iter + 0.8) % 1
+hsv[..., 1] = 0.7 + 0.3 * (burning_ship / max_iter)
+hsv[..., 2] = (burning_ship / max_iter) ** 0.9
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
+img = Image.fromarray(rgb)
 
-fig, ax = plt.subplots(figsize=(8, 8), dpi=112)
-im = ax.imshow(rgb, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), 
-               origin='lower')
-ax.set_title('Julia Set (SciPy Enhanced)', fontsize=14)
-ax.set_xlabel('Re(z)', fontsize=12)
-ax.set_ylabel('Im(z)', fontsize=12)
-ax.grid(True, color='white', alpha=0.3, linestyle='--', linewidth=0.5)
+# Overlay: grid
+step = 0.3
+for val in np.arange(x_range[0], x_range[1] + step, step):
+    x_pix = int((val - x_range[0]) / (x_range[1] - x_range[0]) * width)
+    ImageDraw.Draw(img).line([(x_pix, 0), (x_pix, height)], fill=(255, 200, 200), width=1)
+for val in np.arange(y_range[0], y_range[1] + step, step):
+    y_pix = int((y_range[1] - val) / (y_range[1] - y_range[0]) * height)
+    ImageDraw.Draw(img).line([(0, y_pix), (width, y_pix)], fill=(255, 200, 200), width=1)
 
-plt.tight_layout()
-plt.savefig('julia_output.jpg', dpi=112, bbox_inches='tight')
-plt.close() 
+img = ImageEnhance.Color(img).enhance(1.3)
+img = ImageEnhance.Contrast(img).enhance(1.2)
+
+output_path = 'julia_output.jpg'
+img.save(output_path) 
