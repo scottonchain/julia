d72a3d258d4e1cb50875b1d6316adce7aeaabee1
Scott <scottimig@gmail.com>
2013-12-31 20:35:40 -0700

    save state

--- julia_render.py
+++ julia_render.py
@@ -1,43 +1,40 @@
 import numpy as np
-from PIL import Image, ImageDraw, ImageEnhance
-from matplotlib.colors import hsv_to_rgb
+import matplotlib.pyplot as plt
 
 width, height = 900, 900
-x_range = (-2.2, 1.2)
-y_range = (-2.0, 1.0)
-max_iter = 320
+x_range = (-1.98, 1.98)
+y_range = (-1.98, 1.98)
+c = complex(-0.74, 0.31)
+max_iter = 300
 
-x = np.linspace(x_range[0], x_range[1], width)
-y = np.linspace(y_range[0], y_range[1], height)
+x = np.linspace(x_range[0], x_range[1], width, dtype=np.float32)
+y = np.linspace(y_range[0], y_range[1], height, dtype=np.float32)
 X, Y = np.meshgrid(x, y)
-C = X + 1j * Y
-Z = np.zeros_like(C)
-burning_ship = np.zeros(C.shape, dtype=int)
-mask = np.ones(C.shape, dtype=bool)
+Z = X + 1j * Y
+
+iteration = np.full(Z.shape, max_iter, dtype=np.uint16)
+mask = np.ones(Z.shape, dtype=bool)
+escape_radius = 4.0
+
 for i in range(max_iter):
-    Z[mask] = (np.abs(Z[mask].real) + 1j * np.abs(Z[mask].imag)) ** 2 + C[mask]
-    mask_new = np.abs(Z) <= 2
-    burning_ship[mask & ~mask_new] = i
+    if not np.any(mask):
+        break
+    Z[mask] = Z[mask] ** 2 + c
+    mask_new = np.abs(Z) <= escape_radius
+    iteration[mask & ~mask_new] = i
     mask = mask_new
 
-hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.1 * burning_ship / max_iter + 0.8) % 1
-hsv[..., 1] = 0.7 + 0.3 * (burning_ship / max_iter)
-hsv[..., 2] = (burning_ship / max_iter) ** 0.9
-rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
-img = Image.fromarray(rgb)
+with np.errstate(divide='ignore', invalid='ignore'):
+    smooth = iteration + 1 - np.log(np.log2(np.abs(Z)))
+    smooth = np.nan_to_num(smooth)
 
-# Overlay: grid
-step = 0.3
-for val in np.arange(x_range[0], x_range[1] + step, step):
-    x_pix = int((val - x_range[0]) / (x_range[1] - x_range[0]) * width)
-    ImageDraw.Draw(img).line([(x_pix, 0), (x_pix, height)], fill=(255, 200, 200), width=1)
-for val in np.arange(y_range[0], y_range[1] + step, step):
-    y_pix = int((y_range[1] - val) / (y_range[1] - y_range[0]) * height)
-    ImageDraw.Draw(img).line([(0, y_pix), (width, y_pix)], fill=(255, 200, 200), width=1)
+fig, ax = plt.subplots(figsize=(8, 8), dpi=112)
+im = ax.imshow(smooth, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), 
+               origin='lower', cmap='plasma')
+ax.set_title('Julia Set (Optimized)', fontsize=14)
+ax.set_xlabel('Re(z)', fontsize=12)
+ax.set_ylabel('Im(z)', fontsize=12)
 
-img = ImageEnhance.Color(img).enhance(1.3)
-img = ImageEnhance.Contrast(img).enhance(1.2)
-
-output_path = 'julia_output.jpg'
-img.save(output_path) 
+plt.tight_layout()
+plt.savefig('julia_output.jpg', dpi=112, bbox_inches='tight')
+plt.close() 
