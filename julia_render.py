0ffded05686d053060d8900585e96facfeee7286
Scott <scottimig@gmail.com>
2014-05-28 00:21:08 -0600

    saving

--- julia_render.py
+++ julia_render.py
@@ -3,10 +3,10 @@ from PIL import Image, ImageFilter, ImageEnhance, ImageOps
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 900, 900
-x_range = (-1.7, 1.7)
-y_range = (-1.7, 1.7)
-c = complex(0.42, -0.21)
-max_iter = 380
+x_range = (-1.79, 1.79)
+y_range = (-1.79, 1.79)
+c = complex(-0.74, -0.41)
+max_iter = 350
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
@@ -26,27 +26,34 @@ with np.errstate(divide='ignore', invalid='ignore'):
     smooth = np.nan_to_num(smooth)
 smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
-# Metallic palette
+# Pastel palette
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.1 * smooth_norm + 0.6) % 1
-hsv[..., 1] = 0.2 + 0.8 * np.abs(np.cos(3 * np.pi * smooth_norm))
-hsv[..., 2] = 0.7 + 0.3 * np.abs(np.sin(2 * np.pi * smooth_norm))
+hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1
+hsv[..., 1] = 0.4 + 0.3 * np.abs(np.sin(2 * np.pi * smooth_norm))
+hsv[..., 2] = smooth_norm ** 0.5
 
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
 
-img = ImageOps.posterize(img, 2)
-
-# Glass tile effect
-def glass_tile(im, tile=30):
+# Heavy pixelation
+def pixelate(im, block=20):
     arr = np.array(im)
-    for i in range(0, arr.shape[0], tile):
-        for j in range(0, arr.shape[1], tile):
-            arr[i:i+tile, j:j+tile] = np.flipud(np.fliplr(arr[i:i+tile, j:j+tile]))
+    for i in range(0, arr.shape[0], block):
+        for j in range(0, arr.shape[1], block):
+            arr[i:i+block, j:j+block] = arr[i, j]
     return Image.fromarray(arr)
 
-img = glass_tile(img, tile=40)
-img = ImageEnhance.Contrast(img).enhance(1.6)
+img = pixelate(img, block=30)
+
+# Vertical split mirror
+def vertical_split_mirror(im):
+    arr = np.array(im)
+    mid = arr.shape[1] // 2
+    arr[:, mid:] = arr[:, :mid][:, ::-1]
+    return Image.fromarray(arr)
+
+img = vertical_split_mirror(img)
+img = ImageEnhance.Color(img).enhance(1.5)
 
 output_path = 'julia_output.jpg'
 img.save(output_path) 
