0c0ded0d7198556e8182f17b1e2d99aec8346412
Scott <scottimig@gmail.com>
2014-06-14 22:08:30 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -1,55 +1,39 @@
 import numpy as np
-from PIL import Image, ImageFilter, ImageEnhance, ImageDraw
-from matplotlib.colors import hsv_to_rgb
+import matplotlib.pyplot as plt
+import random
 
 width, height = 1600, 1600
-x_range = (-1.53, 1.53)
-y_range = (-1.53, 1.53)
-c = complex(-0.83, 0.15)
-max_iter = 400
+x_range = (-2.08, 2.08)
+y_range = (-2.08, 2.08)
+c = complex(-0.68, 0.3)
+max_iter = 300
+
+# Randomize color palette
+random.seed(42)
+colormaps = ['plasma', 'viridis', 'inferno', 'magma', 'hot', 'cool', 'spring', 'summer', 'autumn', 'winter', 'rainbow', 'jet', 'hsv', 'twilight', 'brg']
+selected_cmap = random.choice(colormaps)
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
 Z = X + 1j * Y
 
-div_iter = np.zeros(Z.shape, dtype=int)
+iteration = np.zeros(Z.shape, dtype=int)
 mask = np.ones(Z.shape, dtype=bool)
+
 for i in range(max_iter):
     Z[mask] = Z[mask] ** 2 + c
     mask_new = np.abs(Z) <= 2
-    div_iter[mask & ~mask_new] = i
+    iteration[mask & ~mask_new] = i
     mask = mask_new
 
-with np.errstate(divide='ignore', invalid='ignore'):
-    smooth = div_iter + 1 - np.log(np.log2(np.abs(Z)))
-    smooth = np.nan_to_num(smooth)
-smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
+fig, ax = plt.subplots(figsize=(8, 8), dpi=112)
+im = ax.imshow(iteration, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), 
+               origin='lower', cmap=selected_cmap)
+ax.set_title(f'Julia Set (Random Palette: {selected_cmap})', fontsize=14)
+ax.set_xlabel('Re(z)', fontsize=12)
+ax.set_ylabel('Im(z)', fontsize=12)
 
-# Deep blue palette
-hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = 0.6 + 0.1 * smooth_norm
-hsv[..., 1] = 0.7 + 0.3 * smooth_norm
-hsv[..., 2] = smooth_norm ** 0.8
-
-rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
-img = Image.fromarray(rgb)
-
-img = img.filter(ImageFilter.GaussianBlur(radius=10))
-
-# Radial gradient overlay
-def radial_gradient(im):
-    arr = np.array(im).astype(np.float32)
-    cy, cx = arr.shape[0] // 2, arr.shape[1] // 2
-    Y, X = np.ogrid[:arr.shape[0], :arr.shape[1]]
-    r = np.sqrt((Y - cy) ** 2 + (X - cx) ** 2)
-    mask = r / r.max()
-    for c in range(3):
-        arr[..., c] = arr[..., c] * (1 - 0.5 * mask)
-    return Image.fromarray(arr.astype(np.uint8))
-
-img = radial_gradient(img)
-img = ImageEnhance.Brightness(img).enhance(1.1)
-
-output_path = 'julia_output.jpg'
-img.save(output_path) 
+plt.tight_layout()
+plt.savefig('julia_output.jpg', dpi=112, bbox_inches='tight')
+plt.close() 
