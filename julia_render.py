b500fb51ec0aadc45a14c1e4290a57a4d0198722
Scott <scottimig@gmail.com>
2014-07-06 17:16:29 -0600

    sync

--- julia_render.py
+++ julia_render.py
@@ -3,48 +3,49 @@ from PIL import Image, ImageFilter, ImageEnhance
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-1.73, 1.73)
-y_range = (-1.73, 1.73)
-c = complex(0.27, -0.53)
-max_iter = 350
+x_range = (-1.98, 1.98)
+y_range = (-1.98, 1.98)
+max_iter = 300
 
+# Burning Ship fractal
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
-Z = X + 1j * Y
-
-div_iter = np.zeros(Z.shape, dtype=int)
-mask = np.ones(Z.shape, dtype=bool)
+C = X + 1j * Y
+Z = np.zeros_like(C)
+ship = np.zeros(C.shape, dtype=int)
+mask = np.ones(C.shape, dtype=bool)
 for i in range(max_iter):
-    Z[mask] = Z[mask] ** 2 + c
+    Z[mask] = (np.abs(Z[mask].real) + 1j * np.abs(Z[mask].imag)) ** 2 + C[mask]
     mask_new = np.abs(Z) <= 2
-    div_iter[mask & ~mask_new] = i
+    ship[mask & ~mask_new] = i
     mask = mask_new
 
-with np.errstate(divide='ignore', invalid='ignore'):
-    smooth = div_iter + 1 - np.log(np.log2(np.abs(Z)))
-    smooth = np.nan_to_num(smooth)
-smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
-
-# Bright rainbow palette
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (smooth_norm + 0.3) % 1
-hsv[..., 1] = 0.95 - 0.4 * smooth_norm
-hsv[..., 2] = smooth_norm ** 0.4
-
+hsv[..., 0] = (0.2 * ship / max_iter + 0.8) % 1
+hsv[..., 1] = 0.9 - 0.7 * (ship / max_iter)
+hsv[..., 2] = (ship / max_iter) ** 0.7
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
 
-img = img.filter(ImageFilter.EDGE_ENHANCE_MORE)
+# Julia set as transparency mask
+c = complex(-0.78, 0.18)
+Z2 = X + 1j * Y
+julia = np.zeros(Z2.shape, dtype=int)
+mask = np.ones(Z2.shape, dtype=bool)
+for i in range(max_iter):
+    Z2[mask] = Z2[mask] ** 2 + c
+    mask_new = np.abs(Z2) <= 2
+    julia[mask & ~mask_new] = i
+    mask = mask_new
+alpha = (julia / max_iter * 255).astype(np.uint8)
+img = img.convert('RGBA')
+img.putalpha(Image.fromarray(alpha))
 
-def vertical_wave(im, amp=12, freq=0.09):
-    arr = np.array(im)
-    for j in range(arr.shape[1]):
-        arr[:, j] = np.roll(arr[:, j], int(amp * np.sin(freq * j)))
-    return Image.fromarray(arr)
-
-img = vertical_wave(img, amp=18, freq=0.13)
-img = ImageEnhance.Color(img).enhance(1.7)
+# Motion blur effect
+img = img.filter(ImageFilter.GaussianBlur(radius=2)).filter(ImageFilter.BoxBlur(3))
+img = img.convert('RGB')
+img = ImageEnhance.Color(img).enhance(2.1)
 
 output_path = 'julia_output.jpg'
 img.save(output_path) 
