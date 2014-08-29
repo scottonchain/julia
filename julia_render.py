05481599c9ad1000baffb9995b53292364aa99de
Scott <scottimig@gmail.com>
2014-08-29 11:17:33 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -1,12 +1,12 @@
 import numpy as np
-from PIL import Image, ImageFilter, ImageEnhance
+from PIL import Image, ImageFilter, ImageEnhance, ImageOps
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-0.66, 0.66)
-y_range = (-0.66, 0.66)
-c = complex(0.39, -0.37)
-max_iter = 360
+x_range = (-0.51, 0.51)
+y_range = (-0.43, 0.43)
+c = complex(0.29, -0.51)
+max_iter = 350
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
@@ -28,15 +28,22 @@ smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
 hsv = np.zeros((height, width, 3), dtype=float)
 hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1
-hsv[..., 1] = 0.95 - 0.1 * np.abs(np.cos(2 * np.pi * smooth_norm))
+hsv[..., 1] = 0.95 - 0.1 * np.abs(np.sin(2 * np.pi * smooth_norm))
 hsv[..., 2] = smooth_norm ** 0.2
 
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
 
-img = img.filter(ImageFilter.EMBOSS)
-enhanced = ImageEnhance.Contrast(img).enhance(1.7)
-enhanced = ImageEnhance.Color(enhanced).enhance(1.3)
+img = ImageOps.posterize(img, 2)
+
+def diagonal_wave(im, amp=10, freq=0.12):
+    arr = np.array(im)
+    for i in range(arr.shape[0]):
+        arr[i] = np.roll(arr[i], int(amp * np.sin(freq * i + freq * i)))
+    return Image.fromarray(arr)
+
+img = diagonal_wave(img, amp=20, freq=0.18)
+img = img.filter(ImageFilter.DETAIL)
 
 output_path = 'julia_output.jpg'
-enhanced.save(output_path) 
+img.save(output_path) 
