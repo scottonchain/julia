23ab2ddaa322a80f01c1e86e5432eaf78f20cb11
Scott <scottimig@gmail.com>
2014-11-10 03:59:14 -0700

    save state

--- julia_render.py
+++ julia_render.py
@@ -1,45 +1,42 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
+from PIL import Image, ImageFilter, ImageEnhance
+from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-# Zoomed in region for detail
-x_range = (-1.54, 1.54)
-y_range = (-1.44, 1.44)
-c = complex(0.38, -0.11)
-max_iter = 1000
+x_range = (-1.39, 1.39)
+y_range = (-1.39, 1.39)
+c = complex(-0.79, 0.16)
+max_iter = 400
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
 Z = X + 1j * Y
 
-iteration = np.zeros(Z.shape, dtype=int)
+div_iter = np.zeros(Z.shape, dtype=int)
 mask = np.ones(Z.shape, dtype=bool)
 for i in range(max_iter):
     Z[mask] = Z[mask] ** 2 + c
     mask_new = np.abs(Z) <= 2
-    iteration[mask & ~mask_new] = i
+    div_iter[mask & ~mask_new] = i
     mask = mask_new
 
-# Smooth coloring for better detail
 with np.errstate(divide='ignore', invalid='ignore'):
-    smooth = iteration + 1 - np.log(np.log2(np.abs(Z)))
+    smooth = div_iter + 1 - np.log(np.log2(np.abs(Z)))
     smooth = np.nan_to_num(smooth)
+smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
-# Use bright, warm prismatic colormap
-fig, ax = plt.subplots(figsize=(8, 8), dpi=112)
-im = ax.imshow(smooth, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), 
-               origin='lower', cmap='spring', interpolation='bilinear')
-ax.set_title('Julia Set Detail (c = 0.37 - 0.1i)', fontsize=14)
-ax.set_xlabel('Re(z)', fontsize=12)
-ax.set_ylabel('Im(z)', fontsize=12)
-ax.grid(True, color='white', alpha=0.3, linestyle='--', linewidth=0.5)
+hsv = np.zeros((height, width, 3), dtype=float)
+hsv[..., 0] = (0.3 * smooth_norm + 0.7) % 1
+hsv[..., 1] = 0.95 - 0.3 * smooth_norm
+hsv[..., 2] = smooth_norm ** 0.7
 
-# Add colorbar
-cbar = plt.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
-cbar.set_label('Iteration Count (Smooth)', fontsize=12)
+rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
+img = Image.fromarray(rgb)
 
-plt.tight_layout()
-plt.savefig('julia_output.jpg', dpi=112, bbox_inches='tight')
-plt.close() 
+img = img.filter(ImageFilter.FIND_EDGES)
+enhanced = ImageEnhance.Contrast(img).enhance(2.5)
+enhanced = ImageEnhance.Color(enhanced).enhance(1.8)
+
+output_path = 'julia_output.jpg'
+enhanced.save(output_path) 
