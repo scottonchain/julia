04df3443f4247eb8930141537fe9ab3150f79a19
Scott <scottimig@gmail.com>
2015-03-18 03:11:21 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -1,11 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-1.14, 1.14)
-y_range = (-0.82, 0.82)
-c = complex(-0.75, 0.31)
+x_range = (-1.16, 1.16)
+y_range = (-0.8, 0.8)
+c = complex(-0.74, 0.32)
 max_iter = 300
 
 x = np.linspace(x_range[0], x_range[1], width)
@@ -22,20 +21,10 @@ for i in range(max_iter):
     iteration[mask & ~mask_new] = i
     mask = mask_new
 
-# Smooth coloring
-with np.errstate(divide='ignore', invalid='ignore'):
-    smooth = iteration + 1 - np.log(np.log2(np.abs(Z)))
-    smooth = np.nan_to_num(smooth)
-smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
-
-# Bright cool palette
-from matplotlib import cm
-rgb = cm.cool(smooth_norm)[..., :3]
-
 fig, ax = plt.subplots(figsize=(8, 8), dpi=112)
-im = ax.imshow(rgb, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), 
-               origin='lower')
-ax.set_title('Julia Set (Bright Cool)', fontsize=14)
+im = ax.imshow(iteration, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), 
+               origin='lower', cmap='hot')
+ax.set_title('Julia Set (Bright Hot)', fontsize=14)
 ax.set_xlabel('Re(z)', fontsize=12)
 ax.set_ylabel('Im(z)', fontsize=12)
 
