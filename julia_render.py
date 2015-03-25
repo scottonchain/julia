516f653e7274bd543550ed7159c97508a19b3ad9
Scott <scottimig@gmail.com>
2015-03-25 11:43:49 -0600

    sync

--- julia_render.py
+++ julia_render.py
@@ -3,10 +3,10 @@ from PIL import Image, ImageFilter, ImageEnhance, ImageOps
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-0.64, 0.64)
-y_range = (-0.7, 0.7)
-c = complex(-0.43, 0.58)
-max_iter = 360
+x_range = (-1.56, 1.56)
+y_range = (-1.56, 1.56)
+c = complex(0.26, -0.0)
+max_iter = 400
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
@@ -26,25 +26,40 @@ with np.errstate(divide='ignore', invalid='ignore'):
     smooth = np.nan_to_num(smooth)
 smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
+# Sepia palette
+def sepia(im):
+    arr = np.array(im).astype(np.float32)
+    r, g, b = arr[..., 0], arr[..., 1], arr[..., 2]
+    tr = 0.393 * r + 0.769 * g + 0.189 * b
+    tg = 0.349 * r + 0.686 * g + 0.168 * b
+    tb = 0.272 * r + 0.534 * g + 0.131 * b
+    arr[..., 0] = np.clip(tr, 0, 255)
+    arr[..., 1] = np.clip(tg, 0, 255)
+    arr[..., 2] = np.clip(tb, 0, 255)
+    return Image.fromarray(arr.astype(np.uint8))
+
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1
-hsv[..., 1] = 0.95 - 0.1 * smooth_norm
-hsv[..., 2] = smooth_norm ** 0.2
+hsv[..., 0] = (0.1 * smooth_norm + 0.1) % 1
+hsv[..., 1] = 0.4 + 0.2 * np.abs(np.sin(2 * np.pi * smooth_norm))
+hsv[..., 2] = smooth_norm ** 0.7
 
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
-
+img = sepia(img)
 img = img.filter(ImageFilter.GaussianBlur(radius=8))
 
-# Vertical split mirror
-def vertical_split_mirror(im):
+# Spiral mask overlay
+def spiral_mask(im):
     arr = np.array(im)
-    mid = arr.shape[1] // 2
-    arr[:, mid:] = arr[:, :mid][:, ::-1]
+    cy, cx = arr.shape[0] // 2, arr.shape[1] // 2
+    Y, X = np.ogrid[:arr.shape[0], :arr.shape[1]]
+    theta = np.arctan2(Y - cy, X - cx)
+    mask = ((theta + np.sqrt((Y-cy)**2 + (X-cx)**2)/40) % (2*np.pi) < np.pi)
+    arr[mask] = arr[mask] // 2
     return Image.fromarray(arr)
 
-img = vertical_split_mirror(img)
-img = ImageEnhance.Contrast(img).enhance(1.5)
+img = spiral_mask(img)
+img = ImageEnhance.Contrast(img).enhance(1.3)
 
 output_path = 'julia_output.jpg'
 img.save(output_path) 
