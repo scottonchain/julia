8eb7d30fb6aea540cfe51c4828c56c7d49fb482c
Scott <scottimig@gmail.com>
2015-06-30 20:25:23 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -1,36 +1,45 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import random
 
 width, height = 1600, 1600
-x_range = (-1.96, 1.96)
-y_range = (-2.02, 2.02)
-c = complex(-0.74, 0.23)
+x_range = (-1.97, 1.97)
+y_range = (-1.97, 1.97)
+c = complex(-0.68, 0.24)
 max_iter = 300
 
-# Randomize color palette
-random.seed(42)
-colormaps = ['plasma', 'viridis', 'inferno', 'magma', 'hot', 'cool', 'spring', 'summer', 'autumn', 'winter', 'rainbow', 'jet', 'hsv', 'twilight', 'brg']
-selected_cmap = random.choice(colormaps)
-
-x = np.linspace(x_range[0], x_range[1], width)
-y = np.linspace(y_range[0], y_range[1], height)
+x = np.linspace(x_range[0], x_range[1], width, dtype=np.float32)
+y = np.linspace(y_range[0], y_range[1], height, dtype=np.float32)
 X, Y = np.meshgrid(x, y)
 Z = X + 1j * Y
 
-iteration = np.zeros(Z.shape, dtype=int)
+iteration = np.full(Z.shape, max_iter, dtype=np.uint16)
 mask = np.ones(Z.shape, dtype=bool)
+escape_radius = 4.0
+escape_radius_sq = escape_radius * escape_radius
+
+Z_sq = np.zeros_like(Z, dtype=np.complex64)
+Z_abs_sq = np.zeros(Z.shape, dtype=np.float32)
 
 for i in range(max_iter):
-    Z[mask] = Z[mask] ** 2 + c
-    mask_new = np.abs(Z) <= 2
+    if not np.any(mask):
+        break
+    
+    Z_sq[mask] = Z[mask] * Z[mask]
+    Z[mask] = Z_sq[mask] + c
+    
+    Z_abs_sq[mask] = Z[mask].real * Z[mask].real + Z[mask].imag * Z[mask].imag
+    mask_new = Z_abs_sq <= escape_radius_sq
     iteration[mask & ~mask_new] = i
     mask = mask_new
 
+with np.errstate(divide='ignore', invalid='ignore'):
+    smooth = iteration + 1 - np.log(np.log2(np.sqrt(Z_abs_sq)))
+    smooth = np.nan_to_num(smooth)
+
 fig, ax = plt.subplots(figsize=(8, 8), dpi=112)
-im = ax.imshow(iteration, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), 
-               origin='lower', cmap=selected_cmap)
-ax.set_title(f'Julia Set (Random Palette: {selected_cmap})', fontsize=14)
+im = ax.imshow(smooth, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), 
+               origin='lower', cmap='plasma')
+ax.set_title('Julia Set (Highly Optimized)', fontsize=14)
 ax.set_xlabel('Re(z)', fontsize=12)
 ax.set_ylabel('Im(z)', fontsize=12)
 
