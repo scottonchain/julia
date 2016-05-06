f3830c766c52b09022b4edc6ec9e23597a3beabd
Scott <scottimig@gmail.com>
2016-05-06 13:27:50 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -1,13 +1,12 @@
 import numpy as np
 from PIL import Image, ImageFilter, ImageEnhance
-import matplotlib.pyplot as plt
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-1.66, 1.66)
-y_range = (-1.54, 1.54)
-c = complex(-0.81, 0.18)
-max_iter = 370
+x_range = (-0.72, 0.72)
+y_range = (-0.44, 0.44)
+c = complex(0.4, -0.42)
+max_iter = 350
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
@@ -16,7 +15,6 @@ Z = X + 1j * Y
 
 div_iter = np.zeros(Z.shape, dtype=int)
 mask = np.ones(Z.shape, dtype=bool)
-
 for i in range(max_iter):
     Z[mask] = Z[mask] ** 2 + c
     mask_new = np.abs(Z) <= 2
@@ -29,28 +27,23 @@ with np.errstate(divide='ignore', invalid='ignore'):
 smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.5 * smooth_norm + 0.3) % 1
-hsv[..., 1] = 0.95 - 0.3 * np.abs(np.sin(2 * np.pi * smooth_norm))
-hsv[..., 2] = smooth_norm ** 0.4
+hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1
+hsv[..., 1] = 0.95 - 0.1 * smooth_norm
+hsv[..., 2] = smooth_norm ** 0.2
 
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
 
-def glass_distort(im, scale=6):
-    arr = np.array(im)
-    dx = (np.random.rand(*arr.shape[:2]) - 0.5) * scale
-    dy = (np.random.rand(*arr.shape[:2]) - 0.5) * scale
-    Y, X = np.meshgrid(np.arange(arr.shape[0]), np.arange(arr.shape[1]), indexing='ij')
-    Xn = np.clip((X + dx).astype(int), 0, arr.shape[1] - 1)
-    Yn = np.clip((Y + dy).astype(int), 0, arr.shape[0] - 1)
-    distorted = arr[Yn, Xn]
-    return Image.fromarray(distorted)
+# Duotone palette
+def duotone(im, color1=(30, 30, 120), color2=(220, 220, 60)):
+    arr = np.array(im).astype(np.float32) / 255.0
+    mask = arr[..., 0] > 0.5
+    arr[mask] = np.array(color1) / 255.0
+    arr[~mask] = np.array(color2) / 255.0
+    return Image.fromarray((arr * 255).astype(np.uint8))
 
-img = glass_distort(img, scale=8)
-
-blur = img.filter(ImageFilter.GaussianBlur(radius=1))
-enhanced = ImageEnhance.Color(blur).enhance(2.2)
-enhanced = ImageEnhance.Contrast(enhanced).enhance(1.5)
+img = duotone(img)
+img = img.filter(ImageFilter.EDGE_ENHANCE_MORE)
 
 output_path = 'julia_output.jpg'
-enhanced.save(output_path) 
+img.save(output_path) 
