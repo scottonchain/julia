e5e6b64bd445d2f289c163aa7595ed0a918cf8a4
Scott <scottimig@gmail.com>
2016-05-16 17:01:35 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -1,45 +1,50 @@
 import numpy as np
-from PIL import Image, ImageDraw, ImageEnhance
+from PIL import Image, ImageFilter, ImageEnhance
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-2.2, 1.2)
-y_range = (-1.7, 1.7)
-max_iter = 320
-p = -0.3 + 0.6j
+x_range = (-1.71, 1.71)
+y_range = (-1.8, 1.8)
+c = complex(0.31, -0.56)
+max_iter = 350
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
-C = X + 1j * Y
-Z = np.zeros_like(C)
-Zold = np.zeros_like(C)
-phoenix = np.zeros(C.shape, dtype=int)
-mask = np.ones(C.shape, dtype=bool)
+Z = X + 1j * Y
+
+div_iter = np.zeros(Z.shape, dtype=int)
+mask = np.ones(Z.shape, dtype=bool)
 for i in range(max_iter):
-    Z[mask], Zold[mask] = Z[mask] ** 2 + C[mask] + p * Zold[mask], Z[mask]
+    Z[mask] = Z[mask] ** 2 + c
     mask_new = np.abs(Z) <= 2
-    phoenix[mask & ~mask_new] = i
+    div_iter[mask & ~mask_new] = i
     mask = mask_new
 
+with np.errstate(divide='ignore', invalid='ignore'):
+    smooth = div_iter + 1 - np.log(np.log2(np.abs(Z)))
+    smooth = np.nan_to_num(smooth)
+smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
+
+# Bright rainbow palette
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.15 * phoenix / max_iter + 0.8) % 1
-hsv[..., 1] = 0.8 + 0.2 * (phoenix / max_iter)
-hsv[..., 2] = (phoenix / max_iter) ** 0.6
+hsv[..., 0] = (smooth_norm + 0.3) % 1
+hsv[..., 1] = 0.95 - 0.4 * smooth_norm
+hsv[..., 2] = smooth_norm ** 0.4
+
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
 
-# Overlay: grid and roots of unity
-step = 0.4
-for val in np.arange(x_range[0], x_range[1] + step, step):
-    x_pix = int((val - x_range[0]) / (x_range[1] - x_range[0]) * width)
-    ImageDraw.Draw(img).line([(x_pix, 0), (x_pix, height)], fill=(180, 220, 255), width=1)
-for val in np.arange(y_range[0], y_range[1] + step, step):
-    y_pix = int((y_range[1] - val) / (y_range[1] - y_range[0]) * height)
-    ImageDraw.Draw(img).line([(0, y_pix), (width, y_pix)], fill=(180, 220, 255), width=1)
+img = img.filter(ImageFilter.EDGE_ENHANCE_MORE)
 
-img = ImageEnhance.Color(img).enhance(1.4)
-img = ImageEnhance.Contrast(img).enhance(1.1)
+def vertical_wave(im, amp=12, freq=0.09):
+    arr = np.array(im)
+    for j in range(arr.shape[1]):
+        arr[:, j] = np.roll(arr[:, j], int(amp * np.sin(freq * j)))
+    return Image.fromarray(arr)
+
+img = vertical_wave(img, amp=18, freq=0.13)
+img = ImageEnhance.Color(img).enhance(1.7)
 
 output_path = 'julia_output.jpg'
 img.save(output_path) 
