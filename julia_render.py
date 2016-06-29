6eb47b0de03fd873329dfa001e6d07d6d9d52ad7
Scott <scottimig@gmail.com>
2016-06-29 10:25:32 -0600

    sync

--- julia_render.py
+++ julia_render.py
@@ -1,51 +1,50 @@
 import numpy as np
-import matplotlib.pyplot as plt
+from PIL import Image, ImageFilter, ImageEnhance, ImageDraw, ImageOps
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-1.35, 1.35)
-y_range = (-1.35, 1.35)
-c = complex(0.32, 0.32)
-max_iter = 360
+x_range = (-1.49, 1.49)
+y_range = (-1.4, 1.4)
+c = complex(-0.71, -0.42)
+max_iter = 340
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
 Z = X + 1j * Y
 
-iteration = np.zeros(Z.shape, dtype=int)
+div_iter = np.zeros(Z.shape, dtype=int)
 mask = np.ones(Z.shape, dtype=bool)
 for i in range(max_iter):
     Z[mask] = Z[mask] ** 2 + c
     mask_new = np.abs(Z) <= 2
-    iteration[mask & ~mask_new] = i
+    div_iter[mask & ~mask_new] = i
     mask = mask_new
 
 with np.errstate(divide='ignore', invalid='ignore'):
-    smooth = iteration + 1 - np.log(np.log2(np.abs(Z)))
+    smooth = div_iter + 1 - np.log(np.log2(np.abs(Z)))
     smooth = np.nan_to_num(smooth)
 smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.2 * smooth_norm + 0.5) % 1
-hsv[..., 1] = 0.95
-hsv[..., 2] = smooth_norm ** 0.6
-rgb = hsv_to_rgb(hsv)
+hsv[..., 0] = (0.08 + 0.12 * smooth_norm) % 1
+hsv[..., 1] = 0.8 - 0.5 * smooth_norm
+hsv[..., 2] = smooth_norm ** 0.7
 
-fig, ax = plt.subplots(figsize=(8, 8), dpi=112)
-im = ax.imshow(rgb, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), origin='lower')
-ax.set_title('Julia Set (c = 0.355 + 0.355i)', fontsize=14)
-ax.set_xlabel('Re(z)', fontsize=12)
-ax.set_ylabel('Im(z)', fontsize=12)
-ax.grid(True, color='white', alpha=0.2, linestyle='--', linewidth=0.5)
+rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
+img = Image.fromarray(rgb)
 
-from matplotlib.cm import ScalarMappable
-from matplotlib.colors import Normalize
-sm = ScalarMappable(cmap='hsv', norm=Normalize(vmin=smooth_norm.min(), vmax=smooth_norm.max()))
-sm.set_array([])
-cbar = plt.colorbar(sm, ax=ax, fraction=0.046, pad=0.04)
-cbar.set_label('Normalized Iteration (Smooth)', fontsize=12)
+img = ImageOps.solarize(img, threshold=120)
 
-plt.tight_layout()
-plt.savefig('julia_output.jpg', dpi=112, bbox_inches='tight')
-plt.close() 
+# Horizontal banding overlay
+def add_bands(im, band_height=30):
+    arr = np.array(im)
+    for y in range(0, arr.shape[0], band_height*2):
+        arr[y:y+band_height] = arr[y:y+band_height] // 2
+    return Image.fromarray(arr)
+
+img = add_bands(img, band_height=40)
+img = ImageEnhance.Color(img).enhance(1.3)
+
+output_path = 'julia_output.jpg'
+img.save(output_path) 
