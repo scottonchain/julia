033b61557c91b688db590d3a146919ea43b927fd
Scott <scottimig@gmail.com>
2016-07-01 22:59:10 -0600

    save state

--- julia_render.py
+++ julia_render.py
@@ -1,11 +1,12 @@
 import numpy as np
-import matplotlib.pyplot as plt
+from PIL import Image, ImageEnhance, ImageFilter
+from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-1.13, 1.13)
-y_range = (-0.79, 0.79)
-c = complex(-0.66, 0.22)
-max_iter = 300
+x_range = (-0.72, 0.72)
+y_range = (-0.59, 0.59)
+c = complex(-0.72, 0.26)
+max_iter = 500
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
@@ -21,13 +22,37 @@ for i in range(max_iter):
     iteration[mask & ~mask_new] = i
     mask = mask_new
 
-fig, ax = plt.subplots(figsize=(8, 8), dpi=112)
-im = ax.imshow(iteration, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), 
-               origin='lower', cmap='jet')
-ax.set_title('Julia Set (Bright Jet)', fontsize=14)
-ax.set_xlabel('Re(z)', fontsize=12)
-ax.set_ylabel('Im(z)', fontsize=12)
+# Smooth coloring
+with np.errstate(divide='ignore', invalid='ignore'):
+    smooth = iteration + 1 - np.log(np.log2(np.abs(Z)))
+    smooth = np.nan_to_num(smooth)
+smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
-plt.tight_layout()
-plt.savefig('julia_output.jpg', dpi=112, bbox_inches='tight')
-plt.close() 
+# Bright palette
+hsv = np.zeros((height, width, 3), dtype=float)
+hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1
+hsv[..., 1] = 0.95 - 0.1 * np.abs(np.sin(2 * np.pi * smooth_norm))
+hsv[..., 2] = smooth_norm ** 0.2
+
+rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
+
+# Create PIL image and apply enhancements
+img = Image.fromarray(rgb)
+
+# Apply PIL filters and enhancements
+img = img.filter(ImageFilter.EDGE_ENHANCE_MORE)
+img = img.filter(ImageFilter.SMOOTH_MORE)
+
+# Enhance colors and contrast
+enhancer = ImageEnhance.Color(img)
+img = enhancer.enhance(1.5)
+
+enhancer = ImageEnhance.Contrast(img)
+img = enhancer.enhance(1.3)
+
+enhancer = ImageEnhance.Brightness(img)
+img = enhancer.enhance(1.1)
+
+# Save output
+output_path = 'julia_output.jpg'
+img.save(output_path, quality=95) 
