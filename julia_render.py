fb39b1128b0a82a0a6e8baec490d5093a520d856
Scott <scottimig@gmail.com>
2016-08-08 06:06:07 -0600

    saving

--- julia_render.py
+++ julia_render.py
@@ -1,12 +1,12 @@
 import numpy as np
-from PIL import Image, ImageFilter, ImageEnhance
+from PIL import Image, ImageFilter, ImageEnhance, ImageDraw
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-0.72, 0.72)
-y_range = (-0.52, 0.52)
-c = complex(0.36, -0.42)
-max_iter = 350
+x_range = (-1.39, 1.39)
+y_range = (-1.39, 1.39)
+c = complex(-0.75, -0.35)
+max_iter = 340
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
@@ -27,23 +27,30 @@ with np.errstate(divide='ignore', invalid='ignore'):
 smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1
-hsv[..., 1] = 0.95 - 0.1 * smooth_norm
-hsv[..., 2] = smooth_norm ** 0.2
+hsv[..., 0] = (0.85 * smooth_norm + 0.15) % 1
+hsv[..., 1] = 1.0 - 0.5 * smooth_norm
+hsv[..., 2] = smooth_norm ** 0.7
 
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
 
-# Duotone palette
-def duotone(im, color1=(30, 30, 120), color2=(220, 220, 60)):
-    arr = np.array(im).astype(np.float32) / 255.0
-    mask = arr[..., 0] > 0.5
-    arr[mask] = np.array(color1) / 255.0
-    arr[~mask] = np.array(color2) / 255.0
-    return Image.fromarray((arr * 255).astype(np.uint8))
+# Pixel sorting effect
+def pixel_sort(im):
+    arr = np.array(im)
+    for col in arr.transpose(1,0,2):
+        col.sort(axis=0)
+    return Image.fromarray(arr)
 
-img = duotone(img)
-img = img.filter(ImageFilter.EDGE_ENHANCE_MORE)
+img = pixel_sort(img)
+
+def add_stripes(im, stripe_width=20):
+    draw = ImageDraw.Draw(im)
+    for x in range(0, im.width, stripe_width*2):
+        draw.rectangle([x, 0, x+stripe_width, im.height], fill=(255,255,255,40))
+    return im
+
+img = add_stripes(img, stripe_width=25)
+img = ImageEnhance.Brightness(img).enhance(1.3)
 
 output_path = 'julia_output.jpg'
 img.save(output_path) 
