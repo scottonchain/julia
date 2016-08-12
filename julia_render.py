d179347c06d2f8d8887ff179bf00403f2dd5140a
Scott <scottimig@gmail.com>
2016-08-12 11:06:28 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -1,12 +1,12 @@
 import numpy as np
-from PIL import Image, ImageFilter, ImageEnhance, ImageOps
+from PIL import Image, ImageFilter, ImageEnhance, ImageDraw
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-1.96, 1.96)
-y_range = (-1.96, 1.96)
-c = complex(0.24, 0.01)
-max_iter = 350
+x_range = (-1.56, 1.56)
+y_range = (-1.56, 1.56)
+c = complex(0.36, 0.36)
+max_iter = 360
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
@@ -28,16 +28,26 @@ smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
 hsv = np.zeros((height, width, 3), dtype=float)
 hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1
-hsv[..., 1] = 0.95 - 0.1 * np.abs(np.sin(2 * np.pi * smooth_norm))
-hsv[..., 2] = smooth_norm ** 0.2
+hsv[..., 1] = 0.4 + 0.3 * np.abs(np.sin(2 * np.pi * smooth_norm))
+hsv[..., 2] = smooth_norm ** 0.5
 
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
 
-img = ImageOps.mirror(img)
-img = ImageOps.flip(img)
-enhanced = ImageEnhance.Color(img).enhance(1.2)
-enhanced = ImageEnhance.Brightness(enhanced).enhance(1.1)
+img = img.filter(ImageFilter.GaussianBlur(radius=8))
+
+# Diamond mask overlay
+def diamond_mask(im):
+    arr = np.array(im)
+    cy, cx = arr.shape[0] // 2, arr.shape[1] // 2
+    for y in range(arr.shape[0]):
+        for x in range(arr.shape[1]):
+            if abs(x - cx) + abs(y - cy) > min(cx, cy):
+                arr[y, x] = arr[y, x] // 2
+    return Image.fromarray(arr)
+
+img = diamond_mask(img)
+img = ImageEnhance.Color(img).enhance(1.5)
 
 output_path = 'julia_output.jpg'
-enhanced.save(output_path) 
+img.save(output_path) 
