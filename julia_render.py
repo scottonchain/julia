fd23486963e7387e4c34d70d92a0703a52213271
Scott <scottimig@gmail.com>
2016-09-20 13:52:23 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -1,49 +1,45 @@
 import numpy as np
-from PIL import Image, ImageFilter, ImageEnhance
-from matplotlib.colors import hsv_to_rgb
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
 width, height = 1600, 1600
-x_range = (-1.2, 1.2)
-y_range = (-1.2, 1.2)
-c = complex(-0.55, 0.56)
-max_iter = 300
+# Zoomed in region for detail
+x_range = (-2.0, 2.0)
+y_range = (-2.08, 2.08)
+c = complex(0.33, 0.01)
+max_iter = 600
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
 Z = X + 1j * Y
 
-div_iter = np.zeros(Z.shape, dtype=int)
+iteration = np.zeros(Z.shape, dtype=int)
 mask = np.ones(Z.shape, dtype=bool)
 for i in range(max_iter):
     Z[mask] = Z[mask] ** 2 + c
     mask_new = np.abs(Z) <= 2
-    div_iter[mask & ~mask_new] = i
+    iteration[mask & ~mask_new] = i
     mask = mask_new
 
+# Smooth coloring for better detail
 with np.errstate(divide='ignore', invalid='ignore'):
-    smooth = div_iter + 1 - np.log(np.log2(np.abs(Z)))
+    smooth = iteration + 1 - np.log(np.log2(np.abs(Z)))
     smooth = np.nan_to_num(smooth)
-smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
-hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.7 * smooth_norm + 0.3) % 1
-hsv[..., 1] = 1.0 - 0.6 * smooth_norm
-hsv[..., 2] = smooth_norm ** 0.8
+# Use bright, warm prismatic colormap
+fig, ax = plt.subplots(figsize=(8, 8), dpi=112)
+im = ax.imshow(smooth, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), 
+               origin='lower', cmap='viridis', interpolation='bilinear')
+ax.set_title('Julia Set Detail (c = 0.285 + 0.01i)', fontsize=14)
+ax.set_xlabel('Re(z)', fontsize=12)
+ax.set_ylabel('Im(z)', fontsize=12)
+ax.grid(True, color='white', alpha=0.3, linestyle='--', linewidth=0.5)
 
-rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
-img = Image.fromarray(rgb)
+# Add colorbar
+cbar = plt.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
+cbar.set_label('Iteration Count (Smooth)', fontsize=12)
 
-# Pixel sorting effect
-def pixel_sort(im):
-    arr = np.array(im)
-    for row in arr:
-        row.sort(axis=0)
-    return Image.fromarray(arr)
-
-img = pixel_sort(img)
-enhanced = ImageEnhance.Color(img).enhance(2.5)
-enhanced = ImageEnhance.Brightness(enhanced).enhance(1.3)
-
-output_path = 'julia_output.jpg'
-enhanced.save(output_path) 
+plt.tight_layout()
+plt.savefig('julia_output.jpg', dpi=112, bbox_inches='tight')
+plt.close() 
