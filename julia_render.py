e4f9f6a0a959966fcfbb8c78eadf0f1c397b3e1d
Scott <scottimig@gmail.com>
2016-12-24 09:41:36 -0700

    update

--- julia_render.py
+++ julia_render.py
@@ -2,36 +2,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 width, height = 1600, 1600
-x_range = (-2.08, 2.08)
-y_range = (-1.93, 1.93)
-c = complex(-0.69, 0.29)
+x_range = (-1.19, 1.19)
+y_range = (-0.86, 0.86)
+c = complex(-0.71, 0.32)
 max_iter = 300
 
-x = np.linspace(x_range[0], x_range[1], width, dtype=np.float32)
-y = np.linspace(y_range[0], y_range[1], height, dtype=np.float32)
+x = np.linspace(x_range[0], x_range[1], width)
+y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
 Z = X + 1j * Y
 
-iteration = np.full(Z.shape, max_iter, dtype=np.uint16)
+iteration = np.zeros(Z.shape, dtype=int)
 mask = np.ones(Z.shape, dtype=bool)
-escape_radius = 4.0
 
 for i in range(max_iter):
-    if not np.any(mask):
-        break
     Z[mask] = Z[mask] ** 2 + c
-    mask_new = np.abs(Z) <= escape_radius
+    mask_new = np.abs(Z) <= 2
     iteration[mask & ~mask_new] = i
     mask = mask_new
 
-with np.errstate(divide='ignore', invalid='ignore'):
-    smooth = iteration + 1 - np.log(np.log2(np.abs(Z)))
-    smooth = np.nan_to_num(smooth)
-
 fig, ax = plt.subplots(figsize=(8, 8), dpi=112)
-im = ax.imshow(smooth, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), 
-               origin='lower', cmap='plasma')
-ax.set_title('Julia Set (Optimized)', fontsize=14)
+im = ax.imshow(iteration, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), 
+               origin='lower', cmap='hsv')
+ax.set_title('Julia Set (Bright HSV)', fontsize=14)
 ax.set_xlabel('Re(z)', fontsize=12)
 ax.set_ylabel('Im(z)', fontsize=12)
 
