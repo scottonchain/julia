4497e25ea098d3f48699cf3b4f170d5413d5562c
Scott <scottimig@gmail.com>
2017-01-02 14:34:40 -0700

    saving

--- julia_render.py
+++ julia_render.py
@@ -1,12 +1,12 @@
 import numpy as np
-from PIL import Image, ImageFilter, ImageEnhance, ImageOps
+from PIL import Image, ImageFilter, ImageEnhance
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-0.46, 0.46)
-y_range = (-0.46, 0.46)
-c = complex(0.28, -0.5)
-max_iter = 350
+x_range = (-1.31, 1.31)
+y_range = (-1.31, 1.31)
+c = complex(-0.41, 0.65)
+max_iter = 340
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
@@ -27,23 +27,22 @@ with np.errstate(divide='ignore', invalid='ignore'):
 smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1
-hsv[..., 1] = 0.95 - 0.1 * np.abs(np.sin(2 * np.pi * smooth_norm))
-hsv[..., 2] = smooth_norm ** 0.2
+hsv[..., 0] = (0.8 * smooth_norm + 0.2) % 1
+hsv[..., 1] = 0.7 + 0.3 * np.abs(np.sin(4 * np.pi * smooth_norm))
+hsv[..., 2] = smooth_norm ** 0.5
 
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
 
-img = ImageOps.posterize(img, 2)
-
-def diagonal_wave(im, amp=10, freq=0.12):
+def wave_warp(im, amp=10, freq=0.1):
     arr = np.array(im)
     for i in range(arr.shape[0]):
-        arr[i] = np.roll(arr[i], int(amp * np.sin(freq * i + freq * i)))
+        arr[i] = np.roll(arr[i], int(amp * np.sin(freq * i)))
     return Image.fromarray(arr)
 
-img = diagonal_wave(img, amp=20, freq=0.18)
-img = img.filter(ImageFilter.DETAIL)
+img = wave_warp(img, amp=15, freq=0.15)
+enhanced = ImageEnhance.Color(img).enhance(2.0)
+enhanced = ImageEnhance.Contrast(enhanced).enhance(1.2)
 
 output_path = 'julia_output.jpg'
-img.save(output_path) 
+enhanced.save(output_path) 
