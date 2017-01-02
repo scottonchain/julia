419a1ef39359094ab71b75af3c288fcfbe117fa6
Scott <scottimig@gmail.com>
2017-01-02 10:53:43 -0700

    saving

--- julia_render.py
+++ julia_render.py
@@ -3,10 +3,10 @@ from PIL import Image, ImageFilter, ImageEnhance, ImageOps
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-1.76, 1.76)
-y_range = (-1.76, 1.76)
-c = complex(0.39, -0.19)
-max_iter = 380
+x_range = (-1.54, 1.54)
+y_range = (-1.6, 1.6)
+c = complex(0.36, 0.31)
+max_iter = 360
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
@@ -28,30 +28,26 @@ smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
 hsv = np.zeros((height, width, 3), dtype=float)
 hsv[..., 0] = (0.8 * smooth_norm + 0.3) % 1
-hsv[..., 1] = 0.9 - 0.7 * smooth_norm
-hsv[..., 2] = smooth_norm ** 0.7
+hsv[..., 1] = 0.7 + 0.3 * np.abs(np.sin(2 * np.pi * smooth_norm))
+hsv[..., 2] = smooth_norm ** 0.5
 
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
 
-# Shuffle in blocks
-def shuffle_blocks(im, block=40):
-    arr = np.array(im)
-    blocks = []
-    for i in range(0, arr.shape[0], block):
-        for j in range(0, arr.shape[1], block):
-            blocks.append(arr[i:i+block, j:j+block].copy())
-    np.random.shuffle(blocks)
-    idx = 0
-    for i in range(0, arr.shape[0], block):
-        for j in range(0, arr.shape[1], block):
-            arr[i:i+block, j:j+block] = blocks[idx]
-            idx += 1
-    return Image.fromarray(arr)
+img = img.quantize(colors=12, method=2)
+img = img.convert('RGB')
 
-img = shuffle_blocks(img, block=60)
-img = ImageOps.posterize(img, 2)
-img = ImageEnhance.Color(img).enhance(2.0)
+# Circular vignette
+def vignette(im):
+    arr = np.array(im).astype(np.float32)
+    cy, cx = arr.shape[0] // 2, arr.shape[1] // 2
+    Y, X = np.ogrid[:arr.shape[0], :arr.shape[1]]
+    mask = ((Y - cy) ** 2 + (X - cx) ** 2) / (cy * cx) > 0.7
+    arr[mask] = arr[mask] * 0.3
+    return Image.fromarray(arr.astype(np.uint8))
+
+img = vignette(img)
+img = ImageEnhance.Contrast(img).enhance(1.4)
 
 output_path = 'julia_output.jpg'
 img.save(output_path) 
