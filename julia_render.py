590a4d54accfb440cf58a49e7d7d0825f76947a1
Scott <scottimig@gmail.com>
2017-04-08 09:40:37 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -3,10 +3,10 @@ from PIL import Image, ImageFilter, ImageEnhance, ImageDraw
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-1.5, 1.5)
-y_range = (-1.44, 1.44)
-c = complex(-0.78, 0.19)
-max_iter = 400
+x_range = (-1.52, 1.52)
+y_range = (-1.52, 1.52)
+c = complex(0.24, 0.03)
+max_iter = 350
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
@@ -26,30 +26,27 @@ with np.errstate(divide='ignore', invalid='ignore'):
     smooth = np.nan_to_num(smooth)
 smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
-# Deep blue palette
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = 0.6 + 0.1 * smooth_norm
-hsv[..., 1] = 0.7 + 0.3 * smooth_norm
+hsv[..., 0] = 0.0
+hsv[..., 1] = 0.0
 hsv[..., 2] = smooth_norm ** 0.8
 
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
 
-img = img.filter(ImageFilter.GaussianBlur(radius=10))
+img = img.filter(ImageFilter.FIND_EDGES)
+img = ImageEnhance.Contrast(img).enhance(2.5)
 
-# Radial gradient overlay
-def radial_gradient(im):
-    arr = np.array(im).astype(np.float32)
-    cy, cx = arr.shape[0] // 2, arr.shape[1] // 2
-    Y, X = np.ogrid[:arr.shape[0], :arr.shape[1]]
-    r = np.sqrt((Y - cy) ** 2 + (X - cx) ** 2)
-    mask = r / r.max()
-    for c in range(3):
-        arr[..., c] = arr[..., c] * (1 - 0.5 * mask)
-    return Image.fromarray(arr.astype(np.uint8))
+# Grid overlay
+def add_grid(im, step=50):
+    draw = ImageDraw.Draw(im)
+    for x in range(0, im.width, step):
+        draw.line((x, 0, x, im.height), fill=(255,255,255,80), width=1)
+    for y in range(0, im.height, step):
+        draw.line((0, y, im.width, y), fill=(255,255,255,80), width=1)
+    return im
 
-img = radial_gradient(img)
-img = ImageEnhance.Brightness(img).enhance(1.1)
+img = add_grid(img, step=60)
 
 output_path = 'julia_output.jpg'
 img.save(output_path) 
