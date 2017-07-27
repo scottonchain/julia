81b3e2730ecacd5d0b16fa6c0929270a6bed9aec
Scott <scottimig@gmail.com>
2017-07-27 17:23:31 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -1,33 +1,42 @@
 import numpy as np
-import matplotlib.pyplot as plt
+from PIL import Image, ImageFilter, ImageEnhance
+from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-1.14, 1.14)
-y_range = (-0.93, 0.93)
-c = complex(-0.69, 0.23)
-max_iter = 300
+x_range = (-1.37, 1.37)
+y_range = (-1.48, 1.48)
+c = complex(-0.82, 0.19)
+max_iter = 400
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
 Z = X + 1j * Y
 
-iteration = np.zeros(Z.shape, dtype=int)
+div_iter = np.zeros(Z.shape, dtype=int)
 mask = np.ones(Z.shape, dtype=bool)
-
 for i in range(max_iter):
     Z[mask] = Z[mask] ** 2 + c
     mask_new = np.abs(Z) <= 2
-    iteration[mask & ~mask_new] = i
+    div_iter[mask & ~mask_new] = i
     mask = mask_new
 
-fig, ax = plt.subplots(figsize=(8, 8), dpi=112)
-im = ax.imshow(iteration, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), 
-               origin='lower', cmap='jet')
-ax.set_title('Julia Set (Bright Jet)', fontsize=14)
-ax.set_xlabel('Re(z)', fontsize=12)
-ax.set_ylabel('Im(z)', fontsize=12)
+with np.errstate(divide='ignore', invalid='ignore'):
+    smooth = div_iter + 1 - np.log(np.log2(np.abs(Z)))
+    smooth = np.nan_to_num(smooth)
+smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
-plt.tight_layout()
-plt.savefig('julia_output.jpg', dpi=112, bbox_inches='tight')
-plt.close() 
+hsv = np.zeros((height, width, 3), dtype=float)
+hsv[..., 0] = (0.3 * smooth_norm + 0.7) % 1
+hsv[..., 1] = 0.95 - 0.3 * smooth_norm
+hsv[..., 2] = smooth_norm ** 0.7
+
+rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
+img = Image.fromarray(rgb)
+
+img = img.filter(ImageFilter.FIND_EDGES)
+enhanced = ImageEnhance.Contrast(img).enhance(2.5)
+enhanced = ImageEnhance.Color(enhanced).enhance(1.8)
+
+output_path = 'julia_output.jpg'
+enhanced.save(output_path) 
