83391a82e1b7d341b5e8ce72b57be9b3dae7e684
Scott <scottimig@gmail.com>
2017-07-27 16:52:02 -0600

    saving

--- julia_render.py
+++ julia_render.py
@@ -2,9 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 width, height = 1600, 1600
-x_range = (-1.11, 1.11)
-y_range = (-0.81, 0.81)
-c = complex(-0.69, 0.25)
+x_range = (-1.14, 1.14)
+y_range = (-0.93, 0.93)
+c = complex(-0.69, 0.23)
 max_iter = 300
 
 x = np.linspace(x_range[0], x_range[1], width)
@@ -23,8 +23,8 @@ for i in range(max_iter):
 
 fig, ax = plt.subplots(figsize=(8, 8), dpi=112)
 im = ax.imshow(iteration, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), 
-               origin='lower', cmap='brg')
-ax.set_title('Julia Set (Bright BRG)', fontsize=14)
+               origin='lower', cmap='jet')
+ax.set_title('Julia Set (Bright Jet)', fontsize=14)
 ax.set_xlabel('Re(z)', fontsize=12)
 ax.set_ylabel('Im(z)', fontsize=12)
 
