a53e77a58b3cdccbc933caf11caea420f1fb4d8b
Scott <scottimig@gmail.com>
2017-08-08 12:48:07 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -1,63 +1,59 @@
 import numpy as np
-from PIL import Image, ImageDraw, ImageEnhance
+import matplotlib.pyplot as plt
+from numba import jit
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-1.92, 1.92)
-y_range = (-2.04, 2.04)
-max_iter = 300
+x_range = (-0.72, 0.72)
+y_range = (-0.66, 0.66)
+c = complex(-0.72, 0.29)
+max_iter = 500
 
-# Tricorn fractal
-x = np.linspace(x_range[0], x_range[1], width)
-y = np.linspace(y_range[0], y_range[1], height)
-X, Y = np.meshgrid(x, y)
-C = X + 1j * Y
-Z = np.zeros_like(C)
-tricorn = np.zeros(C.shape, dtype=int)
-mask = np.ones(C.shape, dtype=bool)
-for i in range(max_iter):
-    Z[mask] = np.conj(Z[mask]) ** 2 + C[mask]
-    mask_new = np.abs(Z) <= 2
-    tricorn[mask & ~mask_new] = i
-    mask = mask_new
+@jit(nopython=True)
+def julia_numba(x_range, y_range, width, height, c, max_iter):
+    x = np.linspace(x_range[0], x_range[1], width)
+    y = np.linspace(y_range[0], y_range[1], height)
+    
+    result = np.zeros((height, width), dtype=np.int32)
+    
+    for i in range(height):
+        for j in range(width):
+            z = complex(x[j], y[i])
+            for k in range(max_iter):
+                z = z * z + c
+                if abs(z) > 2:
+                    result[i, j] = k
+                    break
+            else:
+                result[i, j] = max_iter
+    
+    return result
 
+# Generate fractal using Numba
+iteration = julia_numba(x_range, y_range, width, height, c, max_iter)
+
+# Smooth coloring
+with np.errstate(divide='ignore', invalid='ignore'):
+    smooth = iteration + 1 - np.log(np.log2(np.abs(complex(x_range[0], y_range[0]))))
+    smooth = np.nan_to_num(smooth)
+smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
+
+# Bright palette
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.2 * tricorn / max_iter + 0.8) % 1
-hsv[..., 1] = 0.9 + 0.1 * (tricorn / max_iter)
-hsv[..., 2] = (tricorn / max_iter) ** 0.5
+hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1
+hsv[..., 1] = 0.95 - 0.1 * np.abs(np.sin(2 * np.pi * smooth_norm))
+hsv[..., 2] = smooth_norm ** 0.2
+
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
-img = Image.fromarray(rgb)
 
-# Julia set overlay
-c = complex(0.32, 0.03)
-Z2 = X + 1j * Y
-julia = np.zeros(Z2.shape, dtype=int)
-mask = np.ones(Z2.shape, dtype=bool)
-for i in range(max_iter):
-    Z2[mask] = Z2[mask] ** 2 + c
-    mask_new = np.abs(Z2) <= 2
-    julia[mask & ~mask_new] = i
-    mask = mask_new
-julia_img = (julia / max_iter * 255).astype(np.uint8)
-julia_img = Image.fromarray(np.stack([julia_img]*3, axis=-1)).convert('RGBA')
-julia_img.putalpha(80)
-img = img.convert('RGBA')
-img = Image.alpha_composite(img, julia_img)
-img = img.convert('RGB')
+fig, ax = plt.subplots(figsize=(8, 8), dpi=112)
+im = ax.imshow(rgb, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), 
+               origin='lower')
+ax.set_title('Julia Set (Numba Accelerated)', fontsize=14)
+ax.set_xlabel('Re(z)', fontsize=12)
+ax.set_ylabel('Im(z)', fontsize=12)
+ax.grid(True, color='white', alpha=0.3, linestyle='--', linewidth=0.5)
 
-# Color cycling effect
-arr = np.array(img)
-arr = np.roll(arr, shift=30, axis=2)
-img = Image.fromarray(arr)
-
-# Geometric grid overlay
-draw = ImageDraw.Draw(img)
-for x in range(0, width, 60):
-    draw.line((x, 0, x, height), fill=(255,255,255,60), width=1)
-for y in range(0, height, 60):
-    draw.line((0, y, width, y), fill=(255,255,255,60), width=1)
-
-img = ImageEnhance.Color(img).enhance(1.7)
-
-output_path = 'julia_output.jpg'
-img.save(output_path) 
+plt.tight_layout()
+plt.savefig('julia_output.jpg', dpi=112, bbox_inches='tight')
+plt.close() 
