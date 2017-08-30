4e7b0ffddca1f68f1a70eedcdf0ae0b4886bec3d
Scott <scottimig@gmail.com>
2017-08-30 15:11:14 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -1,59 +1,62 @@
 import numpy as np
-from PIL import Image, ImageDraw, ImageEnhance, ImageOps
-from matplotlib.colors import hsv_to_rgb
+import matplotlib.pyplot as plt
+import random
 
 width, height = 1600, 1600
-x_range = (-1.97, 1.97)
-y_range = (-1.97, 1.97)
-c = complex(-0.71, 0.22)
+x_range = (-1.95, 1.95)
+y_range = (-1.95, 1.95)
+c = complex(-0.74, 0.23)
 max_iter = 300
 
-# Julia set
+# Randomize fractal type and iteration formula
+random.seed(42)
+fractal_types = ['julia', 'mandelbrot', 'burning_ship', 'tricorn', 'phoenix']
+selected_type = random.choice(fractal_types)
+
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
 Z = X + 1j * Y
-julia = np.zeros(Z.shape, dtype=int)
+
+iteration = np.zeros(Z.shape, dtype=int)
 mask = np.ones(Z.shape, dtype=bool)
+
+if selected_type == 'julia':
+    C = c
+elif selected_type == 'mandelbrot':
+    C = Z.copy()
+elif selected_type == 'burning_ship':
+    C = Z.copy()
+elif selected_type == 'tricorn':
+    C = c
+elif selected_type == 'phoenix':
+    C = Z.copy()
+    p = random.uniform(-0.5, 0.5) + 1j * random.uniform(-0.5, 0.5)
+    Zold = np.zeros_like(Z)
+
 for i in range(max_iter):
-    Z[mask] = Z[mask] ** 2 + c
+    if selected_type == 'julia':
+        Z[mask] = Z[mask] ** 2 + C
+    elif selected_type == 'mandelbrot':
+        Z[mask] = Z[mask] ** 2 + C[mask]
+    elif selected_type == 'burning_ship':
+        Z[mask] = (np.abs(Z[mask].real) + 1j * np.abs(Z[mask].imag)) ** 2 + C[mask]
+    elif selected_type == 'tricorn':
+        Z[mask] = np.conj(Z[mask]) ** 2 + C
+    elif selected_type == 'phoenix':
+        Z[mask], Zold[mask] = Z[mask] ** 2 + C[mask] + p * Zold[mask], Z[mask]
+    
     mask_new = np.abs(Z) <= 2
-    julia[mask & ~mask_new] = i
+    iteration[mask & ~mask_new] = i
     mask = mask_new
 
-# Mandelbrot mask
-mandel = np.zeros(Z.shape, dtype=int)
-C = X + 1j * Y
-Z2 = np.zeros_like(C)
-mask = np.ones(C.shape, dtype=bool)
-for i in range(max_iter):
-    Z2[mask] = Z2[mask] ** 2 + C[mask]
-    mask_new = np.abs(Z2) <= 2
-    mandel[mask & ~mask_new] = i
-    mask = mask_new
+fig, ax = plt.subplots(figsize=(8, 8), dpi=112)
+im = ax.imshow(iteration, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), 
+               origin='lower', cmap='plasma')
+ax.set_title(f'Fractal (Random Type: {selected_type})', fontsize=14)
+ax.set_xlabel('Re(z)', fontsize=12)
+ax.set_ylabel('Im(z)', fontsize=12)
 
-# Vibrant color map
-hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.9 * julia / max_iter + 0.1) % 1
-hsv[..., 1] = 0.8 + 0.2 * (mandel / max_iter)
-hsv[..., 2] = (julia / max_iter) ** 0.5
-rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
-img = Image.fromarray(rgb)
-
-# Hand-drawn scribble effect
-scribble = Image.new('RGBA', img.size, (0,0,0,0))
-draw = ImageDraw.Draw(scribble)
-for _ in range(200):
-    x0, y0 = np.random.randint(0, width), np.random.randint(0, height)
-    x1, y1 = x0 + np.random.randint(-30, 30), y0 + np.random.randint(-30, 30)
-    color = tuple(np.random.randint(100, 255, 3)) + (np.random.randint(40, 100),)
-    draw.line((x0, y0, x1, y1), fill=color, width=np.random.randint(1, 4))
-img = img.convert('RGBA')
-img = Image.alpha_composite(img, scribble)
-img = img.convert('RGB')
-
-img = ImageEnhance.Color(img).enhance(2.2)
-img = ImageEnhance.Contrast(img).enhance(1.3)
-
-output_path = 'julia_output.jpg'
-img.save(output_path) 
+plt.tight_layout()
+plt.savefig('julia_output.jpg', dpi=112, bbox_inches='tight')
+plt.close() 
