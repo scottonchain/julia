192f0ce523749b4f2d1fcf341c694b57a814d1a9
Scott <scottimig@gmail.com>
2017-09-23 16:14:44 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -1,12 +1,12 @@
 import numpy as np
-from PIL import Image, ImageFilter, ImageEnhance, ImageDraw
+from PIL import Image, ImageFilter, ImageEnhance
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-1.31, 1.31)
-y_range = (-1.31, 1.31)
-c = complex(-0.72, -0.41)
-max_iter = 340
+x_range = (-1.25, 1.25)
+y_range = (-1.2, 1.2)
+c = complex(-0.4, 0.57)
+max_iter = 360
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
@@ -26,24 +26,25 @@ with np.errstate(divide='ignore', invalid='ignore'):
     smooth = np.nan_to_num(smooth)
 smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
+# Fire palette
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.45 * smooth_norm + 0.1) % 1
-hsv[..., 1] = 0.9 - 0.3 * smooth_norm
-hsv[..., 2] = smooth_norm ** 0.5
+hsv[..., 0] = (0.05 + 0.1 * smooth_norm) % 1
+hsv[..., 1] = 1.0 - 0.5 * smooth_norm
+hsv[..., 2] = smooth_norm ** 0.7
 
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
 
-img = img.filter(ImageFilter.MedianFilter(size=7))
+img = img.filter(ImageFilter.GaussianBlur(radius=8))
 
-def add_stripes(im, stripe_width=20):
-    draw = ImageDraw.Draw(im)
-    for x in range(0, im.width, stripe_width*2):
-        draw.rectangle([x, 0, x+stripe_width, im.height], fill=(255,255,255,40))
-    return im
+def horizontal_ripple(im, amp=10, freq=0.1):
+    arr = np.array(im)
+    for i in range(arr.shape[0]):
+        arr[i] = np.roll(arr[i], int(amp * np.sin(freq * i)))
+    return Image.fromarray(arr)
 
-img = add_stripes(img, stripe_width=30)
-img = ImageEnhance.Brightness(img).enhance(1.2)
+img = horizontal_ripple(img, amp=20, freq=0.18)
+img = ImageEnhance.Contrast(img).enhance(1.5)
 
 output_path = 'julia_output.jpg'
 img.save(output_path) 
