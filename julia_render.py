b445d1fda372ab3975de0dff8f6f1b19a68dcdc6
Scott <scottimig@gmail.com>
2017-10-03 03:57:33 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -3,10 +3,10 @@ from PIL import Image, ImageFilter, ImageEnhance, ImageOps
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-2.02, 2.02)
-y_range = (-1.95, 1.95)
-c = complex(0.28, -0.01)
-max_iter = 350
+x_range = (-1.75, 1.75)
+y_range = (-1.75, 1.75)
+c = complex(0.36, -0.2)
+max_iter = 380
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
@@ -27,17 +27,31 @@ with np.errstate(divide='ignore', invalid='ignore'):
 smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1
-hsv[..., 1] = 0.95 - 0.1 * np.abs(np.sin(2 * np.pi * smooth_norm))
-hsv[..., 2] = smooth_norm ** 0.2
+hsv[..., 0] = (0.8 * smooth_norm + 0.3) % 1
+hsv[..., 1] = 0.9 - 0.7 * smooth_norm
+hsv[..., 2] = smooth_norm ** 0.7
 
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
 
-img = ImageOps.mirror(img)
-img = ImageOps.flip(img)
-enhanced = ImageEnhance.Color(img).enhance(1.2)
-enhanced = ImageEnhance.Brightness(enhanced).enhance(1.1)
+# Shuffle in blocks
+def shuffle_blocks(im, block=40):
+    arr = np.array(im)
+    blocks = []
+    for i in range(0, arr.shape[0], block):
+        for j in range(0, arr.shape[1], block):
+            blocks.append(arr[i:i+block, j:j+block].copy())
+    np.random.shuffle(blocks)
+    idx = 0
+    for i in range(0, arr.shape[0], block):
+        for j in range(0, arr.shape[1], block):
+            arr[i:i+block, j:j+block] = blocks[idx]
+            idx += 1
+    return Image.fromarray(arr)
+
+img = shuffle_blocks(img, block=60)
+img = ImageOps.posterize(img, 2)
+img = ImageEnhance.Color(img).enhance(2.0)
 
 output_path = 'julia_output.jpg'
-enhanced.save(output_path) 
+img.save(output_path) 
