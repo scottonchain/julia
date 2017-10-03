06cdd49f364e066c95f8539b3b32c3135ad1a843
Scott <scottimig@gmail.com>
2017-10-03 04:37:05 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -3,10 +3,10 @@ from PIL import Image, ImageFilter, ImageEnhance, ImageOps
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-1.75, 1.75)
-y_range = (-1.75, 1.75)
-c = complex(0.36, -0.2)
-max_iter = 380
+x_range = (-1.52, 1.52)
+y_range = (-1.52, 1.52)
+c = complex(-0.84, 0.13)
+max_iter = 370
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
@@ -26,32 +26,26 @@ with np.errstate(divide='ignore', invalid='ignore'):
     smooth = np.nan_to_num(smooth)
 smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
+# Green-magenta palette
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.8 * smooth_norm + 0.3) % 1
+hsv[..., 0] = (0.4 * smooth_norm + 0.7) % 1
 hsv[..., 1] = 0.9 - 0.7 * smooth_norm
 hsv[..., 2] = smooth_norm ** 0.7
 
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
 
-# Shuffle in blocks
-def shuffle_blocks(im, block=40):
+img = ImageOps.solarize(img, threshold=80)
+
+# Kaleidoscope effect
+def kaleidoscope(im):
     arr = np.array(im)
-    blocks = []
-    for i in range(0, arr.shape[0], block):
-        for j in range(0, arr.shape[1], block):
-            blocks.append(arr[i:i+block, j:j+block].copy())
-    np.random.shuffle(blocks)
-    idx = 0
-    for i in range(0, arr.shape[0], block):
-        for j in range(0, arr.shape[1], block):
-            arr[i:i+block, j:j+block] = blocks[idx]
-            idx += 1
+    arr = np.concatenate([arr, arr[:, ::-1]], axis=1)
+    arr = np.concatenate([arr, arr[::-1, :]], axis=0)
     return Image.fromarray(arr)
 
-img = shuffle_blocks(img, block=60)
-img = ImageOps.posterize(img, 2)
-img = ImageEnhance.Color(img).enhance(2.0)
+img = kaleidoscope(img)
+img = ImageEnhance.Color(img).enhance(1.8)
 
 output_path = 'julia_output.jpg'
 img.save(output_path) 
