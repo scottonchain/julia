6ce638ba98971ca5f77a15aac5d5a6daf8fd5c9c
Scott <scottimig@gmail.com>
2018-01-10 06:04:33 -0700

    save state

--- julia_render.py
+++ julia_render.py
@@ -1,12 +1,12 @@
 import numpy as np
-from PIL import Image, ImageFilter, ImageEnhance, ImageOps
+from PIL import Image, ImageFilter, ImageEnhance, ImageDraw
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-1.98, 1.98)
-y_range = (-1.98, 1.98)
-c = complex(-0.71, -0.37)
-max_iter = 350
+x_range = (-1.45, 1.45)
+y_range = (-1.45, 1.45)
+c = complex(-0.77, 0.12)
+max_iter = 400
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
@@ -26,25 +26,30 @@ with np.errstate(divide='ignore', invalid='ignore'):
     smooth = np.nan_to_num(smooth)
 smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
+# Deep blue palette
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1
-hsv[..., 1] = 0.95 - 0.1 * smooth_norm
-hsv[..., 2] = smooth_norm ** 0.2
+hsv[..., 0] = 0.6 + 0.1 * smooth_norm
+hsv[..., 1] = 0.7 + 0.3 * smooth_norm
+hsv[..., 2] = smooth_norm ** 0.8
 
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
 
-# Heavy pixelation
-def pixelate(im, block=20):
-    arr = np.array(im)
-    for i in range(0, arr.shape[0], block):
-        for j in range(0, arr.shape[1], block):
-            arr[i:i+block, j:j+block] = arr[i, j]
-    return Image.fromarray(arr)
+img = img.filter(ImageFilter.GaussianBlur(radius=10))
 
-img = pixelate(img, block=30)
-img = ImageOps.flip(img)
-img = ImageEnhance.Color(img).enhance(2.0)
+# Radial gradient overlay
+def radial_gradient(im):
+    arr = np.array(im).astype(np.float32)
+    cy, cx = arr.shape[0] // 2, arr.shape[1] // 2
+    Y, X = np.ogrid[:arr.shape[0], :arr.shape[1]]
+    r = np.sqrt((Y - cy) ** 2 + (X - cx) ** 2)
+    mask = r / r.max()
+    for c in range(3):
+        arr[..., c] = arr[..., c] * (1 - 0.5 * mask)
+    return Image.fromarray(arr.astype(np.uint8))
+
+img = radial_gradient(img)
+img = ImageEnhance.Brightness(img).enhance(1.1)
 
 output_path = 'julia_output.jpg'
 img.save(output_path) 
