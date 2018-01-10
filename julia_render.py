2ae4a4e04b7693b60fc3fbbeb0efe7269a0fd901
Scott <scottimig@gmail.com>
2018-01-10 05:31:05 -0700

    update

--- julia_render.py
+++ julia_render.py
@@ -1,45 +1,50 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
+from PIL import Image, ImageFilter, ImageEnhance, ImageOps
+from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-# Zoomed in region for detail
-x_range = (-1.5, 1.5)
-y_range = (-1.5, 1.5)
-c = complex(0.4, 0.35)
-max_iter = 800
+x_range = (-1.98, 1.98)
+y_range = (-1.98, 1.98)
+c = complex(-0.71, -0.37)
+max_iter = 350
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
 Z = X + 1j * Y
 
-iteration = np.zeros(Z.shape, dtype=int)
+div_iter = np.zeros(Z.shape, dtype=int)
 mask = np.ones(Z.shape, dtype=bool)
 for i in range(max_iter):
     Z[mask] = Z[mask] ** 2 + c
     mask_new = np.abs(Z) <= 2
-    iteration[mask & ~mask_new] = i
+    div_iter[mask & ~mask_new] = i
     mask = mask_new
 
-# Smooth coloring for better detail
 with np.errstate(divide='ignore', invalid='ignore'):
-    smooth = iteration + 1 - np.log(np.log2(np.abs(Z)))
+    smooth = div_iter + 1 - np.log(np.log2(np.abs(Z)))
     smooth = np.nan_to_num(smooth)
+smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
-# Use bright, warm prismatic colormap
-fig, ax = plt.subplots(figsize=(8, 8), dpi=112)
-im = ax.imshow(smooth, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), 
-               origin='lower', cmap='magma', interpolation='bilinear')
-ax.set_title('Julia Set Detail (c = 0.355 + 0.355i)', fontsize=14)
-ax.set_xlabel('Re(z)', fontsize=12)
-ax.set_ylabel('Im(z)', fontsize=12)
-ax.grid(True, color='white', alpha=0.3, linestyle='--', linewidth=0.5)
+hsv = np.zeros((height, width, 3), dtype=float)
+hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1
+hsv[..., 1] = 0.95 - 0.1 * smooth_norm
+hsv[..., 2] = smooth_norm ** 0.2
 
-# Add colorbar
-cbar = plt.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
-cbar.set_label('Iteration Count (Smooth)', fontsize=12)
+rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
+img = Image.fromarray(rgb)
 
-plt.tight_layout()
-plt.savefig('julia_output.jpg', dpi=112, bbox_inches='tight')
-plt.close() 
+# Heavy pixelation
+def pixelate(im, block=20):
+    arr = np.array(im)
+    for i in range(0, arr.shape[0], block):
+        for j in range(0, arr.shape[1], block):
+            arr[i:i+block, j:j+block] = arr[i, j]
+    return Image.fromarray(arr)
+
+img = pixelate(img, block=30)
+img = ImageOps.flip(img)
+img = ImageEnhance.Color(img).enhance(2.0)
+
+output_path = 'julia_output.jpg'
+img.save(output_path) 
