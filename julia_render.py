941b7d633264d97dd1d1d5a08e120e4b3ba47353
Scott <scottimig@gmail.com>
2018-01-17 02:24:29 -0700

    update

--- julia_render.py
+++ julia_render.py
@@ -3,10 +3,10 @@ from PIL import Image, ImageFilter, ImageEnhance, ImageOps
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-1.69, 1.69)
-y_range = (-1.75, 1.75)
-c = complex(0.35, -0.18)
-max_iter = 380
+x_range = (-2.08, 2.08)
+y_range = (-2.08, 2.08)
+c = complex(-0.68, -0.37)
+max_iter = 350
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
@@ -27,30 +27,23 @@ with np.errstate(divide='ignore', invalid='ignore'):
 smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.8 * smooth_norm + 0.3) % 1
-hsv[..., 1] = 0.9 - 0.7 * smooth_norm
-hsv[..., 2] = smooth_norm ** 0.7
+hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1
+hsv[..., 1] = 0.95 - 0.1 * smooth_norm
+hsv[..., 2] = smooth_norm ** 0.2
 
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
 
-# Shuffle in blocks
-def shuffle_blocks(im, block=40):
+# Heavy pixelation
+def pixelate(im, block=20):
     arr = np.array(im)
-    blocks = []
     for i in range(0, arr.shape[0], block):
         for j in range(0, arr.shape[1], block):
-            blocks.append(arr[i:i+block, j:j+block].copy())
-    np.random.shuffle(blocks)
-    idx = 0
-    for i in range(0, arr.shape[0], block):
-        for j in range(0, arr.shape[1], block):
-            arr[i:i+block, j:j+block] = blocks[idx]
-            idx += 1
+            arr[i:i+block, j:j+block] = arr[i, j]
     return Image.fromarray(arr)
 
-img = shuffle_blocks(img, block=60)
-img = ImageOps.posterize(img, 2)
+img = pixelate(img, block=30)
+img = ImageOps.flip(img)
 img = ImageEnhance.Color(img).enhance(2.0)
 
 output_path = 'julia_output.jpg'
