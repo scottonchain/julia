b0e4fa6897d94ac16042a964a5570f6acf7308aa
Scott <scottimig@gmail.com>
2018-02-23 23:03:23 -0700

    saving

--- julia_render.py
+++ julia_render.py
@@ -1,42 +1,59 @@
 import numpy as np
-from PIL import Image, ImageFilter, ImageEnhance
+from PIL import Image, ImageDraw, ImageEnhance, ImageOps
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-1.4, 1.4)
-y_range = (-1.4, 1.4)
-c = complex(-0.79, 0.17)
-max_iter = 400
+x_range = (-2.06, 2.06)
+y_range = (-2.0, 2.0)
+c = complex(-0.72, 0.31)
+max_iter = 300
 
+# Julia set
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
 Z = X + 1j * Y
-
-div_iter = np.zeros(Z.shape, dtype=int)
+julia = np.zeros(Z.shape, dtype=int)
 mask = np.ones(Z.shape, dtype=bool)
 for i in range(max_iter):
     Z[mask] = Z[mask] ** 2 + c
     mask_new = np.abs(Z) <= 2
-    div_iter[mask & ~mask_new] = i
+    julia[mask & ~mask_new] = i
     mask = mask_new
 
-with np.errstate(divide='ignore', invalid='ignore'):
-    smooth = div_iter + 1 - np.log(np.log2(np.abs(Z)))
-    smooth = np.nan_to_num(smooth)
-smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
+# Mandelbrot mask
+mandel = np.zeros(Z.shape, dtype=int)
+C = X + 1j * Y
+Z2 = np.zeros_like(C)
+mask = np.ones(C.shape, dtype=bool)
+for i in range(max_iter):
+    Z2[mask] = Z2[mask] ** 2 + C[mask]
+    mask_new = np.abs(Z2) <= 2
+    mandel[mask & ~mask_new] = i
+    mask = mask_new
 
+# Vibrant color map
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.3 * smooth_norm + 0.7) % 1
-hsv[..., 1] = 0.95 - 0.3 * smooth_norm
-hsv[..., 2] = smooth_norm ** 0.7
-
+hsv[..., 0] = (0.9 * julia / max_iter + 0.1) % 1
+hsv[..., 1] = 0.8 + 0.2 * (mandel / max_iter)
+hsv[..., 2] = (julia / max_iter) ** 0.5
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
 
-img = img.filter(ImageFilter.FIND_EDGES)
-enhanced = ImageEnhance.Contrast(img).enhance(2.5)
-enhanced = ImageEnhance.Color(enhanced).enhance(1.8)
+# Hand-drawn scribble effect
+scribble = Image.new('RGBA', img.size, (0,0,0,0))
+draw = ImageDraw.Draw(scribble)
+for _ in range(200):
+    x0, y0 = np.random.randint(0, width), np.random.randint(0, height)
+    x1, y1 = x0 + np.random.randint(-30, 30), y0 + np.random.randint(-30, 30)
+    color = tuple(np.random.randint(100, 255, 3)) + (np.random.randint(40, 100),)
+    draw.line((x0, y0, x1, y1), fill=color, width=np.random.randint(1, 4))
+img = img.convert('RGBA')
+img = Image.alpha_composite(img, scribble)
+img = img.convert('RGB')
+
+img = ImageEnhance.Color(img).enhance(2.2)
+img = ImageEnhance.Contrast(img).enhance(1.3)
 
 output_path = 'julia_output.jpg'
-enhanced.save(output_path) 
+img.save(output_path) 
