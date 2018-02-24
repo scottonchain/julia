d713bb5cf53739d139b7c7931cafafe1f26b966c
Scott <scottimig@gmail.com>
2018-02-23 23:43:53 -0700

    update

--- julia_render.py
+++ julia_render.py
@@ -1,63 +1,49 @@
 import numpy as np
-from PIL import Image, ImageDraw, ImageEnhance
+from PIL import Image, ImageFilter, ImageEnhance
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-2.01, 2.01)
-y_range = (-2.08, 2.08)
-max_iter = 300
+x_range = (-0.65, 0.65)
+y_range = (-0.46, 0.46)
+c = complex(0.43, -0.42)
+max_iter = 350
 
-# Tricorn fractal
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
-C = X + 1j * Y
-Z = np.zeros_like(C)
-tricorn = np.zeros(C.shape, dtype=int)
-mask = np.ones(C.shape, dtype=bool)
+Z = X + 1j * Y
+
+div_iter = np.zeros(Z.shape, dtype=int)
+mask = np.ones(Z.shape, dtype=bool)
 for i in range(max_iter):
-    Z[mask] = np.conj(Z[mask]) ** 2 + C[mask]
+    Z[mask] = Z[mask] ** 2 + c
     mask_new = np.abs(Z) <= 2
-    tricorn[mask & ~mask_new] = i
+    div_iter[mask & ~mask_new] = i
     mask = mask_new
 
+with np.errstate(divide='ignore', invalid='ignore'):
+    smooth = div_iter + 1 - np.log(np.log2(np.abs(Z)))
+    smooth = np.nan_to_num(smooth)
+smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
+
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.2 * tricorn / max_iter + 0.8) % 1
-hsv[..., 1] = 0.9 + 0.1 * (tricorn / max_iter)
-hsv[..., 2] = (tricorn / max_iter) ** 0.5
+hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1
+hsv[..., 1] = 0.95 - 0.1 * smooth_norm
+hsv[..., 2] = smooth_norm ** 0.2
+
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
 
-# Julia set overlay
-c = complex(0.27, -0.04)
-Z2 = X + 1j * Y
-julia = np.zeros(Z2.shape, dtype=int)
-mask = np.ones(Z2.shape, dtype=bool)
-for i in range(max_iter):
-    Z2[mask] = Z2[mask] ** 2 + c
-    mask_new = np.abs(Z2) <= 2
-    julia[mask & ~mask_new] = i
-    mask = mask_new
-julia_img = (julia / max_iter * 255).astype(np.uint8)
-julia_img = Image.fromarray(np.stack([julia_img]*3, axis=-1)).convert('RGBA')
-julia_img.putalpha(80)
-img = img.convert('RGBA')
-img = Image.alpha_composite(img, julia_img)
-img = img.convert('RGB')
+# Duotone palette
+def duotone(im, color1=(30, 30, 120), color2=(220, 220, 60)):
+    arr = np.array(im).astype(np.float32) / 255.0
+    mask = arr[..., 0] > 0.5
+    arr[mask] = np.array(color1) / 255.0
+    arr[~mask] = np.array(color2) / 255.0
+    return Image.fromarray((arr * 255).astype(np.uint8))
 
-# Color cycling effect
-arr = np.array(img)
-arr = np.roll(arr, shift=30, axis=2)
-img = Image.fromarray(arr)
-
-# Geometric grid overlay
-draw = ImageDraw.Draw(img)
-for x in range(0, width, 60):
-    draw.line((x, 0, x, height), fill=(255,255,255,60), width=1)
-for y in range(0, height, 60):
-    draw.line((0, y, width, y), fill=(255,255,255,60), width=1)
-
-img = ImageEnhance.Color(img).enhance(1.7)
+img = duotone(img)
+img = img.filter(ImageFilter.EDGE_ENHANCE_MORE)
 
 output_path = 'julia_output.jpg'
 img.save(output_path) 
