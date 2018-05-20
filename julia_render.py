e7882117ca120433ea9c34ee5b113f08414987bc
Scott <scottimig@gmail.com>
2018-05-19 22:29:03 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -1,59 +1,57 @@
 import numpy as np
-from PIL import Image, ImageDraw, ImageEnhance, ImageOps
+from PIL import Image, ImageFilter, ImageEnhance, ImageOps
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-2.01, 2.01)
-y_range = (-2.01, 2.01)
-c = complex(-0.72, 0.26)
-max_iter = 300
+x_range = (-1.67, 1.67)
+y_range = (-1.73, 1.73)
+c = complex(0.35, -0.22)
+max_iter = 380
 
-# Julia set
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
 Z = X + 1j * Y
-julia = np.zeros(Z.shape, dtype=int)
+
+div_iter = np.zeros(Z.shape, dtype=int)
 mask = np.ones(Z.shape, dtype=bool)
 for i in range(max_iter):
     Z[mask] = Z[mask] ** 2 + c
     mask_new = np.abs(Z) <= 2
-    julia[mask & ~mask_new] = i
+    div_iter[mask & ~mask_new] = i
     mask = mask_new
 
-# Mandelbrot mask
-mandel = np.zeros(Z.shape, dtype=int)
-C = X + 1j * Y
-Z2 = np.zeros_like(C)
-mask = np.ones(C.shape, dtype=bool)
-for i in range(max_iter):
-    Z2[mask] = Z2[mask] ** 2 + C[mask]
-    mask_new = np.abs(Z2) <= 2
-    mandel[mask & ~mask_new] = i
-    mask = mask_new
+with np.errstate(divide='ignore', invalid='ignore'):
+    smooth = div_iter + 1 - np.log(np.log2(np.abs(Z)))
+    smooth = np.nan_to_num(smooth)
+smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
-# Vibrant color map
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.9 * julia / max_iter + 0.1) % 1
-hsv[..., 1] = 0.8 + 0.2 * (mandel / max_iter)
-hsv[..., 2] = (julia / max_iter) ** 0.5
+hsv[..., 0] = (0.8 * smooth_norm + 0.3) % 1
+hsv[..., 1] = 0.9 - 0.7 * smooth_norm
+hsv[..., 2] = smooth_norm ** 0.7
+
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
 
-# Hand-drawn scribble effect
-scribble = Image.new('RGBA', img.size, (0,0,0,0))
-draw = ImageDraw.Draw(scribble)
-for _ in range(200):
-    x0, y0 = np.random.randint(0, width), np.random.randint(0, height)
-    x1, y1 = x0 + np.random.randint(-30, 30), y0 + np.random.randint(-30, 30)
-    color = tuple(np.random.randint(100, 255, 3)) + (np.random.randint(40, 100),)
-    draw.line((x0, y0, x1, y1), fill=color, width=np.random.randint(1, 4))
-img = img.convert('RGBA')
-img = Image.alpha_composite(img, scribble)
-img = img.convert('RGB')
+# Shuffle in blocks
+def shuffle_blocks(im, block=40):
+    arr = np.array(im)
+    blocks = []
+    for i in range(0, arr.shape[0], block):
+        for j in range(0, arr.shape[1], block):
+            blocks.append(arr[i:i+block, j:j+block].copy())
+    np.random.shuffle(blocks)
+    idx = 0
+    for i in range(0, arr.shape[0], block):
+        for j in range(0, arr.shape[1], block):
+            arr[i:i+block, j:j+block] = blocks[idx]
+            idx += 1
+    return Image.fromarray(arr)
 
-img = ImageEnhance.Color(img).enhance(2.2)
-img = ImageEnhance.Contrast(img).enhance(1.3)
+img = shuffle_blocks(img, block=60)
+img = ImageOps.posterize(img, 2)
+img = ImageEnhance.Color(img).enhance(2.0)
 
 output_path = 'julia_output.jpg'
 img.save(output_path) 
