c49688003313aca3090f437ef61ff7e573414fc7
Scott <scottimig@gmail.com>
2018-05-19 21:28:52 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -1,50 +1,45 @@
 import numpy as np
-from PIL import Image, ImageFilter, ImageEnhance, ImageOps
+from PIL import Image, ImageDraw, ImageEnhance
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-0.64, 0.64)
-y_range = (-0.64, 0.64)
-c = complex(0.39, -0.19)
-max_iter = 380
+x_range = (-2.2, 1.2)
+y_range = (-1.7, 1.7)
+max_iter = 320
+p = -0.3 + 0.6j
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
-Z = X + 1j * Y
-
-div_iter = np.zeros(Z.shape, dtype=int)
-mask = np.ones(Z.shape, dtype=bool)
+C = X + 1j * Y
+Z = np.zeros_like(C)
+Zold = np.zeros_like(C)
+phoenix = np.zeros(C.shape, dtype=int)
+mask = np.ones(C.shape, dtype=bool)
 for i in range(max_iter):
-    Z[mask] = Z[mask] ** 2 + c
+    Z[mask], Zold[mask] = Z[mask] ** 2 + C[mask] + p * Zold[mask], Z[mask]
     mask_new = np.abs(Z) <= 2
-    div_iter[mask & ~mask_new] = i
+    phoenix[mask & ~mask_new] = i
     mask = mask_new
 
-with np.errstate(divide='ignore', invalid='ignore'):
-    smooth = div_iter + 1 - np.log(np.log2(np.abs(Z)))
-    smooth = np.nan_to_num(smooth)
-smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
-
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1
-hsv[..., 1] = 0.95 - 0.1 * smooth_norm
-hsv[..., 2] = smooth_norm ** 0.2
-
+hsv[..., 0] = (0.15 * phoenix / max_iter + 0.8) % 1
+hsv[..., 1] = 0.8 + 0.2 * (phoenix / max_iter)
+hsv[..., 2] = (phoenix / max_iter) ** 0.6
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
 
-img = img.filter(ImageFilter.FIND_EDGES)
+# Overlay: grid and roots of unity
+step = 0.4
+for val in np.arange(x_range[0], x_range[1] + step, step):
+    x_pix = int((val - x_range[0]) / (x_range[1] - x_range[0]) * width)
+    ImageDraw.Draw(img).line([(x_pix, 0), (x_pix, height)], fill=(180, 220, 255), width=1)
+for val in np.arange(y_range[0], y_range[1] + step, step):
+    y_pix = int((y_range[1] - val) / (y_range[1] - y_range[0]) * height)
+    ImageDraw.Draw(img).line([(0, y_pix), (width, y_pix)], fill=(180, 220, 255), width=1)
 
-# Horizontal flip every 100 pixels
-def flip_blocks(im, block=100):
-    arr = np.array(im)
-    for i in range(0, arr.shape[0], block*2):
-        arr[i:i+block] = arr[i:i+block][::-1]
-    return Image.fromarray(arr)
-
-img = flip_blocks(img, block=100)
-img = ImageEnhance.Contrast(img).enhance(1.8)
+img = ImageEnhance.Color(img).enhance(1.4)
+img = ImageEnhance.Contrast(img).enhance(1.1)
 
 output_path = 'julia_output.jpg'
 img.save(output_path) 
