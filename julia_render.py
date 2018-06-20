b8e671f94ae4868aabf8e0fbe2be7ba24ecd11c7
Scott <scottimig@gmail.com>
2018-06-20 05:39:46 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -4,10 +4,10 @@ import matplotlib.cm as cm
 
 width, height = 1600, 1600
 # Zoomed in region for detail
-x_range = (-2.02, 2.02)
-y_range = (-2.02, 2.02)
-c = complex(-0.45, 0.56)
-max_iter = 700
+x_range = (-1.95, 1.95)
+y_range = (-2.06, 2.06)
+c = complex(0.33, 0.04)
+max_iter = 600
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
@@ -30,8 +30,8 @@ with np.errstate(divide='ignore', invalid='ignore'):
 # Use bright, warm prismatic colormap
 fig, ax = plt.subplots(figsize=(8, 8), dpi=112)
 im = ax.imshow(smooth, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), 
-               origin='lower', cmap='inferno', interpolation='bilinear')
-ax.set_title('Julia Set Detail (c = -0.4 + 0.6i)', fontsize=14)
+               origin='lower', cmap='viridis', interpolation='bilinear')
+ax.set_title('Julia Set Detail (c = 0.285 + 0.01i)', fontsize=14)
 ax.set_xlabel('Re(z)', fontsize=12)
 ax.set_ylabel('Im(z)', fontsize=12)
 ax.grid(True, color='white', alpha=0.3, linestyle='--', linewidth=0.5)
