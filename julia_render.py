17103599e24b64efdeb25d20711e32ddbd054bc2
Scott <scottimig@gmail.com>
2018-09-19 10:10:13 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -1,62 +1,50 @@
 import numpy as np
-from PIL import Image, ImageEnhance
+from PIL import Image, ImageFilter, ImageEnhance, ImageOps
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-2.05, 2.05)
-y_range = (-2.05, 2.05)
-max_iter = 300
-p = -0.5 + 0.5j
+x_range = (-0.71, 0.71)
+y_range = (-0.71, 0.71)
+c = complex(-0.43, 0.64)
+max_iter = 360
 
-# Phoenix fractal
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
-C = X + 1j * Y
-Z = np.zeros_like(C)
-Zold = np.zeros_like(C)
-phoenix = np.zeros(C.shape, dtype=int)
-mask = np.ones(C.shape, dtype=bool)
+Z = X + 1j * Y
+
+div_iter = np.zeros(Z.shape, dtype=int)
+mask = np.ones(Z.shape, dtype=bool)
 for i in range(max_iter):
-    Z[mask], Zold[mask] = Z[mask] ** 2 + C[mask] + p * Zold[mask], Z[mask]
+    Z[mask] = Z[mask] ** 2 + c
     mask_new = np.abs(Z) <= 2
-    phoenix[mask & ~mask_new] = i
+    div_iter[mask & ~mask_new] = i
     mask = mask_new
 
+with np.errstate(divide='ignore', invalid='ignore'):
+    smooth = div_iter + 1 - np.log(np.log2(np.abs(Z)))
+    smooth = np.nan_to_num(smooth)
+smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
+
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.8 * phoenix / max_iter + 0.1) % 1
-hsv[..., 1] = 0.7 + 0.3 * (phoenix / max_iter)
-hsv[..., 2] = (phoenix / max_iter) ** 0.7
+hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1
+hsv[..., 1] = 0.95 - 0.1 * smooth_norm
+hsv[..., 2] = smooth_norm ** 0.2
+
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
 
-# Julia set as mask
-c = complex(-0.85, 0.2)
-Z2 = X + 1j * Y
-julia = np.zeros(Z2.shape, dtype=int)
-mask = np.ones(Z2.shape, dtype=bool)
-for i in range(max_iter):
-    Z2[mask] = Z2[mask] ** 2 + c
-    mask_new = np.abs(Z2) <= 2
-    julia[mask & ~mask_new] = i
-    mask = mask_new
-alpha = (julia / max_iter * 255).astype(np.uint8)
-img = img.convert('RGBA')
-img.putalpha(Image.fromarray(alpha))
+img = img.filter(ImageFilter.GaussianBlur(radius=8))
 
-# Channel shuffle
-arr = np.array(img)
-arr[..., 0], arr[..., 1], arr[..., 2] = arr[..., 1], arr[..., 2], arr[..., 0]
-img = Image.fromarray(arr, 'RGBA')
+# Vertical split mirror
+def vertical_split_mirror(im):
+    arr = np.array(im)
+    mid = arr.shape[1] // 2
+    arr[:, mid:] = arr[:, :mid][:, ::-1]
+    return Image.fromarray(arr)
 
-# Transparency gradient
-grad = np.linspace(0, 255, height).astype(np.uint8)
-grad = np.tile(grad[:, None], (1, width))
-arr = np.array(img)
-arr[..., 3] = (arr[..., 3].astype(np.float32) * grad / 255).astype(np.uint8)
-img = Image.fromarray(arr, 'RGBA').convert('RGB')
-
-img = ImageEnhance.Color(img).enhance(1.7)
+img = vertical_split_mirror(img)
+img = ImageEnhance.Contrast(img).enhance(1.5)
 
 output_path = 'julia_output.jpg'
 img.save(output_path) 
