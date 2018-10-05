d103a21eb2f9b28d20a2b0405373f1e9668b21a6
Scott <scottimig@gmail.com>
2018-10-05 12:02:04 -0600

    saving

--- julia_render.py
+++ julia_render.py
@@ -1,50 +1,54 @@
 import numpy as np
-from PIL import Image, ImageFilter, ImageEnhance, ImageOps
+import matplotlib.pyplot as plt
+from scipy import ndimage
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-0.78, 0.78)
-y_range = (-0.66, 0.66)
-c = complex(0.36, -0.19)
-max_iter = 380
+x_range = (-0.68, 0.68)
+y_range = (-0.68, 0.68)
+c = complex(-0.69, 0.23)
+max_iter = 500
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
 Z = X + 1j * Y
 
-div_iter = np.zeros(Z.shape, dtype=int)
+iteration = np.zeros(Z.shape, dtype=int)
 mask = np.ones(Z.shape, dtype=bool)
+
 for i in range(max_iter):
     Z[mask] = Z[mask] ** 2 + c
     mask_new = np.abs(Z) <= 2
-    div_iter[mask & ~mask_new] = i
+    iteration[mask & ~mask_new] = i
     mask = mask_new
 
+# Apply scipy filters for enhancement
+iteration_smooth = ndimage.gaussian_filter(iteration.astype(float), sigma=0.5)
+iteration_enhanced = ndimage.uniform_filter(iteration_smooth, size=2)
+
+# Smooth coloring
 with np.errstate(divide='ignore', invalid='ignore'):
-    smooth = div_iter + 1 - np.log(np.log2(np.abs(Z)))
+    smooth = iteration_enhanced + 1 - np.log(np.log2(np.abs(Z)))
     smooth = np.nan_to_num(smooth)
 smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
+# Bright palette
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1
-hsv[..., 1] = 0.95 - 0.1 * smooth_norm
+hsv[..., 0] = (0.6 * smooth_norm + 0.3) % 1
+hsv[..., 1] = 0.95 - 0.1 * np.abs(np.sin(2 * np.pi * smooth_norm))
 hsv[..., 2] = smooth_norm ** 0.2
 
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
-img = Image.fromarray(rgb)
 
-img = img.filter(ImageFilter.FIND_EDGES)
+fig, ax = plt.subplots(figsize=(8, 8), dpi=112)
+im = ax.imshow(rgb, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), 
+               origin='lower')
+ax.set_title('Julia Set (SciPy Enhanced)', fontsize=14)
+ax.set_xlabel('Re(z)', fontsize=12)
+ax.set_ylabel('Im(z)', fontsize=12)
+ax.grid(True, color='white', alpha=0.3, linestyle='--', linewidth=0.5)
 
-# Horizontal flip every 100 pixels
-def flip_blocks(im, block=100):
-    arr = np.array(im)
-    for i in range(0, arr.shape[0], block*2):
-        arr[i:i+block] = arr[i:i+block][::-1]
-    return Image.fromarray(arr)
-
-img = flip_blocks(img, block=100)
-img = ImageEnhance.Contrast(img).enhance(1.8)
-
-output_path = 'julia_output.jpg'
-img.save(output_path) 
+plt.tight_layout()
+plt.savefig('julia_output.jpg', dpi=112, bbox_inches='tight')
+plt.close() 
