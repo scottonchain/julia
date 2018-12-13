d99fa5ed6d4d40b87f88378b01bb2aa736e07cbb
Scott <scottimig@gmail.com>
2018-12-12 22:35:57 -0700

    save state

--- julia_render.py
+++ julia_render.py
@@ -1,56 +1,44 @@
 import numpy as np
-from PIL import Image, ImageFilter, ImageEnhance
 import matplotlib.pyplot as plt
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-1.53, 1.53)
-y_range = (-1.6, 1.6)
-c = complex(-0.77, 0.14)
-max_iter = 370
+x_range = (-1.09, 1.09)
+y_range = (-0.84, 0.84)
+c = complex(-0.66, 0.28)
+max_iter = 300
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
 Z = X + 1j * Y
 
-div_iter = np.zeros(Z.shape, dtype=int)
+iteration = np.zeros(Z.shape, dtype=int)
 mask = np.ones(Z.shape, dtype=bool)
 
 for i in range(max_iter):
     Z[mask] = Z[mask] ** 2 + c
     mask_new = np.abs(Z) <= 2
-    div_iter[mask & ~mask_new] = i
+    iteration[mask & ~mask_new] = i
     mask = mask_new
 
+# Smooth coloring
 with np.errstate(divide='ignore', invalid='ignore'):
-    smooth = div_iter + 1 - np.log(np.log2(np.abs(Z)))
+    smooth = iteration + 1 - np.log(np.log2(np.abs(Z)))
     smooth = np.nan_to_num(smooth)
 smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
-hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.5 * smooth_norm + 0.3) % 1
-hsv[..., 1] = 0.95 - 0.3 * np.abs(np.sin(2 * np.pi * smooth_norm))
-hsv[..., 2] = smooth_norm ** 0.4
+# Bright autumn palette
+from matplotlib import cm
+rgb = cm.autumn(smooth_norm)[..., :3]
 
-rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
-img = Image.fromarray(rgb)
+fig, ax = plt.subplots(figsize=(8, 8), dpi=112)
+im = ax.imshow(rgb, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), 
+               origin='lower')
+ax.set_title('Julia Set (Bright Autumn)', fontsize=14)
+ax.set_xlabel('Re(z)', fontsize=12)
+ax.set_ylabel('Im(z)', fontsize=12)
 
-def glass_distort(im, scale=6):
-    arr = np.array(im)
-    dx = (np.random.rand(*arr.shape[:2]) - 0.5) * scale
-    dy = (np.random.rand(*arr.shape[:2]) - 0.5) * scale
-    Y, X = np.meshgrid(np.arange(arr.shape[0]), np.arange(arr.shape[1]), indexing='ij')
-    Xn = np.clip((X + dx).astype(int), 0, arr.shape[1] - 1)
-    Yn = np.clip((Y + dy).astype(int), 0, arr.shape[0] - 1)
-    distorted = arr[Yn, Xn]
-    return Image.fromarray(distorted)
-
-img = glass_distort(img, scale=8)
-
-blur = img.filter(ImageFilter.GaussianBlur(radius=1))
-enhanced = ImageEnhance.Color(blur).enhance(2.2)
-enhanced = ImageEnhance.Contrast(enhanced).enhance(1.5)
-
-output_path = 'julia_output.jpg'
-enhanced.save(output_path) 
+plt.tight_layout()
+plt.savefig('julia_output.jpg', dpi=112, bbox_inches='tight')
+plt.close() 
