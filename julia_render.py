6133f6a5cc47cbeb00cfed768c82e95be2523172
Scott <scottimig@gmail.com>
2018-12-16 17:41:39 -0700

    saving

--- julia_render.py
+++ julia_render.py
@@ -1,53 +1,39 @@
 import numpy as np
-from PIL import Image, ImageFilter, ImageEnhance, ImageDraw
-from matplotlib.colors import hsv_to_rgb
+import matplotlib.pyplot as plt
+import random
 
 width, height = 1600, 1600
-x_range = (-0.77, 0.77)
-y_range = (-0.77, 0.77)
-c = complex(0.38, 0.32)
-max_iter = 340
+x_range = (-2.04, 2.04)
+y_range = (-1.95, 1.95)
+c = complex(-0.7, 0.29)
+max_iter = 300
+
+# Randomize color palette
+random.seed(42)
+colormaps = ['plasma', 'viridis', 'inferno', 'magma', 'hot', 'cool', 'spring', 'summer', 'autumn', 'winter', 'rainbow', 'jet', 'hsv', 'twilight', 'brg']
+selected_cmap = random.choice(colormaps)
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
 Z = X + 1j * Y
 
-div_iter = np.zeros(Z.shape, dtype=int)
+iteration = np.zeros(Z.shape, dtype=int)
 mask = np.ones(Z.shape, dtype=bool)
+
 for i in range(max_iter):
     Z[mask] = Z[mask] ** 2 + c
     mask_new = np.abs(Z) <= 2
-    div_iter[mask & ~mask_new] = i
+    iteration[mask & ~mask_new] = i
     mask = mask_new
 
-with np.errstate(divide='ignore', invalid='ignore'):
-    smooth = div_iter + 1 - np.log(np.log2(np.abs(Z)))
-    smooth = np.nan_to_num(smooth)
-smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
+fig, ax = plt.subplots(figsize=(8, 8), dpi=112)
+im = ax.imshow(iteration, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), 
+               origin='lower', cmap=selected_cmap)
+ax.set_title(f'Julia Set (Random Palette: {selected_cmap})', fontsize=14)
+ax.set_xlabel('Re(z)', fontsize=12)
+ax.set_ylabel('Im(z)', fontsize=12)
 
-# Bright gold palette
-hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = 0.12 + 0.08 * smooth_norm
-hsv[..., 1] = 0.9 - 0.3 * smooth_norm
-hsv[..., 2] = smooth_norm ** 0.5
-
-rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
-img = Image.fromarray(rgb)
-
-img = img.filter(ImageFilter.EMBOSS)
-
-# Grid overlay
-def add_grid(im, step=50):
-    draw = ImageDraw.Draw(im)
-    for x in range(0, im.width, step):
-        draw.line((x, 0, x, im.height), fill=(255,255,255,80), width=1)
-    for y in range(0, im.height, step):
-        draw.line((0, y, im.width, y), fill=(255,255,255,80), width=1)
-    return im
-
-img = add_grid(img, step=60)
-img = ImageEnhance.Color(img).enhance(1.7)
-
-output_path = 'julia_output.jpg'
-img.save(output_path) 
+plt.tight_layout()
+plt.savefig('julia_output.jpg', dpi=112, bbox_inches='tight')
+plt.close() 
