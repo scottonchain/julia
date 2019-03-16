61b5e1c8add7dd801ae1a10a1a0290c50cda36d6
Scott <scottimig@gmail.com>
2019-03-15 22:48:28 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -1,37 +1,35 @@
 import numpy as np
-from PIL import Image, ImageEnhance
+from PIL import Image, ImageDraw, ImageEnhance
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-2.04, 2.04)
-y_range = (-2.04, 2.04)
+x_range = (-2.06, 2.06)
+y_range = (-1.98, 1.98)
 max_iter = 300
-p = -0.5 + 0.5j
 
-# Phoenix fractal
+# Tricorn fractal
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
 C = X + 1j * Y
 Z = np.zeros_like(C)
-Zold = np.zeros_like(C)
-phoenix = np.zeros(C.shape, dtype=int)
+tricorn = np.zeros(C.shape, dtype=int)
 mask = np.ones(C.shape, dtype=bool)
 for i in range(max_iter):
-    Z[mask], Zold[mask] = Z[mask] ** 2 + C[mask] + p * Zold[mask], Z[mask]
+    Z[mask] = np.conj(Z[mask]) ** 2 + C[mask]
     mask_new = np.abs(Z) <= 2
-    phoenix[mask & ~mask_new] = i
+    tricorn[mask & ~mask_new] = i
     mask = mask_new
 
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.8 * phoenix / max_iter + 0.1) % 1
-hsv[..., 1] = 0.7 + 0.3 * (phoenix / max_iter)
-hsv[..., 2] = (phoenix / max_iter) ** 0.7
+hsv[..., 0] = (0.2 * tricorn / max_iter + 0.8) % 1
+hsv[..., 1] = 0.9 + 0.1 * (tricorn / max_iter)
+hsv[..., 2] = (tricorn / max_iter) ** 0.5
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
 
-# Julia set as mask
-c = complex(-0.82, 0.13)
+# Julia set overlay
+c = complex(0.24, 0.03)
 Z2 = X + 1j * Y
 julia = np.zeros(Z2.shape, dtype=int)
 mask = np.ones(Z2.shape, dtype=bool)
@@ -40,21 +38,24 @@ for i in range(max_iter):
     mask_new = np.abs(Z2) <= 2
     julia[mask & ~mask_new] = i
     mask = mask_new
-alpha = (julia / max_iter * 255).astype(np.uint8)
+julia_img = (julia / max_iter * 255).astype(np.uint8)
+julia_img = Image.fromarray(np.stack([julia_img]*3, axis=-1)).convert('RGBA')
+julia_img.putalpha(80)
 img = img.convert('RGBA')
-img.putalpha(Image.fromarray(alpha))
+img = Image.alpha_composite(img, julia_img)
+img = img.convert('RGB')
 
-# Channel shuffle
+# Color cycling effect
 arr = np.array(img)
-arr[..., 0], arr[..., 1], arr[..., 2] = arr[..., 1], arr[..., 2], arr[..., 0]
-img = Image.fromarray(arr, 'RGBA')
+arr = np.roll(arr, shift=30, axis=2)
+img = Image.fromarray(arr)
 
-# Transparency gradient
-grad = np.linspace(0, 255, height).astype(np.uint8)
-grad = np.tile(grad[:, None], (1, width))
-arr = np.array(img)
-arr[..., 3] = (arr[..., 3].astype(np.float32) * grad / 255).astype(np.uint8)
-img = Image.fromarray(arr, 'RGBA').convert('RGB')
+# Geometric grid overlay
+draw = ImageDraw.Draw(img)
+for x in range(0, width, 60):
+    draw.line((x, 0, x, height), fill=(255,255,255,60), width=1)
+for y in range(0, height, 60):
+    draw.line((0, y, width, y), fill=(255,255,255,60), width=1)
 
 img = ImageEnhance.Color(img).enhance(1.7)
 
