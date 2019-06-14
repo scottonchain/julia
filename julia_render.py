6d2a3b54e20e245fda96539b14e8c24fcb89af8c
Scott <scottimig@gmail.com>
2019-06-13 19:23:21 -0600

    saving

--- julia_render.py
+++ julia_render.py
@@ -1,53 +1,45 @@
 import numpy as np
-from PIL import Image, ImageFilter, ImageEnhance, ImageDraw
-from matplotlib.colors import hsv_to_rgb
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
 width, height = 1600, 1600
-x_range = (-1.54, 1.54)
-y_range = (-1.54, 1.54)
-c = complex(0.35, 0.32)
-max_iter = 360
+# Zoomed in region for detail
+x_range = (-0.9, 0.9)
+y_range = (-0.64, 0.64)
+c = complex(-0.81, 0.16)
+max_iter = 1300
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
 Z = X + 1j * Y
 
-div_iter = np.zeros(Z.shape, dtype=int)
+iteration = np.zeros(Z.shape, dtype=int)
 mask = np.ones(Z.shape, dtype=bool)
 for i in range(max_iter):
     Z[mask] = Z[mask] ** 2 + c
     mask_new = np.abs(Z) <= 2
-    div_iter[mask & ~mask_new] = i
+    iteration[mask & ~mask_new] = i
     mask = mask_new
 
+# Smooth coloring for better detail
 with np.errstate(divide='ignore', invalid='ignore'):
-    smooth = div_iter + 1 - np.log(np.log2(np.abs(Z)))
+    smooth = iteration + 1 - np.log(np.log2(np.abs(Z)))
     smooth = np.nan_to_num(smooth)
-smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
-hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1
-hsv[..., 1] = 0.4 + 0.3 * np.abs(np.sin(2 * np.pi * smooth_norm))
-hsv[..., 2] = smooth_norm ** 0.5
+# Use bright, warm prismatic colormap
+fig, ax = plt.subplots(figsize=(8, 8), dpi=112)
+im = ax.imshow(smooth, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), 
+               origin='lower', cmap='winter', interpolation='bilinear')
+ax.set_title('Julia Set Detail (c = -0.8 + 0.156i)', fontsize=14)
+ax.set_xlabel('Re(z)', fontsize=12)
+ax.set_ylabel('Im(z)', fontsize=12)
+ax.grid(True, color='white', alpha=0.3, linestyle='--', linewidth=0.5)
 
-rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
-img = Image.fromarray(rgb)
+# Add colorbar
+cbar = plt.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
+cbar.set_label('Iteration Count (Smooth)', fontsize=12)
 
-img = img.filter(ImageFilter.GaussianBlur(radius=8))
-
-# Diamond mask overlay
-def diamond_mask(im):
-    arr = np.array(im)
-    cy, cx = arr.shape[0] // 2, arr.shape[1] // 2
-    for y in range(arr.shape[0]):
-        for x in range(arr.shape[1]):
-            if abs(x - cx) + abs(y - cy) > min(cx, cy):
-                arr[y, x] = arr[y, x] // 2
-    return Image.fromarray(arr)
-
-img = diamond_mask(img)
-img = ImageEnhance.Color(img).enhance(1.5)
-
-output_path = 'julia_output.jpg'
-img.save(output_path) 
+plt.tight_layout()
+plt.savefig('julia_output.jpg', dpi=112, bbox_inches='tight')
+plt.close() 
