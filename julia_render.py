79a3822d41394b93d88ce4fcd3ee7d68529600e8
Scott <scottimig@gmail.com>
2019-07-04 01:18:47 -0600

    saving

--- julia_render.py
+++ julia_render.py
@@ -1,68 +1,49 @@
 import numpy as np
-from PIL import Image, ImageFilter, ImageEnhance
-import matplotlib.pyplot as plt
+from PIL import Image, ImageFilter, ImageEnhance, ImageDraw
 from matplotlib.colors import hsv_to_rgb
 
-# Parameters (these will be programmatically changed by the main script)
 width, height = 1600, 1600
-x_range = (-0.75, 0.75)
-y_range = (-0.86, 0.86)
-c = complex(0.38, 0.35)
-max_iter = 320
+x_range = (-1.38, 1.38)
+y_range = (-1.43, 1.43)
+c = complex(-0.7, -0.36)
+max_iter = 340
 
-# Generate grid of complex points
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
 Z = X + 1j * Y
 
-# Initialize iteration counts and mask
 div_iter = np.zeros(Z.shape, dtype=int)
 mask = np.ones(Z.shape, dtype=bool)
-
-# Iterate and record divergence
 for i in range(max_iter):
     Z[mask] = Z[mask] ** 2 + c
     mask_new = np.abs(Z) <= 2
     div_iter[mask & ~mask_new] = i
     mask = mask_new
 
-# Smooth coloring
 with np.errstate(divide='ignore', invalid='ignore'):
     smooth = div_iter + 1 - np.log(np.log2(np.abs(Z)))
     smooth = np.nan_to_num(smooth)
 smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
-# Build HSV image with a unique color scheme
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.6 * smooth_norm + 0.3) % 1  # Brighter hue
-hsv[..., 1] = 0.95 - 0.1 * np.cos(4 * np.pi * smooth_norm)  # High saturation
-hsv[..., 2] = smooth_norm ** 0.3  # Bright value
+hsv[..., 0] = (0.45 * smooth_norm + 0.1) % 1
+hsv[..., 1] = 0.9 - 0.3 * smooth_norm
+hsv[..., 2] = smooth_norm ** 0.5
 
-# Convert to RGB
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
 
-# Circular ripple postprocessing effect
-def circular_ripple(im, freq=12, amp=8):
-    arr = np.array(im)
-    cy, cx = arr.shape[0] // 2, arr.shape[1] // 2
-    Y, X = np.ogrid[:arr.shape[0], :arr.shape[1]]
-    r = np.sqrt((Y - cy) ** 2 + (X - cx) ** 2)
-    ripple = amp * np.sin(2 * np.pi * r / freq)
-    newY = np.clip((Y + ripple).astype(int), 0, arr.shape[0] - 1)
-    newX = np.clip((X + ripple).astype(int), 0, arr.shape[1] - 1)
-    rippled = arr[newY, newX]
-    return Image.fromarray(rippled)
+img = img.filter(ImageFilter.MedianFilter(size=7))
 
-img = circular_ripple(img, freq=18, amp=10)
+def add_stripes(im, stripe_width=20):
+    draw = ImageDraw.Draw(im)
+    for x in range(0, im.width, stripe_width*2):
+        draw.rectangle([x, 0, x+stripe_width, im.height], fill=(255,255,255,40))
+    return im
 
-# Artistic postprocessing: blur, color, and detail
-blur = img.filter(ImageFilter.GaussianBlur(radius=2))
-enhanced = ImageEnhance.Color(blur).enhance(1.7)
-enhanced = ImageEnhance.Contrast(enhanced).enhance(1.3)
-enhanced = enhanced.filter(ImageFilter.DETAIL)
+img = add_stripes(img, stripe_width=30)
+img = ImageEnhance.Brightness(img).enhance(1.2)
 
-# Save output
 output_path = 'julia_output.jpg'
-enhanced.save(output_path) 
+img.save(output_path) 
