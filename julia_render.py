0a93a7c2784f86a9b7766933fb3ced061fa3ebc2
Scott <scottimig@gmail.com>
2020-01-07 18:52:29 -0700

    sync

--- julia_render.py
+++ julia_render.py
@@ -3,10 +3,10 @@ from PIL import Image, ImageFilter, ImageEnhance, ImageOps
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-0.71, 0.71)
-y_range = (-0.65, 0.65)
-c = complex(-0.41, 0.57)
-max_iter = 360
+x_range = (-0.38, 0.38)
+y_range = (-0.86, -0.06)
+c = complex(0.28, -0.57)
+max_iter = 350
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
@@ -28,23 +28,22 @@ smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
 hsv = np.zeros((height, width, 3), dtype=float)
 hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1
-hsv[..., 1] = 0.95 - 0.1 * smooth_norm
+hsv[..., 1] = 0.95 - 0.1 * np.abs(np.sin(2 * np.pi * smooth_norm))
 hsv[..., 2] = smooth_norm ** 0.2
 
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
 
-img = img.filter(ImageFilter.GaussianBlur(radius=8))
+img = ImageOps.posterize(img, 2)
 
-# Vertical split mirror
-def vertical_split_mirror(im):
+def diagonal_wave(im, amp=10, freq=0.12):
     arr = np.array(im)
-    mid = arr.shape[1] // 2
-    arr[:, mid:] = arr[:, :mid][:, ::-1]
+    for i in range(arr.shape[0]):
+        arr[i] = np.roll(arr[i], int(amp * np.sin(freq * i + freq * i)))
     return Image.fromarray(arr)
 
-img = vertical_split_mirror(img)
-img = ImageEnhance.Contrast(img).enhance(1.5)
+img = diagonal_wave(img, amp=20, freq=0.18)
+img = img.filter(ImageFilter.DETAIL)
 
 output_path = 'julia_output.jpg'
 img.save(output_path) 
