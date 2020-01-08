41ecb3ac3fa7ff084d134a6b1b5efafe1b8a08d9
Scott <scottimig@gmail.com>
2020-01-07 18:57:44 -0700

    update

--- julia_render.py
+++ julia_render.py
@@ -1,49 +1,68 @@
 import numpy as np
 from PIL import Image, ImageFilter, ImageEnhance, ImageOps
+import matplotlib.pyplot as plt
 from matplotlib.colors import hsv_to_rgb
 
+# Parameters (these will be programmatically changed by the main script)
 width, height = 1600, 1600
-x_range = (-0.38, 0.38)
-y_range = (-0.86, -0.06)
-c = complex(0.28, -0.57)
-max_iter = 350
+x_range = (-0.79, 0.79)
+y_range = (-0.79, 0.79)
+c = complex(-0.66, -0.43)
+max_iter = 500
 
+# Generate grid of complex points
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
 Z = X + 1j * Y
 
+# Initialize iteration counts and mask
 div_iter = np.zeros(Z.shape, dtype=int)
 mask = np.ones(Z.shape, dtype=bool)
+
+# Iterate and record divergence
 for i in range(max_iter):
     Z[mask] = Z[mask] ** 2 + c
     mask_new = np.abs(Z) <= 2
     div_iter[mask & ~mask_new] = i
     mask = mask_new
 
+# Smooth coloring
 with np.errstate(divide='ignore', invalid='ignore'):
     smooth = div_iter + 1 - np.log(np.log2(np.abs(Z)))
     smooth = np.nan_to_num(smooth)
 smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
+# Build HSV image with a unique color scheme
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1
-hsv[..., 1] = 0.95 - 0.1 * np.abs(np.sin(2 * np.pi * smooth_norm))
-hsv[..., 2] = smooth_norm ** 0.2
+hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1  # Brighter hue
+hsv[..., 1] = 0.95 - 0.2 * np.abs(np.cos(3 * np.pi * smooth_norm))  # High saturation
+hsv[..., 2] = smooth_norm ** 0.4  # Bright value
 
+# Convert to RGB
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
 
-img = ImageOps.posterize(img, 2)
+# Mirrored effect
+def mirror(im):
+    return ImageOps.mirror(im)
 
-def diagonal_wave(im, amp=10, freq=0.12):
-    arr = np.array(im)
-    for i in range(arr.shape[0]):
-        arr[i] = np.roll(arr[i], int(amp * np.sin(freq * i + freq * i)))
-    return Image.fromarray(arr)
+img = mirror(img)
 
-img = diagonal_wave(img, amp=20, freq=0.18)
-img = img.filter(ImageFilter.DETAIL)
+# Solarize effect
+img = ImageOps.solarize(img, threshold=128)
 
+# Artistic postprocessing: blur, color, and emboss
+blur = img.filter(ImageFilter.GaussianBlur(radius=2))
+enhanced = ImageEnhance.Color(blur).enhance(1.8)
+enhanced = enhanced.filter(ImageFilter.EMBOSS)
+
+# Save output
 output_path = 'julia_output.jpg'
-img.save(output_path) 
+enhanced.save(output_path)
+
+# Optionally display
+# plt.figure(figsize=(10, 8))
+# plt.axis('off')
+# plt.imshow(enhanced)
+# plt.show() 
