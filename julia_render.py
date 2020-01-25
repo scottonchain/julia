ab3e86a377bbaf447f261be62da984cf07e7a6d5
Scott <scottimig@gmail.com>
2020-01-24 21:41:18 -0700

    update

--- julia_render.py
+++ julia_render.py
@@ -1,40 +1,55 @@
 import numpy as np
-import matplotlib.pyplot as plt
+from PIL import Image, ImageFilter, ImageEnhance, ImageOps
+from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-1.98, 1.98)
-y_range = (-1.98, 1.98)
-c = complex(-0.73, 0.22)
-max_iter = 300
+x_range = (-0.75, 0.75)
+y_range = (-0.61, 0.61)
+c = complex(-0.77, 0.18)
+max_iter = 370
 
-x = np.linspace(x_range[0], x_range[1], width, dtype=np.float32)
-y = np.linspace(y_range[0], y_range[1], height, dtype=np.float32)
+x = np.linspace(x_range[0], x_range[1], width)
+y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
 Z = X + 1j * Y
 
-iteration = np.full(Z.shape, max_iter, dtype=np.uint16)
+div_iter = np.zeros(Z.shape, dtype=int)
 mask = np.ones(Z.shape, dtype=bool)
-escape_radius = 4.0
-
 for i in range(max_iter):
-    if not np.any(mask):
-        break
     Z[mask] = Z[mask] ** 2 + c
-    mask_new = np.abs(Z) <= escape_radius
-    iteration[mask & ~mask_new] = i
+    mask_new = np.abs(Z) <= 2
+    div_iter[mask & ~mask_new] = i
     mask = mask_new
 
 with np.errstate(divide='ignore', invalid='ignore'):
-    smooth = iteration + 1 - np.log(np.log2(np.abs(Z)))
+    smooth = div_iter + 1 - np.log(np.log2(np.abs(Z)))
     smooth = np.nan_to_num(smooth)
+smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
-fig, ax = plt.subplots(figsize=(8, 8), dpi=112)
-im = ax.imshow(smooth, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), 
-               origin='lower', cmap='plasma')
-ax.set_title('Julia Set (Optimized)', fontsize=14)
-ax.set_xlabel('Re(z)', fontsize=12)
-ax.set_ylabel('Im(z)', fontsize=12)
+hsv = np.zeros((height, width, 3), dtype=float)
+hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1
+hsv[..., 1] = 0.95 - 0.1 * smooth_norm
+hsv[..., 2] = smooth_norm ** 0.2
 
-plt.tight_layout()
-plt.savefig('julia_output.jpg', dpi=112, bbox_inches='tight')
-plt.close() 
+rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
+img = Image.fromarray(rgb)
+
+img = ImageOps.solarize(img, threshold=80)
+
+# Circular pixel sort
+def circular_pixel_sort(im):
+    arr = np.array(im)
+    cy, cx = arr.shape[0] // 2, arr.shape[1] // 2
+    for r in range(1, min(cy, cx)):
+        indices = (np.abs(np.sqrt((np.arange(arr.shape[0])[:, None] - cy) ** 2 + (np.arange(arr.shape[1]) - cx) ** 2) - r) < 1)
+        for c in range(3):
+            band = arr[..., c][indices]
+            band.sort()
+            arr[..., c][indices] = band
+    return Image.fromarray(arr)
+
+img = circular_pixel_sort(img)
+img = img.filter(ImageFilter.EDGE_ENHANCE)
+
+output_path = 'julia_output.jpg'
+img.save(output_path) 
