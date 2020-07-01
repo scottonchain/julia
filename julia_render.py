30ddd9709fd460ab5f8ee053dc8cecb854b1902d
Scott <scottimig@gmail.com>
2020-07-01 00:21:36 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -1,51 +1,58 @@
 import numpy as np
-from PIL import Image, ImageFilter, ImageEnhance
+from PIL import Image, ImageEnhance, ImageFilter
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-2.07, 2.07)
-y_range = (-2.07, 2.07)
-max_iter = 300
+x_range = (-0.67, 0.67)
+y_range = (-0.58, 0.58)
+c = complex(-0.74, 0.25)
+max_iter = 500
 
-# Burning Ship fractal
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
-C = X + 1j * Y
-Z = np.zeros_like(C)
-ship = np.zeros(C.shape, dtype=int)
-mask = np.ones(C.shape, dtype=bool)
+Z = X + 1j * Y
+
+iteration = np.zeros(Z.shape, dtype=int)
+mask = np.ones(Z.shape, dtype=bool)
+
 for i in range(max_iter):
-    Z[mask] = (np.abs(Z[mask].real) + 1j * np.abs(Z[mask].imag)) ** 2 + C[mask]
+    Z[mask] = Z[mask] ** 2 + c
     mask_new = np.abs(Z) <= 2
-    ship[mask & ~mask_new] = i
+    iteration[mask & ~mask_new] = i
     mask = mask_new
 
+# Smooth coloring
+with np.errstate(divide='ignore', invalid='ignore'):
+    smooth = iteration + 1 - np.log(np.log2(np.abs(Z)))
+    smooth = np.nan_to_num(smooth)
+smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
+
+# Bright palette
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.2 * ship / max_iter + 0.8) % 1
-hsv[..., 1] = 0.9 - 0.7 * (ship / max_iter)
-hsv[..., 2] = (ship / max_iter) ** 0.7
+hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1
+hsv[..., 1] = 0.95 - 0.1 * np.abs(np.sin(2 * np.pi * smooth_norm))
+hsv[..., 2] = smooth_norm ** 0.2
+
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
+
+# Create PIL image and apply enhancements
 img = Image.fromarray(rgb)
 
-# Julia set as transparency mask
-c = complex(-0.8, 0.15)
-Z2 = X + 1j * Y
-julia = np.zeros(Z2.shape, dtype=int)
-mask = np.ones(Z2.shape, dtype=bool)
-for i in range(max_iter):
-    Z2[mask] = Z2[mask] ** 2 + c
-    mask_new = np.abs(Z2) <= 2
-    julia[mask & ~mask_new] = i
-    mask = mask_new
-alpha = (julia / max_iter * 255).astype(np.uint8)
-img = img.convert('RGBA')
-img.putalpha(Image.fromarray(alpha))
+# Apply PIL filters and enhancements
+img = img.filter(ImageFilter.EDGE_ENHANCE_MORE)
+img = img.filter(ImageFilter.SMOOTH_MORE)
 
-# Motion blur effect
-img = img.filter(ImageFilter.GaussianBlur(radius=2)).filter(ImageFilter.BoxBlur(3))
-img = img.convert('RGB')
-img = ImageEnhance.Color(img).enhance(2.1)
+# Enhance colors and contrast
+enhancer = ImageEnhance.Color(img)
+img = enhancer.enhance(1.5)
 
+enhancer = ImageEnhance.Contrast(img)
+img = enhancer.enhance(1.3)
+
+enhancer = ImageEnhance.Brightness(img)
+img = enhancer.enhance(1.1)
+
+# Save output
 output_path = 'julia_output.jpg'
-img.save(output_path) 
+img.save(output_path, quality=95) 
