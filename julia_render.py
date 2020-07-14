50eb07652a7913c2f522195209724aec3567f04d
Scott <scottimig@gmail.com>
2020-07-14 07:53:31 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -1,55 +1,58 @@
 import numpy as np
-from PIL import Image, ImageFilter, ImageEnhance, ImageDraw
+from PIL import Image, ImageEnhance, ImageFilter
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-1.41, 1.41)
-y_range = (-1.58, 1.58)
-c = complex(-0.84, 0.16)
-max_iter = 400
+x_range = (-0.77, 0.77)
+y_range = (-0.71, 0.71)
+c = complex(-0.68, 0.28)
+max_iter = 500
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
 Z = X + 1j * Y
 
-div_iter = np.zeros(Z.shape, dtype=int)
+iteration = np.zeros(Z.shape, dtype=int)
 mask = np.ones(Z.shape, dtype=bool)
+
 for i in range(max_iter):
     Z[mask] = Z[mask] ** 2 + c
     mask_new = np.abs(Z) <= 2
-    div_iter[mask & ~mask_new] = i
+    iteration[mask & ~mask_new] = i
     mask = mask_new
 
+# Smooth coloring
 with np.errstate(divide='ignore', invalid='ignore'):
-    smooth = div_iter + 1 - np.log(np.log2(np.abs(Z)))
+    smooth = iteration + 1 - np.log(np.log2(np.abs(Z)))
     smooth = np.nan_to_num(smooth)
 smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
-# Deep blue palette
+# Bright palette
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = 0.6 + 0.1 * smooth_norm
-hsv[..., 1] = 0.7 + 0.3 * smooth_norm
-hsv[..., 2] = smooth_norm ** 0.8
+hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1
+hsv[..., 1] = 0.95 - 0.1 * np.abs(np.sin(2 * np.pi * smooth_norm))
+hsv[..., 2] = smooth_norm ** 0.2
 
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
+
+# Create PIL image and apply enhancements
 img = Image.fromarray(rgb)
 
-img = img.filter(ImageFilter.GaussianBlur(radius=10))
+# Apply PIL filters and enhancements
+img = img.filter(ImageFilter.EDGE_ENHANCE_MORE)
+img = img.filter(ImageFilter.SMOOTH_MORE)
 
-# Radial gradient overlay
-def radial_gradient(im):
-    arr = np.array(im).astype(np.float32)
-    cy, cx = arr.shape[0] // 2, arr.shape[1] // 2
-    Y, X = np.ogrid[:arr.shape[0], :arr.shape[1]]
-    r = np.sqrt((Y - cy) ** 2 + (X - cx) ** 2)
-    mask = r / r.max()
-    for c in range(3):
-        arr[..., c] = arr[..., c] * (1 - 0.5 * mask)
-    return Image.fromarray(arr.astype(np.uint8))
+# Enhance colors and contrast
+enhancer = ImageEnhance.Color(img)
+img = enhancer.enhance(1.5)
 
-img = radial_gradient(img)
-img = ImageEnhance.Brightness(img).enhance(1.1)
+enhancer = ImageEnhance.Contrast(img)
+img = enhancer.enhance(1.3)
 
+enhancer = ImageEnhance.Brightness(img)
+img = enhancer.enhance(1.1)
+
+# Save output
 output_path = 'julia_output.jpg'
-img.save(output_path) 
+img.save(output_path, quality=95) 
