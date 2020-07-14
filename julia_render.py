676472d2e2a5a994b8c71af032c8fd184771be9b
Scott <scottimig@gmail.com>
2020-07-14 09:05:53 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -1,12 +1,13 @@
 import numpy as np
-from PIL import Image, ImageFilter, ImageEnhance, ImageOps
+from PIL import Image, ImageFilter, ImageEnhance
+import matplotlib.pyplot as plt
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-0.76, 0.76)
-y_range = (-0.76, 0.76)
-c = complex(0.41, -0.23)
-max_iter = 380
+x_range = (-1.62, 1.62)
+y_range = (-1.62, 1.62)
+c = complex(-0.83, 0.19)
+max_iter = 370
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
@@ -15,6 +16,7 @@ Z = X + 1j * Y
 
 div_iter = np.zeros(Z.shape, dtype=int)
 mask = np.ones(Z.shape, dtype=bool)
+
 for i in range(max_iter):
     Z[mask] = Z[mask] ** 2 + c
     mask_new = np.abs(Z) <= 2
@@ -27,24 +29,28 @@ with np.errstate(divide='ignore', invalid='ignore'):
 smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1
-hsv[..., 1] = 0.95 - 0.1 * smooth_norm
-hsv[..., 2] = smooth_norm ** 0.2
+hsv[..., 0] = (0.5 * smooth_norm + 0.3) % 1
+hsv[..., 1] = 0.95 - 0.3 * np.abs(np.sin(2 * np.pi * smooth_norm))
+hsv[..., 2] = smooth_norm ** 0.4
 
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
 
-img = img.filter(ImageFilter.FIND_EDGES)
-
-# Horizontal flip every 100 pixels
-def flip_blocks(im, block=100):
+def glass_distort(im, scale=6):
     arr = np.array(im)
-    for i in range(0, arr.shape[0], block*2):
-        arr[i:i+block] = arr[i:i+block][::-1]
-    return Image.fromarray(arr)
+    dx = (np.random.rand(*arr.shape[:2]) - 0.5) * scale
+    dy = (np.random.rand(*arr.shape[:2]) - 0.5) * scale
+    Y, X = np.meshgrid(np.arange(arr.shape[0]), np.arange(arr.shape[1]), indexing='ij')
+    Xn = np.clip((X + dx).astype(int), 0, arr.shape[1] - 1)
+    Yn = np.clip((Y + dy).astype(int), 0, arr.shape[0] - 1)
+    distorted = arr[Yn, Xn]
+    return Image.fromarray(distorted)
 
-img = flip_blocks(img, block=100)
-img = ImageEnhance.Contrast(img).enhance(1.8)
+img = glass_distort(img, scale=8)
+
+blur = img.filter(ImageFilter.GaussianBlur(radius=1))
+enhanced = ImageEnhance.Color(blur).enhance(2.2)
+enhanced = ImageEnhance.Contrast(enhanced).enhance(1.5)
 
 output_path = 'julia_output.jpg'
-img.save(output_path) 
+enhanced.save(output_path) 
