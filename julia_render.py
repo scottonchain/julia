3247a4f5211877884475186dd2147e2dbde73681
Scott <scottimig@gmail.com>
2020-08-30 23:43:16 -0600

    sync

--- julia_render.py
+++ julia_render.py
@@ -1,33 +1,61 @@
 import numpy as np
-import matplotlib.pyplot as plt
+from PIL import Image, ImageFilter, ImageEnhance, ImageOps
+from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-1.06, 1.06)
-y_range = (-0.94, 0.94)
-c = complex(-0.71, 0.25)
-max_iter = 300
+x_range = (-0.76, 0.76)
+y_range = (-0.95, 0.95)
+c = complex(-0.61, 0.49)
+max_iter = 320
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
 Z = X + 1j * Y
 
-iteration = np.zeros(Z.shape, dtype=int)
+div_iter = np.zeros(Z.shape, dtype=int)
 mask = np.ones(Z.shape, dtype=bool)
-
 for i in range(max_iter):
     Z[mask] = Z[mask] ** 2 + c
     mask_new = np.abs(Z) <= 2
-    iteration[mask & ~mask_new] = i
+    div_iter[mask & ~mask_new] = i
     mask = mask_new
 
-fig, ax = plt.subplots(figsize=(8, 8), dpi=112)
-im = ax.imshow(iteration, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), 
-               origin='lower', cmap='hsv')
-ax.set_title('Julia Set (Bright HSV)', fontsize=14)
-ax.set_xlabel('Re(z)', fontsize=12)
-ax.set_ylabel('Im(z)', fontsize=12)
+with np.errstate(divide='ignore', invalid='ignore'):
+    smooth = div_iter + 1 - np.log(np.log2(np.abs(Z)))
+    smooth = np.nan_to_num(smooth)
+smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
-plt.tight_layout()
-plt.savefig('julia_output.jpg', dpi=112, bbox_inches='tight')
-plt.close() 
+hsv = np.zeros((height, width, 3), dtype=float)
+hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1
+hsv[..., 1] = 0.95 - 0.1 * np.abs(np.sin(2 * np.pi * smooth_norm))
+hsv[..., 2] = smooth_norm ** 0.2
+
+rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
+img = Image.fromarray(rgb)
+
+# Sepia tone
+def sepia(im):
+    arr = np.array(im).astype(np.float32)
+    r, g, b = arr[..., 0], arr[..., 1], arr[..., 2]
+    tr = 0.393 * r + 0.769 * g + 0.189 * b
+    tg = 0.349 * r + 0.686 * g + 0.168 * b
+    tb = 0.272 * r + 0.534 * g + 0.131 * b
+    arr[..., 0] = np.clip(tr, 0, 255)
+    arr[..., 1] = np.clip(tg, 0, 255)
+    arr[..., 2] = np.clip(tb, 0, 255)
+    return Image.fromarray(arr.astype(np.uint8))
+
+img = sepia(img)
+img = img.filter(ImageFilter.GaussianBlur(radius=6))
+
+def vertical_wave(im, amp=12, freq=0.09):
+    arr = np.array(im)
+    for j in range(arr.shape[1]):
+        arr[:, j] = np.roll(arr[:, j], int(amp * np.sin(freq * j)))
+    return Image.fromarray(arr)
+
+img = vertical_wave(img, amp=18, freq=0.13)
+
+output_path = 'julia_output.jpg'
+img.save(output_path) 
