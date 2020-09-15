55f08a8da33042930509607104d2c0876c37c86d
Scott <scottimig@gmail.com>
2020-09-15 11:22:20 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -1,40 +1,51 @@
 import numpy as np
-import matplotlib.pyplot as plt
+from PIL import Image, ImageFilter, ImageEnhance, ImageOps
+from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-2.01, 2.01)
-y_range = (-2.01, 2.01)
-c = complex(-0.74, 0.24)
-max_iter = 300
+x_range = (-1.51, 1.51)
+y_range = (-1.51, 1.51)
+c = complex(-0.8, 0.14)
+max_iter = 370
 
-x = np.linspace(x_range[0], x_range[1], width, dtype=np.float32)
-y = np.linspace(y_range[0], y_range[1], height, dtype=np.float32)
+x = np.linspace(x_range[0], x_range[1], width)
+y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
 Z = X + 1j * Y
 
-iteration = np.full(Z.shape, max_iter, dtype=np.uint16)
+div_iter = np.zeros(Z.shape, dtype=int)
 mask = np.ones(Z.shape, dtype=bool)
-escape_radius = 4.0
-
 for i in range(max_iter):
-    if not np.any(mask):
-        break
     Z[mask] = Z[mask] ** 2 + c
-    mask_new = np.abs(Z) <= escape_radius
-    iteration[mask & ~mask_new] = i
+    mask_new = np.abs(Z) <= 2
+    div_iter[mask & ~mask_new] = i
     mask = mask_new
 
 with np.errstate(divide='ignore', invalid='ignore'):
-    smooth = iteration + 1 - np.log(np.log2(np.abs(Z)))
+    smooth = div_iter + 1 - np.log(np.log2(np.abs(Z)))
     smooth = np.nan_to_num(smooth)
+smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
-fig, ax = plt.subplots(figsize=(8, 8), dpi=112)
-im = ax.imshow(smooth, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), 
-               origin='lower', cmap='plasma')
-ax.set_title('Julia Set (Optimized)', fontsize=14)
-ax.set_xlabel('Re(z)', fontsize=12)
-ax.set_ylabel('Im(z)', fontsize=12)
+# Green-magenta palette
+hsv = np.zeros((height, width, 3), dtype=float)
+hsv[..., 0] = (0.4 * smooth_norm + 0.7) % 1
+hsv[..., 1] = 0.9 - 0.7 * smooth_norm
+hsv[..., 2] = smooth_norm ** 0.7
 
-plt.tight_layout()
-plt.savefig('julia_output.jpg', dpi=112, bbox_inches='tight')
-plt.close() 
+rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
+img = Image.fromarray(rgb)
+
+img = ImageOps.solarize(img, threshold=80)
+
+# Kaleidoscope effect
+def kaleidoscope(im):
+    arr = np.array(im)
+    arr = np.concatenate([arr, arr[:, ::-1]], axis=1)
+    arr = np.concatenate([arr, arr[::-1, :]], axis=0)
+    return Image.fromarray(arr)
+
+img = kaleidoscope(img)
+img = ImageEnhance.Color(img).enhance(1.8)
+
+output_path = 'julia_output.jpg'
+img.save(output_path) 
