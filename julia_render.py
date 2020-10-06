0289d5186c093c37879973d7dad47041b06e5982
Scott <scottimig@gmail.com>
2020-10-06 15:57:56 -0600

    saving

--- julia_render.py
+++ julia_render.py
@@ -1,51 +1,59 @@
 import numpy as np
-from PIL import Image, ImageFilter, ImageEnhance, ImageOps
+import matplotlib.pyplot as plt
 from matplotlib.colors import hsv_to_rgb
 
+# Parameters for a scientific Julia set plot
 width, height = 1600, 1600
-x_range = (-1.51, 1.51)
-y_range = (-1.51, 1.51)
-c = complex(-0.8, 0.14)
-max_iter = 370
+x_range = (-1.56, 1.56)
+y_range = (-1.56, 1.56)
+c = complex(-0.69, 0.26)
+max_iter = 400
 
+# Generate grid
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
 Z = X + 1j * Y
 
-div_iter = np.zeros(Z.shape, dtype=int)
+# Julia set calculation
+iteration = np.zeros(Z.shape, dtype=int)
 mask = np.ones(Z.shape, dtype=bool)
 for i in range(max_iter):
     Z[mask] = Z[mask] ** 2 + c
     mask_new = np.abs(Z) <= 2
-    div_iter[mask & ~mask_new] = i
+    iteration[mask & ~mask_new] = i
     mask = mask_new
 
+# Smooth coloring
 with np.errstate(divide='ignore', invalid='ignore'):
-    smooth = div_iter + 1 - np.log(np.log2(np.abs(Z)))
+    smooth = iteration + 1 - np.log(np.log2(np.abs(Z)))
     smooth = np.nan_to_num(smooth)
 smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
-# Green-magenta palette
+# HSV colormap for scientific look
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.4 * smooth_norm + 0.7) % 1
-hsv[..., 1] = 0.9 - 0.7 * smooth_norm
-hsv[..., 2] = smooth_norm ** 0.7
+hsv[..., 0] = (0.6 * smooth_norm + 0.2) % 1
+hsv[..., 1] = 0.8
+hsv[..., 2] = smooth_norm ** 0.8
+rgb = hsv_to_rgb(hsv)
 
-rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
-img = Image.fromarray(rgb)
+# Plot with scientific style
+fig, ax = plt.subplots(figsize=(8, 8), dpi=112)
+im = ax.imshow(rgb, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), origin='lower')
+ax.set_title('Julia Set (c = -0.7 + 0.27015i)', fontsize=14)
+ax.set_xlabel('Re(z)', fontsize=12)
+ax.set_ylabel('Im(z)', fontsize=12)
+ax.grid(True, color='white', alpha=0.2, linestyle='--', linewidth=0.5)
 
-img = ImageOps.solarize(img, threshold=80)
+# Add colorbar
+from matplotlib.cm import ScalarMappable
+from matplotlib.colors import Normalize
+sm = ScalarMappable(cmap='hsv', norm=Normalize(vmin=smooth_norm.min(), vmax=smooth_norm.max()))
+sm.set_array([])
+cbar = plt.colorbar(sm, ax=ax, fraction=0.046, pad=0.04)
+cbar.set_label('Normalized Iteration (Smooth)', fontsize=12)
 
-# Kaleidoscope effect
-def kaleidoscope(im):
-    arr = np.array(im)
-    arr = np.concatenate([arr, arr[:, ::-1]], axis=1)
-    arr = np.concatenate([arr, arr[::-1, :]], axis=0)
-    return Image.fromarray(arr)
-
-img = kaleidoscope(img)
-img = ImageEnhance.Color(img).enhance(1.8)
-
-output_path = 'julia_output.jpg'
-img.save(output_path) 
+# Save as scientific-looking chart
+plt.tight_layout()
+plt.savefig('julia_output.jpg', dpi=112, bbox_inches='tight')
+plt.close() 
