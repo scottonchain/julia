b1ae560bec29b6ba218bcfbf03157252a42a1966
Scott <scottimig@gmail.com>
2021-04-22 23:00:28 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -1,52 +1,54 @@
 import numpy as np
-from PIL import Image, ImageFilter, ImageEnhance, ImageOps
+import matplotlib.pyplot as plt
+from scipy import ndimage
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-1.71, 1.71)
-y_range = (-1.71, 1.71)
-c = complex(0.35, -0.21)
-max_iter = 380
+x_range = (-0.78, 0.78)
+y_range = (-0.61, 0.61)
+c = complex(-0.69, 0.24)
+max_iter = 500
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
 Z = X + 1j * Y
 
-div_iter = np.zeros(Z.shape, dtype=int)
+iteration = np.zeros(Z.shape, dtype=int)
 mask = np.ones(Z.shape, dtype=bool)
+
 for i in range(max_iter):
     Z[mask] = Z[mask] ** 2 + c
     mask_new = np.abs(Z) <= 2
-    div_iter[mask & ~mask_new] = i
+    iteration[mask & ~mask_new] = i
     mask = mask_new
 
+# Apply scipy filters for enhancement
+iteration_smooth = ndimage.gaussian_filter(iteration.astype(float), sigma=0.5)
+iteration_enhanced = ndimage.uniform_filter(iteration_smooth, size=2)
+
+# Smooth coloring
 with np.errstate(divide='ignore', invalid='ignore'):
-    smooth = div_iter + 1 - np.log(np.log2(np.abs(Z)))
+    smooth = iteration_enhanced + 1 - np.log(np.log2(np.abs(Z)))
     smooth = np.nan_to_num(smooth)
 smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
-# Metallic palette
+# Bright palette
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.1 * smooth_norm + 0.6) % 1
-hsv[..., 1] = 0.2 + 0.8 * np.abs(np.cos(3 * np.pi * smooth_norm))
-hsv[..., 2] = 0.7 + 0.3 * np.abs(np.sin(2 * np.pi * smooth_norm))
+hsv[..., 0] = (0.6 * smooth_norm + 0.3) % 1
+hsv[..., 1] = 0.95 - 0.1 * np.abs(np.sin(2 * np.pi * smooth_norm))
+hsv[..., 2] = smooth_norm ** 0.2
 
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
-img = Image.fromarray(rgb)
 
-img = ImageOps.posterize(img, 2)
+fig, ax = plt.subplots(figsize=(8, 8), dpi=112)
+im = ax.imshow(rgb, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), 
+               origin='lower')
+ax.set_title('Julia Set (SciPy Enhanced)', fontsize=14)
+ax.set_xlabel('Re(z)', fontsize=12)
+ax.set_ylabel('Im(z)', fontsize=12)
+ax.grid(True, color='white', alpha=0.3, linestyle='--', linewidth=0.5)
 
-# Glass tile effect
-def glass_tile(im, tile=30):
-    arr = np.array(im)
-    for i in range(0, arr.shape[0], tile):
-        for j in range(0, arr.shape[1], tile):
-            arr[i:i+tile, j:j+tile] = np.flipud(np.fliplr(arr[i:i+tile, j:j+tile]))
-    return Image.fromarray(arr)
-
-img = glass_tile(img, tile=40)
-img = ImageEnhance.Contrast(img).enhance(1.6)
-
-output_path = 'julia_output.jpg'
-img.save(output_path) 
+plt.tight_layout()
+plt.savefig('julia_output.jpg', dpi=112, bbox_inches='tight')
+plt.close() 
