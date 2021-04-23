cc523c8ba5b6aabaea3478c405bf38f749d35517
Scott <scottimig@gmail.com>
2021-04-23 01:51:55 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -1,59 +1,60 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from numba import jit
+from PIL import Image, ImageEnhance
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-0.71, 0.71)
-y_range = (-0.62, 0.62)
-c = complex(-0.67, 0.25)
-max_iter = 500
+x_range = (-1.54, 1.54)
+y_range = (-1.54, 1.54)
+max_iter = 300
 
-@jit(nopython=True)
-def julia_numba(x_range, y_range, width, height, c, max_iter):
-    x = np.linspace(x_range[0], x_range[1], width)
-    y = np.linspace(y_range[0], y_range[1], height)
-    
-    result = np.zeros((height, width), dtype=np.int32)
-    
-    for i in range(height):
-        for j in range(width):
-            z = complex(x[j], y[i])
-            for k in range(max_iter):
-                z = z * z + c
-                if abs(z) > 2:
-                    result[i, j] = k
-                    break
-            else:
-                result[i, j] = max_iter
-    
-    return result
+# Multibrot (power 3)
+x = np.linspace(x_range[0], x_range[1], width)
+y = np.linspace(y_range[0], y_range[1], height)
+X, Y = np.meshgrid(x, y)
+C = X + 1j * Y
+Z = np.zeros_like(C)
+multi = np.zeros(C.shape, dtype=int)
+mask = np.ones(C.shape, dtype=bool)
+for i in range(max_iter):
+    Z[mask] = Z[mask] ** 3 + C[mask]
+    mask_new = np.abs(Z) <= 2
+    multi[mask & ~mask_new] = i
+    mask = mask_new
 
-# Generate fractal using Numba
-iteration = julia_numba(x_range, y_range, width, height, c, max_iter)
-
-# Smooth coloring
-with np.errstate(divide='ignore', invalid='ignore'):
-    smooth = iteration + 1 - np.log(np.log2(np.abs(complex(x_range[0], y_range[0]))))
-    smooth = np.nan_to_num(smooth)
-smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
-
-# Bright palette
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1
-hsv[..., 1] = 0.95 - 0.1 * np.abs(np.sin(2 * np.pi * smooth_norm))
-hsv[..., 2] = smooth_norm ** 0.2
-
+hsv[..., 0] = (0.6 * multi / max_iter + 0.2) % 1
+hsv[..., 1] = 0.5 + 0.5 * (multi / max_iter)
+hsv[..., 2] = (multi / max_iter) ** 0.8
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
+img = Image.fromarray(rgb)
 
-fig, ax = plt.subplots(figsize=(8, 8), dpi=112)
-im = ax.imshow(rgb, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), 
-               origin='lower')
-ax.set_title('Julia Set (Numba Accelerated)', fontsize=14)
-ax.set_xlabel('Re(z)', fontsize=12)
-ax.set_ylabel('Im(z)', fontsize=12)
-ax.grid(True, color='white', alpha=0.3, linestyle='--', linewidth=0.5)
+# Julia set as mask
+c = complex(-0.83, 0.13)
+Z2 = X + 1j * Y
+julia = np.zeros(Z2.shape, dtype=int)
+mask = np.ones(Z2.shape, dtype=bool)
+for i in range(max_iter):
+    Z2[mask] = Z2[mask] ** 2 + c
+    mask_new = np.abs(Z2) <= 2
+    julia[mask & ~mask_new] = i
+    mask = mask_new
+alpha = (julia / max_iter * 255).astype(np.uint8)
+img = img.convert('RGBA')
+img.putalpha(Image.fromarray(alpha))
 
-plt.tight_layout()
-plt.savefig('julia_output.jpg', dpi=112, bbox_inches='tight')
-plt.close() 
+# Channel mixing
+arr = np.array(img)
+arr[..., 0], arr[..., 1], arr[..., 2] = arr[..., 2], arr[..., 0], arr[..., 1]
+img = Image.fromarray(arr, 'RGBA')
+
+# Transparency gradient
+grad = np.linspace(0, 255, height).astype(np.uint8)
+grad = np.tile(grad[:, None], (1, width))
+arr = np.array(img)
+arr[..., 3] = (arr[..., 3].astype(np.float32) * grad / 255).astype(np.uint8)
+img = Image.fromarray(arr, 'RGBA').convert('RGB')
+
+img = ImageEnhance.Color(img).enhance(1.5)
+
+output_path = 'julia_output.jpg'
+img.save(output_path) 
