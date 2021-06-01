77fa1632af840e3fc8f6283ae27c48b9328394ed
Scott <scottimig@gmail.com>
2021-06-01 09:38:20 -0600

    saving

--- julia_render.py
+++ julia_render.py
@@ -1,12 +1,12 @@
 import numpy as np
-from PIL import Image, ImageFilter, ImageEnhance, ImageOps
+from PIL import Image, ImageFilter, ImageEnhance, ImageDraw
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-1.85, 1.85)
-y_range = (-1.85, 1.85)
-c = complex(-0.7, -0.34)
-max_iter = 350
+x_range = (-1.48, 1.48)
+y_range = (-1.58, 1.58)
+c = complex(-0.78, 0.15)
+max_iter = 400
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
@@ -26,34 +26,30 @@ with np.errstate(divide='ignore', invalid='ignore'):
     smooth = np.nan_to_num(smooth)
 smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
-# Pastel palette
+# Deep blue palette
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1
-hsv[..., 1] = 0.4 + 0.3 * np.abs(np.sin(2 * np.pi * smooth_norm))
-hsv[..., 2] = smooth_norm ** 0.5
+hsv[..., 0] = 0.6 + 0.1 * smooth_norm
+hsv[..., 1] = 0.7 + 0.3 * smooth_norm
+hsv[..., 2] = smooth_norm ** 0.8
 
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
 
-# Heavy pixelation
-def pixelate(im, block=20):
-    arr = np.array(im)
-    for i in range(0, arr.shape[0], block):
-        for j in range(0, arr.shape[1], block):
-            arr[i:i+block, j:j+block] = arr[i, j]
-    return Image.fromarray(arr)
+img = img.filter(ImageFilter.GaussianBlur(radius=10))
 
-img = pixelate(img, block=30)
+# Radial gradient overlay
+def radial_gradient(im):
+    arr = np.array(im).astype(np.float32)
+    cy, cx = arr.shape[0] // 2, arr.shape[1] // 2
+    Y, X = np.ogrid[:arr.shape[0], :arr.shape[1]]
+    r = np.sqrt((Y - cy) ** 2 + (X - cx) ** 2)
+    mask = r / r.max()
+    for c in range(3):
+        arr[..., c] = arr[..., c] * (1 - 0.5 * mask)
+    return Image.fromarray(arr.astype(np.uint8))
 
-# Vertical split mirror
-def vertical_split_mirror(im):
-    arr = np.array(im)
-    mid = arr.shape[1] // 2
-    arr[:, mid:] = arr[:, :mid][:, ::-1]
-    return Image.fromarray(arr)
-
-img = vertical_split_mirror(img)
-img = ImageEnhance.Color(img).enhance(1.5)
+img = radial_gradient(img)
+img = ImageEnhance.Brightness(img).enhance(1.1)
 
 output_path = 'julia_output.jpg'
 img.save(output_path) 
