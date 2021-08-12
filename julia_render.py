3048b882188932c84fbccf4500e711d6bc4c717e
Scott <scottimig@gmail.com>
2021-08-12 14:45:20 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -1,60 +1,52 @@
 import numpy as np
-from PIL import Image, ImageEnhance
+from PIL import Image, ImageFilter, ImageEnhance, ImageOps
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-1.46, 1.46)
-y_range = (-1.54, 1.54)
-max_iter = 300
+x_range = (-1.62, 1.62)
+y_range = (-1.62, 1.62)
+c = complex(0.32, 0.34)
+max_iter = 340
 
-# Multibrot (power 3)
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
-C = X + 1j * Y
-Z = np.zeros_like(C)
-multi = np.zeros(C.shape, dtype=int)
-mask = np.ones(C.shape, dtype=bool)
+Z = X + 1j * Y
+
+div_iter = np.zeros(Z.shape, dtype=int)
+mask = np.ones(Z.shape, dtype=bool)
 for i in range(max_iter):
-    Z[mask] = Z[mask] ** 3 + C[mask]
+    Z[mask] = Z[mask] ** 2 + c
     mask_new = np.abs(Z) <= 2
-    multi[mask & ~mask_new] = i
+    div_iter[mask & ~mask_new] = i
     mask = mask_new
 
+with np.errstate(divide='ignore', invalid='ignore'):
+    smooth = div_iter + 1 - np.log(np.log2(np.abs(Z)))
+    smooth = np.nan_to_num(smooth)
+smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
+
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.6 * multi / max_iter + 0.2) % 1
-hsv[..., 1] = 0.5 + 0.5 * (multi / max_iter)
-hsv[..., 2] = (multi / max_iter) ** 0.8
+hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1
+hsv[..., 1] = 0.9 + 0.1 * np.abs(np.sin(2 * np.pi * smooth_norm))
+hsv[..., 2] = smooth_norm ** 0.4
+
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
 
-# Julia set as mask
-c = complex(-0.8, 0.18)
-Z2 = X + 1j * Y
-julia = np.zeros(Z2.shape, dtype=int)
-mask = np.ones(Z2.shape, dtype=bool)
-for i in range(max_iter):
-    Z2[mask] = Z2[mask] ** 2 + c
-    mask_new = np.abs(Z2) <= 2
-    julia[mask & ~mask_new] = i
-    mask = mask_new
-alpha = (julia / max_iter * 255).astype(np.uint8)
-img = img.convert('RGBA')
-img.putalpha(Image.fromarray(alpha))
+img = img.filter(ImageFilter.EMBOSS)
 
-# Channel mixing
-arr = np.array(img)
-arr[..., 0], arr[..., 1], arr[..., 2] = arr[..., 2], arr[..., 0], arr[..., 1]
-img = Image.fromarray(arr, 'RGBA')
+# Checkerboard mask overlay
+def checkerboard(im, size=40):
+    arr = np.array(im)
+    for i in range(0, arr.shape[0], size):
+        for j in range(0, arr.shape[1], size):
+            if (i // size + j // size) % 2 == 0:
+                arr[i:i+size, j:j+size] = arr[i:i+size, j:j+size] // 2
+    return Image.fromarray(arr)
 
-# Transparency gradient
-grad = np.linspace(0, 255, height).astype(np.uint8)
-grad = np.tile(grad[:, None], (1, width))
-arr = np.array(img)
-arr[..., 3] = (arr[..., 3].astype(np.float32) * grad / 255).astype(np.uint8)
-img = Image.fromarray(arr, 'RGBA').convert('RGB')
-
-img = ImageEnhance.Color(img).enhance(1.5)
+img = checkerboard(img, size=50)
+img = ImageEnhance.Color(img).enhance(1.7)
 
 output_path = 'julia_output.jpg'
 img.save(output_path) 
