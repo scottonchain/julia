01ca62e4ec1155be066e1e804ba46f13153c148f
Scott <scottimig@gmail.com>
2021-08-12 19:44:17 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-1.1, 1.1)
-y_range = (-0.82, 0.82)
-c = complex(-0.73, 0.32)
+x_range = (-1.05, 1.05)
+y_range = (-0.88, 0.88)
+c = complex(-0.69, 0.24)
 max_iter = 300
 
 x = np.linspace(x_range[0], x_range[1], width)
@@ -28,14 +28,14 @@ with np.errstate(divide='ignore', invalid='ignore'):
     smooth = np.nan_to_num(smooth)
 smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
-# Bright autumn palette
+# Bright spring palette
 from matplotlib import cm
-rgb = cm.autumn(smooth_norm)[..., :3]
+rgb = cm.spring(smooth_norm)[..., :3]
 
 fig, ax = plt.subplots(figsize=(8, 8), dpi=112)
 im = ax.imshow(rgb, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), 
                origin='lower')
-ax.set_title('Julia Set (Bright Autumn)', fontsize=14)
+ax.set_title('Julia Set (Bright Spring)', fontsize=14)
 ax.set_xlabel('Re(z)', fontsize=12)
 ax.set_ylabel('Im(z)', fontsize=12)
 
