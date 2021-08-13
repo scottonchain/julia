91af1c1f3c9f1b7468903e6aeec98762116daa96
Scott <scottimig@gmail.com>
2021-08-12 20:27:13 -0600

    saving

--- julia_render.py
+++ julia_render.py
@@ -3,10 +3,10 @@ from PIL import Image, ImageFilter, ImageEnhance
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-1.19, 1.19)
-y_range = (-1.19, 1.19)
-c = complex(-0.54, 0.5)
-max_iter = 300
+x_range = (-1.97, 1.97)
+y_range = (-1.97, 1.97)
+c = complex(0.24, -0.01)
+max_iter = 400
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
@@ -27,23 +27,27 @@ with np.errstate(divide='ignore', invalid='ignore'):
 smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.7 * smooth_norm + 0.3) % 1
-hsv[..., 1] = 1.0 - 0.6 * smooth_norm
-hsv[..., 2] = smooth_norm ** 0.8
+hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1
+hsv[..., 1] = 0.95 - 0.1 * smooth_norm
+hsv[..., 2] = smooth_norm ** 0.2
 
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
 
-# Pixel sorting effect
-def pixel_sort(im):
+img = img.filter(ImageFilter.GaussianBlur(radius=7))
+
+# Spiral mask overlay
+def spiral_mask(im):
     arr = np.array(im)
-    for row in arr:
-        row.sort(axis=0)
+    cy, cx = arr.shape[0] // 2, arr.shape[1] // 2
+    Y, X = np.ogrid[:arr.shape[0], :arr.shape[1]]
+    theta = np.arctan2(Y - cy, X - cx)
+    mask = ((theta + np.sqrt((Y-cy)**2 + (X-cx)**2)/40) % (2*np.pi) < np.pi)
+    arr[mask] = arr[mask] // 2
     return Image.fromarray(arr)
 
-img = pixel_sort(img)
-enhanced = ImageEnhance.Color(img).enhance(2.5)
-enhanced = ImageEnhance.Brightness(enhanced).enhance(1.3)
+img = spiral_mask(img)
+img = ImageEnhance.Contrast(img).enhance(1.6)
 
 output_path = 'julia_output.jpg'
-enhanced.save(output_path) 
+img.save(output_path) 
