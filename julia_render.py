5ef88bb583fe5df3c9865f657611af2e6da686ce
Scott <scottimig@gmail.com>
2021-09-22 15:13:47 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -1,11 +1,10 @@
 import numpy as np
-from PIL import Image, ImageFilter, ImageEnhance
-from matplotlib.colors import hsv_to_rgb
+import matplotlib.pyplot as plt
 
 width, height = 1600, 1600
-x_range = (-1.23, 1.23)
-y_range = (-1.23, 1.23)
-c = complex(-0.52, 0.59)
+x_range = (-1.1, 1.1)
+y_range = (-0.86, 0.86)
+c = complex(-0.73, 0.24)
 max_iter = 300
 
 x = np.linspace(x_range[0], x_range[1], width)
@@ -13,37 +12,22 @@ y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
 Z = X + 1j * Y
 
-div_iter = np.zeros(Z.shape, dtype=int)
+iteration = np.zeros(Z.shape, dtype=int)
 mask = np.ones(Z.shape, dtype=bool)
+
 for i in range(max_iter):
     Z[mask] = Z[mask] ** 2 + c
     mask_new = np.abs(Z) <= 2
-    div_iter[mask & ~mask_new] = i
+    iteration[mask & ~mask_new] = i
     mask = mask_new
 
-with np.errstate(divide='ignore', invalid='ignore'):
-    smooth = div_iter + 1 - np.log(np.log2(np.abs(Z)))
-    smooth = np.nan_to_num(smooth)
-smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
+fig, ax = plt.subplots(figsize=(8, 8), dpi=112)
+im = ax.imshow(iteration, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), 
+               origin='lower', cmap='rainbow')
+ax.set_title('Julia Set (Bright Rainbow)', fontsize=14)
+ax.set_xlabel('Re(z)', fontsize=12)
+ax.set_ylabel('Im(z)', fontsize=12)
 
-hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.7 * smooth_norm + 0.3) % 1
-hsv[..., 1] = 1.0 - 0.6 * smooth_norm
-hsv[..., 2] = smooth_norm ** 0.8
-
-rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
-img = Image.fromarray(rgb)
-
-# Pixel sorting effect
-def pixel_sort(im):
-    arr = np.array(im)
-    for row in arr:
-        row.sort(axis=0)
-    return Image.fromarray(arr)
-
-img = pixel_sort(img)
-enhanced = ImageEnhance.Color(img).enhance(2.5)
-enhanced = ImageEnhance.Brightness(enhanced).enhance(1.3)
-
-output_path = 'julia_output.jpg'
-enhanced.save(output_path) 
+plt.tight_layout()
+plt.savefig('julia_output.jpg', dpi=112, bbox_inches='tight')
+plt.close() 
