da1000b4f9bcb4eead612f4ca5884e489f8bb5e1
Scott <scottimig@gmail.com>
2021-09-22 16:09:17 -0600

    saving

--- julia_render.py
+++ julia_render.py
@@ -1,12 +1,12 @@
 import numpy as np
-from PIL import Image, ImageFilter, ImageEnhance, ImageOps
+from PIL import Image, ImageEnhance
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-1.77, 1.77)
-y_range = (-1.77, 1.77)
-c = complex(-0.71, -0.41)
-max_iter = 350
+x_range = (-1.56, 1.56)
+y_range = (-1.56, 1.56)
+c = complex(0.24, -0.03)
+max_iter = 900
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
@@ -26,34 +26,15 @@ with np.errstate(divide='ignore', invalid='ignore'):
     smooth = np.nan_to_num(smooth)
 smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
-# Pastel palette
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1
-hsv[..., 1] = 0.4 + 0.3 * np.abs(np.sin(2 * np.pi * smooth_norm))
-hsv[..., 2] = smooth_norm ** 0.5
+hsv[..., 0] = (0.8 * smooth_norm + 0.1) % 1
+hsv[..., 1] = 0.98 - 0.2 * np.abs(np.sin(2 * np.pi * smooth_norm))
+hsv[..., 2] = smooth_norm ** 0.2
 
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
-
-# Heavy pixelation
-def pixelate(im, block=20):
-    arr = np.array(im)
-    for i in range(0, arr.shape[0], block):
-        for j in range(0, arr.shape[1], block):
-            arr[i:i+block, j:j+block] = arr[i, j]
-    return Image.fromarray(arr)
-
-img = pixelate(img, block=30)
-
-# Vertical split mirror
-def vertical_split_mirror(im):
-    arr = np.array(im)
-    mid = arr.shape[1] // 2
-    arr[:, mid:] = arr[:, :mid][:, ::-1]
-    return Image.fromarray(arr)
-
-img = vertical_split_mirror(img)
-img = ImageEnhance.Color(img).enhance(1.5)
+img = ImageEnhance.Color(img).enhance(2.0)
+img = ImageEnhance.Contrast(img).enhance(1.3)
 
 output_path = 'julia_output.jpg'
 img.save(output_path) 
