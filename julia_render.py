8f6df8b85da580e529cbb828a7472beff7b770ac
Scott <scottimig@gmail.com>
2022-03-03 14:39:38 -0700

    update

--- julia_render.py
+++ julia_render.py
@@ -1,11 +1,11 @@
 import numpy as np
-from PIL import Image, ImageFilter, ImageEnhance, ImageOps
+from PIL import Image, ImageFilter, ImageEnhance
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-0.38, 0.38)
-y_range = (-0.46, 0.46)
-c = complex(0.28, -0.51)
+x_range = (-0.64, 0.64)
+y_range = (-0.5, 0.5)
+c = complex(0.39, -0.45)
 max_iter = 350
 
 x = np.linspace(x_range[0], x_range[1], width)
@@ -28,22 +28,22 @@ smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
 hsv = np.zeros((height, width, 3), dtype=float)
 hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1
-hsv[..., 1] = 0.95 - 0.1 * np.abs(np.sin(2 * np.pi * smooth_norm))
+hsv[..., 1] = 0.95 - 0.1 * smooth_norm
 hsv[..., 2] = smooth_norm ** 0.2
 
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
 
-img = ImageOps.posterize(img, 2)
+# Duotone palette
+def duotone(im, color1=(30, 30, 120), color2=(220, 220, 60)):
+    arr = np.array(im).astype(np.float32) / 255.0
+    mask = arr[..., 0] > 0.5
+    arr[mask] = np.array(color1) / 255.0
+    arr[~mask] = np.array(color2) / 255.0
+    return Image.fromarray((arr * 255).astype(np.uint8))
 
-def diagonal_wave(im, amp=10, freq=0.12):
-    arr = np.array(im)
-    for i in range(arr.shape[0]):
-        arr[i] = np.roll(arr[i], int(amp * np.sin(freq * i + freq * i)))
-    return Image.fromarray(arr)
-
-img = diagonal_wave(img, amp=20, freq=0.18)
-img = img.filter(ImageFilter.DETAIL)
+img = duotone(img)
+img = img.filter(ImageFilter.EDGE_ENHANCE_MORE)
 
 output_path = 'julia_output.jpg'
 img.save(output_path) 
