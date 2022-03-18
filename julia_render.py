636af90e77674458bff8e6256912ac27290af4c8
Scott <scottimig@gmail.com>
2022-03-18 15:47:00 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -1,66 +1,55 @@
 import numpy as np
-from PIL import Image, ImageDraw, ImageEnhance
+from PIL import Image, ImageFilter, ImageEnhance, ImageOps
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-2.06, 2.06)
-y_range = (-2.06, 2.06)
-c = complex(-0.77, 0.17)
-max_iter = 300
+x_range = (-0.84, 0.84)
+y_range = (-0.55, 0.55)
+c = complex(-0.78, 0.17)
+max_iter = 370
 
-# Julia set
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
 Z = X + 1j * Y
-julia = np.zeros(Z.shape, dtype=int)
+
+div_iter = np.zeros(Z.shape, dtype=int)
 mask = np.ones(Z.shape, dtype=bool)
 for i in range(max_iter):
     Z[mask] = Z[mask] ** 2 + c
     mask_new = np.abs(Z) <= 2
-    julia[mask & ~mask_new] = i
+    div_iter[mask & ~mask_new] = i
     mask = mask_new
 
-# Bright rainbow palette
+with np.errstate(divide='ignore', invalid='ignore'):
+    smooth = div_iter + 1 - np.log(np.log2(np.abs(Z)))
+    smooth = np.nan_to_num(smooth)
+smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
+
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (julia / max_iter + 0.3) % 1
-hsv[..., 1] = 0.9 + 0.1 * (julia / max_iter)
-hsv[..., 2] = (julia / max_iter) ** 0.5
+hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1
+hsv[..., 1] = 0.95 - 0.1 * smooth_norm
+hsv[..., 2] = smooth_norm ** 0.2
+
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
 
-# Zoomed Mandelbrot overlay
-mandel = np.zeros(Z.shape, dtype=int)
-C = (X/2) + 1j * (Y/2)
-Z2 = np.zeros_like(C)
-mask = np.ones(C.shape, dtype=bool)
-for i in range(max_iter):
-    Z2[mask] = Z2[mask] ** 2 + C[mask]
-    mask_new = np.abs(Z2) <= 2
-    mandel[mask & ~mask_new] = i
-    mask = mask_new
-mandel_img = (mandel / max_iter * 255).astype(np.uint8)
-mandel_img = Image.fromarray(np.stack([mandel_img]*3, axis=-1)).convert('RGBA')
-mandel_img.putalpha(80)
-img = img.convert('RGBA')
-img = Image.alpha_composite(img, mandel_img)
-img = img.convert('RGB')
+img = ImageOps.solarize(img, threshold=80)
 
-# Draw random geometric shapes
-draw = ImageDraw.Draw(img)
-for _ in range(30):
-    shape = np.random.choice(['ellipse', 'rectangle', 'line'])
-    xy = [np.random.randint(0, width), np.random.randint(0, height), np.random.randint(0, width), np.random.randint(0, height)]
-    color = tuple(np.random.randint(100, 255, 3))
-    if shape == 'ellipse':
-        draw.ellipse(xy, outline=color, width=2)
-    elif shape == 'rectangle':
-        draw.rectangle(xy, outline=color, width=2)
-    else:
-        draw.line(xy, fill=color, width=2)
+# Circular pixel sort
+def circular_pixel_sort(im):
+    arr = np.array(im)
+    cy, cx = arr.shape[0] // 2, arr.shape[1] // 2
+    for r in range(1, min(cy, cx)):
+        indices = (np.abs(np.sqrt((np.arange(arr.shape[0])[:, None] - cy) ** 2 + (np.arange(arr.shape[1]) - cx) ** 2) - r) < 1)
+        for c in range(3):
+            band = arr[..., c][indices]
+            band.sort()
+            arr[..., c][indices] = band
+    return Image.fromarray(arr)
 
-img = ImageEnhance.Color(img).enhance(2.0)
-img = ImageEnhance.Contrast(img).enhance(1.2)
+img = circular_pixel_sort(img)
+img = img.filter(ImageFilter.EDGE_ENHANCE)
 
 output_path = 'julia_output.jpg'
 img.save(output_path) 
