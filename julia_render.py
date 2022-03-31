5a1e54a9dcd4c182e903954307ffd8fb6c1474b5
Scott <scottimig@gmail.com>
2022-03-31 11:58:28 -0600

    saving

--- julia_render.py
+++ julia_render.py
@@ -1,65 +1,51 @@
 import numpy as np
-from PIL import Image, ImageFilter, ImageEnhance, ImageOps
+import matplotlib.pyplot as plt
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-1.52, 1.52)
-y_range = (-1.52, 1.52)
-c = complex(0.25, -0.03)
-max_iter = 400
+x_range = (-1.38, 1.38)
+y_range = (-1.5, 1.5)
+c = complex(0.37, 0.4)
+max_iter = 360
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
 Z = X + 1j * Y
 
-div_iter = np.zeros(Z.shape, dtype=int)
+iteration = np.zeros(Z.shape, dtype=int)
 mask = np.ones(Z.shape, dtype=bool)
 for i in range(max_iter):
     Z[mask] = Z[mask] ** 2 + c
     mask_new = np.abs(Z) <= 2
-    div_iter[mask & ~mask_new] = i
+    iteration[mask & ~mask_new] = i
     mask = mask_new
 
 with np.errstate(divide='ignore', invalid='ignore'):
-    smooth = div_iter + 1 - np.log(np.log2(np.abs(Z)))
+    smooth = iteration + 1 - np.log(np.log2(np.abs(Z)))
     smooth = np.nan_to_num(smooth)
 smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
-# Sepia palette
-def sepia(im):
-    arr = np.array(im).astype(np.float32)
-    r, g, b = arr[..., 0], arr[..., 1], arr[..., 2]
-    tr = 0.393 * r + 0.769 * g + 0.189 * b
-    tg = 0.349 * r + 0.686 * g + 0.168 * b
-    tb = 0.272 * r + 0.534 * g + 0.131 * b
-    arr[..., 0] = np.clip(tr, 0, 255)
-    arr[..., 1] = np.clip(tg, 0, 255)
-    arr[..., 2] = np.clip(tb, 0, 255)
-    return Image.fromarray(arr.astype(np.uint8))
-
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.1 * smooth_norm + 0.1) % 1
-hsv[..., 1] = 0.4 + 0.2 * np.abs(np.sin(2 * np.pi * smooth_norm))
-hsv[..., 2] = smooth_norm ** 0.7
+hsv[..., 0] = (0.2 * smooth_norm + 0.5) % 1
+hsv[..., 1] = 0.95
+hsv[..., 2] = smooth_norm ** 0.6
+rgb = hsv_to_rgb(hsv)
 
-rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
-img = Image.fromarray(rgb)
-img = sepia(img)
-img = img.filter(ImageFilter.GaussianBlur(radius=8))
+fig, ax = plt.subplots(figsize=(8, 8), dpi=112)
+im = ax.imshow(rgb, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), origin='lower')
+ax.set_title('Julia Set (c = 0.355 + 0.355i)', fontsize=14)
+ax.set_xlabel('Re(z)', fontsize=12)
+ax.set_ylabel('Im(z)', fontsize=12)
+ax.grid(True, color='white', alpha=0.2, linestyle='--', linewidth=0.5)
 
-# Spiral mask overlay
-def spiral_mask(im):
-    arr = np.array(im)
-    cy, cx = arr.shape[0] // 2, arr.shape[1] // 2
-    Y, X = np.ogrid[:arr.shape[0], :arr.shape[1]]
-    theta = np.arctan2(Y - cy, X - cx)
-    mask = ((theta + np.sqrt((Y-cy)**2 + (X-cx)**2)/40) % (2*np.pi) < np.pi)
-    arr[mask] = arr[mask] // 2
-    return Image.fromarray(arr)
+from matplotlib.cm import ScalarMappable
+from matplotlib.colors import Normalize
+sm = ScalarMappable(cmap='hsv', norm=Normalize(vmin=smooth_norm.min(), vmax=smooth_norm.max()))
+sm.set_array([])
+cbar = plt.colorbar(sm, ax=ax, fraction=0.046, pad=0.04)
+cbar.set_label('Normalized Iteration (Smooth)', fontsize=12)
 
-img = spiral_mask(img)
-img = ImageEnhance.Contrast(img).enhance(1.3)
-
-output_path = 'julia_output.jpg'
-img.save(output_path) 
+plt.tight_layout()
+plt.savefig('julia_output.jpg', dpi=112, bbox_inches='tight')
+plt.close() 
