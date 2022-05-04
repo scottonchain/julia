c285265c71788c2dc9a608e6f7816979dd28cc29
Scott <scottimig@gmail.com>
2022-05-04 00:08:09 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -1,12 +1,12 @@
 import numpy as np
-from PIL import Image, ImageFilter, ImageEnhance, ImageOps
+from PIL import Image, ImageFilter, ImageEnhance, ImageDraw
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-0.69, 0.69)
-y_range = (-0.69, 0.69)
-c = complex(0.26, 0.04)
-max_iter = 320
+x_range = (-1.32, 1.32)
+y_range = (-1.38, 1.38)
+c = complex(-0.68, -0.35)
+max_iter = 340
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
@@ -27,16 +27,30 @@ with np.errstate(divide='ignore', invalid='ignore'):
 smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1
-hsv[..., 1] = 0.95 - 0.1 * np.abs(np.sin(2 * np.pi * smooth_norm))
-hsv[..., 2] = smooth_norm ** 0.2
+hsv[..., 0] = (0.85 * smooth_norm + 0.15) % 1
+hsv[..., 1] = 1.0 - 0.5 * smooth_norm
+hsv[..., 2] = smooth_norm ** 0.7
 
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
 
-img = img.quantize(colors=8, method=2)
-img = img.convert('RGB')
-enhanced = ImageEnhance.Contrast(img).enhance(1.5)
+# Pixel sorting effect
+def pixel_sort(im):
+    arr = np.array(im)
+    for col in arr.transpose(1,0,2):
+        col.sort(axis=0)
+    return Image.fromarray(arr)
+
+img = pixel_sort(img)
+
+def add_stripes(im, stripe_width=20):
+    draw = ImageDraw.Draw(im)
+    for x in range(0, im.width, stripe_width*2):
+        draw.rectangle([x, 0, x+stripe_width, im.height], fill=(255,255,255,40))
+    return im
+
+img = add_stripes(img, stripe_width=25)
+img = ImageEnhance.Brightness(img).enhance(1.3)
 
 output_path = 'julia_output.jpg'
-enhanced.save(output_path) 
+img.save(output_path) 
