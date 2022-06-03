01bf4c2a835744ab43e468333e95a945832fdbd4
Scott <scottimig@gmail.com>
2022-06-03 10:51:14 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -1,63 +1,45 @@
 import numpy as np
-from PIL import Image, ImageDraw, ImageEnhance
-from matplotlib.colors import hsv_to_rgb
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
 width, height = 1600, 1600
+# Zoomed in region for detail
 x_range = (-2.02, 2.02)
-y_range = (-2.02, 2.02)
-max_iter = 300
+y_range = (-1.97, 1.97)
+c = complex(-0.44, -0.55)
+max_iter = 1100
 
-# Tricorn fractal
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
-C = X + 1j * Y
-Z = np.zeros_like(C)
-tricorn = np.zeros(C.shape, dtype=int)
-mask = np.ones(C.shape, dtype=bool)
+Z = X + 1j * Y
+
+iteration = np.zeros(Z.shape, dtype=int)
+mask = np.ones(Z.shape, dtype=bool)
 for i in range(max_iter):
-    Z[mask] = np.conj(Z[mask]) ** 2 + C[mask]
+    Z[mask] = Z[mask] ** 2 + c
     mask_new = np.abs(Z) <= 2
-    tricorn[mask & ~mask_new] = i
+    iteration[mask & ~mask_new] = i
     mask = mask_new
 
-hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.2 * tricorn / max_iter + 0.8) % 1
-hsv[..., 1] = 0.9 + 0.1 * (tricorn / max_iter)
-hsv[..., 2] = (tricorn / max_iter) ** 0.5
-rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
-img = Image.fromarray(rgb)
+# Smooth coloring for better detail
+with np.errstate(divide='ignore', invalid='ignore'):
+    smooth = iteration + 1 - np.log(np.log2(np.abs(Z)))
+    smooth = np.nan_to_num(smooth)
 
-# Julia set overlay
-c = complex(0.24, -0.04)
-Z2 = X + 1j * Y
-julia = np.zeros(Z2.shape, dtype=int)
-mask = np.ones(Z2.shape, dtype=bool)
-for i in range(max_iter):
-    Z2[mask] = Z2[mask] ** 2 + c
-    mask_new = np.abs(Z2) <= 2
-    julia[mask & ~mask_new] = i
-    mask = mask_new
-julia_img = (julia / max_iter * 255).astype(np.uint8)
-julia_img = Image.fromarray(np.stack([julia_img]*3, axis=-1)).convert('RGBA')
-julia_img.putalpha(80)
-img = img.convert('RGBA')
-img = Image.alpha_composite(img, julia_img)
-img = img.convert('RGB')
+# Use bright, warm prismatic colormap
+fig, ax = plt.subplots(figsize=(8, 8), dpi=112)
+im = ax.imshow(smooth, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), 
+               origin='lower', cmap='summer', interpolation='bilinear')
+ax.set_title('Julia Set Detail (c = -0.4 - 0.59i)', fontsize=14)
+ax.set_xlabel('Re(z)', fontsize=12)
+ax.set_ylabel('Im(z)', fontsize=12)
+ax.grid(True, color='white', alpha=0.3, linestyle='--', linewidth=0.5)
 
-# Color cycling effect
-arr = np.array(img)
-arr = np.roll(arr, shift=30, axis=2)
-img = Image.fromarray(arr)
+# Add colorbar
+cbar = plt.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
+cbar.set_label('Iteration Count (Smooth)', fontsize=12)
 
-# Geometric grid overlay
-draw = ImageDraw.Draw(img)
-for x in range(0, width, 60):
-    draw.line((x, 0, x, height), fill=(255,255,255,60), width=1)
-for y in range(0, height, 60):
-    draw.line((0, y, width, y), fill=(255,255,255,60), width=1)
-
-img = ImageEnhance.Color(img).enhance(1.7)
-
-output_path = 'julia_output.jpg'
-img.save(output_path) 
+plt.tight_layout()
+plt.savefig('julia_output.jpg', dpi=112, bbox_inches='tight')
+plt.close() 
