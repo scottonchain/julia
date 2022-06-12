d09bc2ede3d593752c04284278829eaa747707e2
Scott <scottimig@gmail.com>
2022-06-11 18:58:06 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -1,12 +1,12 @@
 import numpy as np
-from PIL import Image, ImageFilter, ImageEnhance
+from PIL import Image, ImageFilter, ImageEnhance, ImageOps
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-1.15, 1.15)
-y_range = (-1.2, 1.2)
-c = complex(-0.57, 0.58)
-max_iter = 300
+x_range = (-0.76, 0.76)
+y_range = (-0.67, 0.67)
+c = complex(-0.76, 0.12)
+max_iter = 370
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
@@ -27,23 +27,29 @@ with np.errstate(divide='ignore', invalid='ignore'):
 smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.7 * smooth_norm + 0.3) % 1
-hsv[..., 1] = 1.0 - 0.6 * smooth_norm
-hsv[..., 2] = smooth_norm ** 0.8
+hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1
+hsv[..., 1] = 0.95 - 0.1 * smooth_norm
+hsv[..., 2] = smooth_norm ** 0.2
 
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
 
-# Pixel sorting effect
-def pixel_sort(im):
+img = ImageOps.solarize(img, threshold=80)
+
+# Circular pixel sort
+def circular_pixel_sort(im):
     arr = np.array(im)
-    for row in arr:
-        row.sort(axis=0)
+    cy, cx = arr.shape[0] // 2, arr.shape[1] // 2
+    for r in range(1, min(cy, cx)):
+        indices = (np.abs(np.sqrt((np.arange(arr.shape[0])[:, None] - cy) ** 2 + (np.arange(arr.shape[1]) - cx) ** 2) - r) < 1)
+        for c in range(3):
+            band = arr[..., c][indices]
+            band.sort()
+            arr[..., c][indices] = band
     return Image.fromarray(arr)
 
-img = pixel_sort(img)
-enhanced = ImageEnhance.Color(img).enhance(2.5)
-enhanced = ImageEnhance.Brightness(enhanced).enhance(1.3)
+img = circular_pixel_sort(img)
+img = img.filter(ImageFilter.EDGE_ENHANCE)
 
 output_path = 'julia_output.jpg'
-enhanced.save(output_path) 
+img.save(output_path) 
