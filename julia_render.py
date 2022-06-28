8969ab9b9ddad7e205a1d34b9baf71d29d5f9039
Scott <scottimig@gmail.com>
2022-06-28 08:04:53 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -1,70 +1,45 @@
 import numpy as np
-from PIL import Image, ImageFilter, ImageEnhance
-import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw, ImageEnhance
 from matplotlib.colors import hsv_to_rgb
 
-# Parameters (these will be programmatically changed by the main script)
 width, height = 1600, 1600
-x_range = (-1.67, 1.67)
-y_range = (-1.67, 1.67)
-c = complex(-0.43, 0.57)
-max_iter = 350
+x_range = (-2.2, 1.2)
+y_range = (-1.7, 1.7)
+max_iter = 320
+p = -0.3 + 0.6j
 
-# Generate grid of complex points
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
-Z = X + 1j * Y
-
-# Initialize iteration counts and mask
-div_iter = np.zeros(Z.shape, dtype=int)
-mask = np.ones(Z.shape, dtype=bool)
-
-# Iterate and record divergence
+C = X + 1j * Y
+Z = np.zeros_like(C)
+Zold = np.zeros_like(C)
+phoenix = np.zeros(C.shape, dtype=int)
+mask = np.ones(C.shape, dtype=bool)
 for i in range(max_iter):
-    Z[mask] = Z[mask] ** 2 + c
+    Z[mask], Zold[mask] = Z[mask] ** 2 + C[mask] + p * Zold[mask], Z[mask]
     mask_new = np.abs(Z) <= 2
-    div_iter[mask & ~mask_new] = i
+    phoenix[mask & ~mask_new] = i
     mask = mask_new
 
-# Smooth coloring
-with np.errstate(divide='ignore', invalid='ignore'):
-    smooth = div_iter + 1 - np.log(np.log2(np.abs(Z)))
-    smooth = np.nan_to_num(smooth)
-smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
-
-# Build HSV image with a bright color scheme
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.6 * smooth_norm + 0.1) % 1  # Bright hue shift
-hsv[..., 1] = 0.9 + 0.1 * np.sin(2 * np.pi * smooth_norm)  # High saturation
-hsv[..., 2] = smooth_norm ** 0.3  # Bright value
-
-# Convert to RGB
+hsv[..., 0] = (0.15 * phoenix / max_iter + 0.8) % 1
+hsv[..., 1] = 0.8 + 0.2 * (phoenix / max_iter)
+hsv[..., 2] = (phoenix / max_iter) ** 0.6
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
 
-# Kaleidoscope effect (4-way mirror)
-def kaleidoscope(im):
-    arr = np.array(im)
-    arr = np.concatenate([arr, arr[:, ::-1]], axis=1)
-    arr = np.concatenate([arr, arr[::-1, :]], axis=0)
-    return Image.fromarray(arr)
+# Overlay: grid and roots of unity
+step = 0.4
+for val in np.arange(x_range[0], x_range[1] + step, step):
+    x_pix = int((val - x_range[0]) / (x_range[1] - x_range[0]) * width)
+    ImageDraw.Draw(img).line([(x_pix, 0), (x_pix, height)], fill=(180, 220, 255), width=1)
+for val in np.arange(y_range[0], y_range[1] + step, step):
+    y_pix = int((y_range[1] - val) / (y_range[1] - y_range[0]) * height)
+    ImageDraw.Draw(img).line([(0, y_pix), (width, y_pix)], fill=(180, 220, 255), width=1)
 
-img = kaleidoscope(img)
+img = ImageEnhance.Color(img).enhance(1.4)
+img = ImageEnhance.Contrast(img).enhance(1.1)
 
-# Artistic postprocessing: glow, contrast, and edge enhancement
-blur = img.filter(ImageFilter.GaussianBlur(radius=3))
-glow = Image.blend(img, blur, alpha=0.4)
-enhanced = ImageEnhance.Contrast(glow).enhance(1.8)
-enhanced = ImageEnhance.Color(enhanced).enhance(1.5)
-enhanced = enhanced.filter(ImageFilter.EDGE_ENHANCE_MORE)
-
-# Save output
 output_path = 'julia_output.jpg'
-enhanced.save(output_path)
-
-# Optionally display
-# plt.figure(figsize=(8, 8))
-# plt.axis('off')
-# plt.imshow(enhanced)
-# plt.show() 
+img.save(output_path) 
