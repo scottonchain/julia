7270992b97d65ce715c49f5241cc5946f77381da
Scott <scottimig@gmail.com>
2022-06-28 08:36:24 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -1,45 +1,59 @@
 import numpy as np
-from PIL import Image, ImageDraw, ImageEnhance
+import matplotlib.pyplot as plt
 from matplotlib.colors import hsv_to_rgb
 
+# Parameters for a scientific Julia set plot
 width, height = 1600, 1600
-x_range = (-2.2, 1.2)
-y_range = (-1.7, 1.7)
-max_iter = 320
-p = -0.3 + 0.6j
+x_range = (-1.53, 1.53)
+y_range = (-1.44, 1.44)
+c = complex(-0.67, 0.28)
+max_iter = 400
 
+# Generate grid
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
-C = X + 1j * Y
-Z = np.zeros_like(C)
-Zold = np.zeros_like(C)
-phoenix = np.zeros(C.shape, dtype=int)
-mask = np.ones(C.shape, dtype=bool)
+Z = X + 1j * Y
+
+# Julia set calculation
+iteration = np.zeros(Z.shape, dtype=int)
+mask = np.ones(Z.shape, dtype=bool)
 for i in range(max_iter):
-    Z[mask], Zold[mask] = Z[mask] ** 2 + C[mask] + p * Zold[mask], Z[mask]
+    Z[mask] = Z[mask] ** 2 + c
     mask_new = np.abs(Z) <= 2
-    phoenix[mask & ~mask_new] = i
+    iteration[mask & ~mask_new] = i
     mask = mask_new
 
+# Smooth coloring
+with np.errstate(divide='ignore', invalid='ignore'):
+    smooth = iteration + 1 - np.log(np.log2(np.abs(Z)))
+    smooth = np.nan_to_num(smooth)
+smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
+
+# HSV colormap for scientific look
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.15 * phoenix / max_iter + 0.8) % 1
-hsv[..., 1] = 0.8 + 0.2 * (phoenix / max_iter)
-hsv[..., 2] = (phoenix / max_iter) ** 0.6
-rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
-img = Image.fromarray(rgb)
+hsv[..., 0] = (0.6 * smooth_norm + 0.2) % 1
+hsv[..., 1] = 0.8
+hsv[..., 2] = smooth_norm ** 0.8
+rgb = hsv_to_rgb(hsv)
 
-# Overlay: grid and roots of unity
-step = 0.4
-for val in np.arange(x_range[0], x_range[1] + step, step):
-    x_pix = int((val - x_range[0]) / (x_range[1] - x_range[0]) * width)
-    ImageDraw.Draw(img).line([(x_pix, 0), (x_pix, height)], fill=(180, 220, 255), width=1)
-for val in np.arange(y_range[0], y_range[1] + step, step):
-    y_pix = int((y_range[1] - val) / (y_range[1] - y_range[0]) * height)
-    ImageDraw.Draw(img).line([(0, y_pix), (width, y_pix)], fill=(180, 220, 255), width=1)
+# Plot with scientific style
+fig, ax = plt.subplots(figsize=(8, 8), dpi=112)
+im = ax.imshow(rgb, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), origin='lower')
+ax.set_title('Julia Set (c = -0.7 + 0.27015i)', fontsize=14)
+ax.set_xlabel('Re(z)', fontsize=12)
+ax.set_ylabel('Im(z)', fontsize=12)
+ax.grid(True, color='white', alpha=0.2, linestyle='--', linewidth=0.5)
 
-img = ImageEnhance.Color(img).enhance(1.4)
-img = ImageEnhance.Contrast(img).enhance(1.1)
+# Add colorbar
+from matplotlib.cm import ScalarMappable
+from matplotlib.colors import Normalize
+sm = ScalarMappable(cmap='hsv', norm=Normalize(vmin=smooth_norm.min(), vmax=smooth_norm.max()))
+sm.set_array([])
+cbar = plt.colorbar(sm, ax=ax, fraction=0.046, pad=0.04)
+cbar.set_label('Normalized Iteration (Smooth)', fontsize=12)
 
-output_path = 'julia_output.jpg'
-img.save(output_path) 
+# Save as scientific-looking chart
+plt.tight_layout()
+plt.savefig('julia_output.jpg', dpi=112, bbox_inches='tight')
+plt.close() 
