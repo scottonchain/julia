5cf63c44d264e595e63cbc2ccf901b07be5c134f
Scott <scottimig@gmail.com>
2022-06-29 06:28:40 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -1,12 +1,12 @@
 import numpy as np
-from PIL import Image, ImageFilter, ImageEnhance, ImageDraw
+from PIL import Image, ImageFilter, ImageEnhance, ImageOps
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-1.61, 1.61)
-y_range = (-1.61, 1.61)
-c = complex(0.35, 0.33)
-max_iter = 360
+x_range = (-0.64, 0.64)
+y_range = (-0.64, 0.64)
+c = complex(0.24, -0.04)
+max_iter = 320
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
@@ -27,27 +27,16 @@ with np.errstate(divide='ignore', invalid='ignore'):
 smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.4 * smooth_norm + 0.4) % 1
-hsv[..., 1] = 0.9 + 0.1 * np.abs(np.sin(2 * np.pi * smooth_norm))
-hsv[..., 2] = smooth_norm ** 0.3
+hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1
+hsv[..., 1] = 0.95 - 0.1 * np.abs(np.sin(2 * np.pi * smooth_norm))
+hsv[..., 2] = smooth_norm ** 0.2
 
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
 
-img = img.filter(ImageFilter.EMBOSS)
-
-# Circular swirl mask overlay
-def swirl_mask(im):
-    arr = np.array(im)
-    cy, cx = arr.shape[0] // 2, arr.shape[1] // 2
-    Y, X = np.ogrid[:arr.shape[0], :arr.shape[1]]
-    r = np.sqrt((Y - cy) ** 2 + (X - cx) ** 2)
-    mask = (np.sin(r / 20) > 0)
-    arr[mask] = arr[mask] // 2
-    return Image.fromarray(arr)
-
-img = swirl_mask(img)
-img = ImageEnhance.Contrast(img).enhance(1.5)
+img = img.quantize(colors=8, method=2)
+img = img.convert('RGB')
+enhanced = ImageEnhance.Contrast(img).enhance(1.5)
 
 output_path = 'julia_output.jpg'
-img.save(output_path) 
+enhanced.save(output_path) 
