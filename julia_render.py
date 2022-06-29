fa00f1d7d8ab99dc9cdc5f4a594fbefb7e298a84
Scott <scottimig@gmail.com>
2022-06-29 05:23:02 -0600

    saving

--- julia_render.py
+++ julia_render.py
@@ -1,12 +1,12 @@
 import numpy as np
-from PIL import Image, ImageFilter, ImageEnhance
+from PIL import Image, ImageEnhance
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-0.62, 0.62)
-y_range = (-0.7, 0.7)
-c = complex(0.36, -0.35)
-max_iter = 360
+x_range = (-1.48, 1.48)
+y_range = (-1.48, 1.48)
+c = complex(-0.39, 0.6)
+max_iter = 1000
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
@@ -27,16 +27,14 @@ with np.errstate(divide='ignore', invalid='ignore'):
 smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1
-hsv[..., 1] = 0.95 - 0.1 * np.abs(np.cos(2 * np.pi * smooth_norm))
+hsv[..., 0] = (0.6 * smooth_norm + 0.3) % 1
+hsv[..., 1] = 0.97 - 0.15 * np.abs(np.sin(2 * np.pi * smooth_norm))
 hsv[..., 2] = smooth_norm ** 0.2
 
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
-
-img = img.filter(ImageFilter.EMBOSS)
-enhanced = ImageEnhance.Contrast(img).enhance(1.7)
-enhanced = ImageEnhance.Color(enhanced).enhance(1.3)
+img = ImageEnhance.Color(img).enhance(2.0)
+img = ImageEnhance.Contrast(img).enhance(1.3)
 
 output_path = 'julia_output.jpg'
-enhanced.save(output_path) 
+img.save(output_path) 
