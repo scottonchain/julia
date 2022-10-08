501ac0a6ca3c1d0d18aec178c19fc9e8d6c583b3
Scott <scottimig@gmail.com>
2022-10-08 04:33:04 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -1,33 +1,51 @@
 import numpy as np
-import matplotlib.pyplot as plt
+from PIL import Image, ImageFilter, ImageEnhance
+from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-2.02, 2.02)
-y_range = (-1.94, 1.94)
-c = complex(-0.71, 0.22)
+x_range = (-2.0, 2.0)
+y_range = (-2.0, 2.0)
 max_iter = 300
 
+# Burning Ship fractal
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
-Z = X + 1j * Y
-
-iteration = np.zeros(Z.shape, dtype=int)
-mask = np.ones(Z.shape, dtype=bool)
-
+C = X + 1j * Y
+Z = np.zeros_like(C)
+ship = np.zeros(C.shape, dtype=int)
+mask = np.ones(C.shape, dtype=bool)
 for i in range(max_iter):
-    Z[mask] = Z[mask] ** 2 + c
+    Z[mask] = (np.abs(Z[mask].real) + 1j * np.abs(Z[mask].imag)) ** 2 + C[mask]
     mask_new = np.abs(Z) <= 2
-    iteration[mask & ~mask_new] = i
+    ship[mask & ~mask_new] = i
     mask = mask_new
 
-fig, ax = plt.subplots(figsize=(8, 8), dpi=112)
-im = ax.imshow(iteration, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), 
-               origin='lower', cmap='plasma')
-ax.set_title('Julia Set (Vectorized)', fontsize=14)
-ax.set_xlabel('Re(z)', fontsize=12)
-ax.set_ylabel('Im(z)', fontsize=12)
+hsv = np.zeros((height, width, 3), dtype=float)
+hsv[..., 0] = (0.2 * ship / max_iter + 0.8) % 1
+hsv[..., 1] = 0.9 - 0.7 * (ship / max_iter)
+hsv[..., 2] = (ship / max_iter) ** 0.7
+rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
+img = Image.fromarray(rgb)
 
-plt.tight_layout()
-plt.savefig('julia_output.jpg', dpi=112, bbox_inches='tight')
-plt.close() 
+# Julia set as transparency mask
+c = complex(-0.79, 0.15)
+Z2 = X + 1j * Y
+julia = np.zeros(Z2.shape, dtype=int)
+mask = np.ones(Z2.shape, dtype=bool)
+for i in range(max_iter):
+    Z2[mask] = Z2[mask] ** 2 + c
+    mask_new = np.abs(Z2) <= 2
+    julia[mask & ~mask_new] = i
+    mask = mask_new
+alpha = (julia / max_iter * 255).astype(np.uint8)
+img = img.convert('RGBA')
+img.putalpha(Image.fromarray(alpha))
+
+# Motion blur effect
+img = img.filter(ImageFilter.GaussianBlur(radius=2)).filter(ImageFilter.BoxBlur(3))
+img = img.convert('RGB')
+img = ImageEnhance.Color(img).enhance(2.1)
+
+output_path = 'julia_output.jpg'
+img.save(output_path) 
