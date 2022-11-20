6b29fb3a66943ce05f71fc52ee0e96f5a0d8d624
Scott <scottimig@gmail.com>
2022-11-20 04:50:21 -0700

    sync

--- julia_render.py
+++ julia_render.py
@@ -3,61 +3,64 @@ from PIL import Image, ImageDraw, ImageEnhance
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-1.96, 1.96)
-y_range = (-2.04, 2.04)
+x_range = (-2.04, 2.04)
+y_range = (-1.96, 1.96)
+c = complex(-0.82, 0.17)
 max_iter = 300
 
-# Tricorn fractal
+# Julia set
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
-C = X + 1j * Y
-Z = np.zeros_like(C)
-tricorn = np.zeros(C.shape, dtype=int)
-mask = np.ones(C.shape, dtype=bool)
+Z = X + 1j * Y
+julia = np.zeros(Z.shape, dtype=int)
+mask = np.ones(Z.shape, dtype=bool)
 for i in range(max_iter):
-    Z[mask] = np.conj(Z[mask]) ** 2 + C[mask]
+    Z[mask] = Z[mask] ** 2 + c
     mask_new = np.abs(Z) <= 2
-    tricorn[mask & ~mask_new] = i
+    julia[mask & ~mask_new] = i
     mask = mask_new
 
+# Bright rainbow palette
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.2 * tricorn / max_iter + 0.8) % 1
-hsv[..., 1] = 0.9 + 0.1 * (tricorn / max_iter)
-hsv[..., 2] = (tricorn / max_iter) ** 0.5
+hsv[..., 0] = (julia / max_iter + 0.3) % 1
+hsv[..., 1] = 0.9 + 0.1 * (julia / max_iter)
+hsv[..., 2] = (julia / max_iter) ** 0.5
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
 
-# Julia set overlay
-c = complex(0.28, -0.0)
-Z2 = X + 1j * Y
-julia = np.zeros(Z2.shape, dtype=int)
-mask = np.ones(Z2.shape, dtype=bool)
+# Zoomed Mandelbrot overlay
+mandel = np.zeros(Z.shape, dtype=int)
+C = (X/2) + 1j * (Y/2)
+Z2 = np.zeros_like(C)
+mask = np.ones(C.shape, dtype=bool)
 for i in range(max_iter):
-    Z2[mask] = Z2[mask] ** 2 + c
+    Z2[mask] = Z2[mask] ** 2 + C[mask]
     mask_new = np.abs(Z2) <= 2
-    julia[mask & ~mask_new] = i
+    mandel[mask & ~mask_new] = i
     mask = mask_new
-julia_img = (julia / max_iter * 255).astype(np.uint8)
-julia_img = Image.fromarray(np.stack([julia_img]*3, axis=-1)).convert('RGBA')
-julia_img.putalpha(80)
+mandel_img = (mandel / max_iter * 255).astype(np.uint8)
+mandel_img = Image.fromarray(np.stack([mandel_img]*3, axis=-1)).convert('RGBA')
+mandel_img.putalpha(80)
 img = img.convert('RGBA')
-img = Image.alpha_composite(img, julia_img)
+img = Image.alpha_composite(img, mandel_img)
 img = img.convert('RGB')
 
-# Color cycling effect
-arr = np.array(img)
-arr = np.roll(arr, shift=30, axis=2)
-img = Image.fromarray(arr)
-
-# Geometric grid overlay
+# Draw random geometric shapes
 draw = ImageDraw.Draw(img)
-for x in range(0, width, 60):
-    draw.line((x, 0, x, height), fill=(255,255,255,60), width=1)
-for y in range(0, height, 60):
-    draw.line((0, y, width, y), fill=(255,255,255,60), width=1)
+for _ in range(30):
+    shape = np.random.choice(['ellipse', 'rectangle', 'line'])
+    xy = [np.random.randint(0, width), np.random.randint(0, height), np.random.randint(0, width), np.random.randint(0, height)]
+    color = tuple(np.random.randint(100, 255, 3))
+    if shape == 'ellipse':
+        draw.ellipse(xy, outline=color, width=2)
+    elif shape == 'rectangle':
+        draw.rectangle(xy, outline=color, width=2)
+    else:
+        draw.line(xy, fill=color, width=2)
 
-img = ImageEnhance.Color(img).enhance(1.7)
+img = ImageEnhance.Color(img).enhance(2.0)
+img = ImageEnhance.Contrast(img).enhance(1.2)
 
 output_path = 'julia_output.jpg'
 img.save(output_path) 
