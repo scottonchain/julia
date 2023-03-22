33c32a1949c3f94f94c64411845f8acd3c5d489f
Scott <scottimig@gmail.com>
2023-03-21 23:37:40 -0600

    save state

--- julia_render.py
+++ julia_render.py
@@ -1,58 +1,64 @@
 import numpy as np
-from PIL import Image, ImageDraw, ImageEnhance, ImageOps
+from PIL import Image, ImageFilter, ImageEnhance, ImageOps
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-2.01, 2.01)
-y_range = (-2.01, 2.01)
-c = complex(-0.73, 0.29)
-max_iter = 300
+x_range = (-1.48, 1.48)
+y_range = (-1.56, 1.56)
+c = complex(0.27, -0.01)
+max_iter = 400
 
-# Julia set
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
 Z = X + 1j * Y
-julia = np.zeros(Z.shape, dtype=int)
+
+div_iter = np.zeros(Z.shape, dtype=int)
 mask = np.ones(Z.shape, dtype=bool)
 for i in range(max_iter):
     Z[mask] = Z[mask] ** 2 + c
     mask_new = np.abs(Z) <= 2
-    julia[mask & ~mask_new] = i
+    div_iter[mask & ~mask_new] = i
     mask = mask_new
 
-# Mandelbrot mask
-mandel = np.zeros(Z.shape, dtype=int)
-C = X + 1j * Y
-Z2 = np.zeros_like(C)
-mask = np.ones(C.shape, dtype=bool)
-for i in range(max_iter):
-    Z2[mask] = Z2[mask] ** 2 + C[mask]
-    mask_new = np.abs(Z2) <= 2
-    mandel[mask & ~mask_new] = i
-    mask = mask_new
+with np.errstate(divide='ignore', invalid='ignore'):
+    smooth = div_iter + 1 - np.log(np.log2(np.abs(Z)))
+    smooth = np.nan_to_num(smooth)
+smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
-# Vibrant color map
+# Sepia palette
+def sepia(im):
+    arr = np.array(im).astype(np.float32)
+    r, g, b = arr[..., 0], arr[..., 1], arr[..., 2]
+    tr = 0.393 * r + 0.769 * g + 0.189 * b
+    tg = 0.349 * r + 0.686 * g + 0.168 * b
+    tb = 0.272 * r + 0.534 * g + 0.131 * b
+    arr[..., 0] = np.clip(tr, 0, 255)
+    arr[..., 1] = np.clip(tg, 0, 255)
+    arr[..., 2] = np.clip(tb, 0, 255)
+    return Image.fromarray(arr.astype(np.uint8))
+
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.9 * julia / max_iter + 0.1) % 1
-hsv[..., 1] = 0.8 + 0.2 * (mandel / max_iter)
-hsv[..., 2] = (julia / max_iter) ** 0.5
+hsv[..., 0] = (0.1 * smooth_norm + 0.1) % 1
+hsv[..., 1] = 0.4 + 0.2 * np.abs(np.sin(2 * np.pi * smooth_norm))
+hsv[..., 2] = smooth_norm ** 0.7
+
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
+img = sepia(img)
+img = img.filter(ImageFilter.GaussianBlur(radius=8))
 
-# Hand-drawn scribble effect
-scribble = Image.new('RGBA', img.size, (0,0,0,0))
-draw = ImageDraw.Draw(scribble)
-for _ in range(200):
-    x0, y0 = np.random.randint(0, width), np.random.randint(0, height)
-    x1, y1 = x0 + np.random.randint(-30, 30), y0 + np.random.randint(-30, 30)
-    color = tuple(np.random.randint(100, 255, 3)) + (np.random.randint(40, 100),)
-    draw.line((x0, y0, x1, y1), fill=color, width=np.random.randint(1, 4))
-img = img.convert('RGBA')
-img = Image.alpha_composite(img, scribble)
-img = img.convert('RGB')
+# Spiral mask overlay
+def spiral_mask(im):
+    arr = np.array(im)
+    cy, cx = arr.shape[0] // 2, arr.shape[1] // 2
+    Y, X = np.ogrid[:arr.shape[0], :arr.shape[1]]
+    theta = np.arctan2(Y - cy, X - cx)
+    mask = ((theta + np.sqrt((Y-cy)**2 + (X-cx)**2)/40) % (2*np.pi) < np.pi)
+    arr[mask] = arr[mask] // 2
+    return Image.fromarray(arr)
 
-img = ImageEnhance.Color(img).enhance(2.2)
+img = spiral_mask(img)
 img = ImageEnhance.Contrast(img).enhance(1.3)
 
 output_path = 'julia_output.jpg'
