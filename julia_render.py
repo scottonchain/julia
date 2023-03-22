98d2c2684704828c2e191b3126c37c915f0ac38c
Scott <scottimig@gmail.com>
2023-03-21 20:29:33 -0600

    update

--- julia_render.py
+++ julia_render.py
@@ -1,53 +1,51 @@
 import numpy as np
-from PIL import Image, ImageFilter, ImageEnhance, ImageOps
+import matplotlib.pyplot as plt
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-1.59, 1.59)
-y_range = (-1.59, 1.59)
-c = complex(0.36, 0.33)
-max_iter = 360
+x_range = (-1.52, 1.52)
+y_range = (-1.52, 1.52)
+c = complex(0.29, 0.01)
+max_iter = 400
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
 Z = X + 1j * Y
 
-div_iter = np.zeros(Z.shape, dtype=int)
+iteration = np.zeros(Z.shape, dtype=int)
 mask = np.ones(Z.shape, dtype=bool)
 for i in range(max_iter):
     Z[mask] = Z[mask] ** 2 + c
     mask_new = np.abs(Z) <= 2
-    div_iter[mask & ~mask_new] = i
+    iteration[mask & ~mask_new] = i
     mask = mask_new
 
 with np.errstate(divide='ignore', invalid='ignore'):
-    smooth = div_iter + 1 - np.log(np.log2(np.abs(Z)))
+    smooth = iteration + 1 - np.log(np.log2(np.abs(Z)))
     smooth = np.nan_to_num(smooth)
 smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.8 * smooth_norm + 0.3) % 1
-hsv[..., 1] = 0.7 + 0.3 * np.abs(np.sin(2 * np.pi * smooth_norm))
-hsv[..., 2] = smooth_norm ** 0.5
+hsv[..., 0] = (0.5 * smooth_norm + 0.4) % 1
+hsv[..., 1] = 0.9
+hsv[..., 2] = smooth_norm ** 0.7
+rgb = hsv_to_rgb(hsv)
 
-rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
-img = Image.fromarray(rgb)
+fig, ax = plt.subplots(figsize=(8, 8), dpi=112)
+im = ax.imshow(rgb, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), origin='lower')
+ax.set_title('Julia Set (c = 0.285 + 0.013i)', fontsize=14)
+ax.set_xlabel('Re(z)', fontsize=12)
+ax.set_ylabel('Im(z)', fontsize=12)
+ax.grid(True, color='white', alpha=0.2, linestyle='--', linewidth=0.5)
 
-img = img.quantize(colors=12, method=2)
-img = img.convert('RGB')
+from matplotlib.cm import ScalarMappable
+from matplotlib.colors import Normalize
+sm = ScalarMappable(cmap='hsv', norm=Normalize(vmin=smooth_norm.min(), vmax=smooth_norm.max()))
+sm.set_array([])
+cbar = plt.colorbar(sm, ax=ax, fraction=0.046, pad=0.04)
+cbar.set_label('Normalized Iteration (Smooth)', fontsize=12)
 
-# Circular vignette
-def vignette(im):
-    arr = np.array(im).astype(np.float32)
-    cy, cx = arr.shape[0] // 2, arr.shape[1] // 2
-    Y, X = np.ogrid[:arr.shape[0], :arr.shape[1]]
-    mask = ((Y - cy) ** 2 + (X - cx) ** 2) / (cy * cx) > 0.7
-    arr[mask] = arr[mask] * 0.3
-    return Image.fromarray(arr.astype(np.uint8))
-
-img = vignette(img)
-img = ImageEnhance.Contrast(img).enhance(1.4)
-
-output_path = 'julia_output.jpg'
-img.save(output_path) 
+plt.tight_layout()
+plt.savefig('julia_output.jpg', dpi=112, bbox_inches='tight')
+plt.close() 
