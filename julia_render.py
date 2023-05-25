c0d8760cfd5e906f04a7d9632c1e5bb3beb43636
Scott <scottimig@gmail.com>
2023-05-25 03:04:54 -0600

    saving

--- julia_render.py
+++ julia_render.py
@@ -1,53 +1,66 @@
 import numpy as np
-from PIL import Image, ImageFilter, ImageEnhance
+from PIL import Image, ImageDraw, ImageEnhance
 from matplotlib.colors import hsv_to_rgb
 
 width, height = 1600, 1600
-x_range = (-1.98, 1.98)
-y_range = (-1.98, 1.98)
-c = complex(0.25, 0.04)
-max_iter = 400
+x_range = (-2.02, 2.02)
+y_range = (-1.97, 1.97)
+c = complex(-0.84, 0.16)
+max_iter = 300
 
+# Julia set
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
 X, Y = np.meshgrid(x, y)
 Z = X + 1j * Y
-
-div_iter = np.zeros(Z.shape, dtype=int)
+julia = np.zeros(Z.shape, dtype=int)
 mask = np.ones(Z.shape, dtype=bool)
 for i in range(max_iter):
     Z[mask] = Z[mask] ** 2 + c
     mask_new = np.abs(Z) <= 2
-    div_iter[mask & ~mask_new] = i
+    julia[mask & ~mask_new] = i
     mask = mask_new
 
-with np.errstate(divide='ignore', invalid='ignore'):
-    smooth = div_iter + 1 - np.log(np.log2(np.abs(Z)))
-    smooth = np.nan_to_num(smooth)
-smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
-
+# Bright rainbow palette
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.7 * smooth_norm + 0.2) % 1
-hsv[..., 1] = 0.95 - 0.1 * smooth_norm
-hsv[..., 2] = smooth_norm ** 0.2
-
+hsv[..., 0] = (julia / max_iter + 0.3) % 1
+hsv[..., 1] = 0.9 + 0.1 * (julia / max_iter)
+hsv[..., 2] = (julia / max_iter) ** 0.5
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
 
-img = img.filter(ImageFilter.GaussianBlur(radius=7))
+# Zoomed Mandelbrot overlay
+mandel = np.zeros(Z.shape, dtype=int)
+C = (X/2) + 1j * (Y/2)
+Z2 = np.zeros_like(C)
+mask = np.ones(C.shape, dtype=bool)
+for i in range(max_iter):
+    Z2[mask] = Z2[mask] ** 2 + C[mask]
+    mask_new = np.abs(Z2) <= 2
+    mandel[mask & ~mask_new] = i
+    mask = mask_new
+mandel_img = (mandel / max_iter * 255).astype(np.uint8)
+mandel_img = Image.fromarray(np.stack([mandel_img]*3, axis=-1)).convert('RGBA')
+mandel_img.putalpha(80)
+img = img.convert('RGBA')
+img = Image.alpha_composite(img, mandel_img)
+img = img.convert('RGB')
 
-# Spiral mask overlay
-def spiral_mask(im):
-    arr = np.array(im)
-    cy, cx = arr.shape[0] // 2, arr.shape[1] // 2
-    Y, X = np.ogrid[:arr.shape[0], :arr.shape[1]]
-    theta = np.arctan2(Y - cy, X - cx)
-    mask = ((theta + np.sqrt((Y-cy)**2 + (X-cx)**2)/40) % (2*np.pi) < np.pi)
-    arr[mask] = arr[mask] // 2
-    return Image.fromarray(arr)
+# Draw random geometric shapes
+draw = ImageDraw.Draw(img)
+for _ in range(30):
+    shape = np.random.choice(['ellipse', 'rectangle', 'line'])
+    xy = [np.random.randint(0, width), np.random.randint(0, height), np.random.randint(0, width), np.random.randint(0, height)]
+    color = tuple(np.random.randint(100, 255, 3))
+    if shape == 'ellipse':
+        draw.ellipse(xy, outline=color, width=2)
+    elif shape == 'rectangle':
+        draw.rectangle(xy, outline=color, width=2)
+    else:
+        draw.line(xy, fill=color, width=2)
 
-img = spiral_mask(img)
-img = ImageEnhance.Contrast(img).enhance(1.6)
+img = ImageEnhance.Color(img).enhance(2.0)
+img = ImageEnhance.Contrast(img).enhance(1.2)
 
 output_path = 'julia_output.jpg'
 img.save(output_path) 
