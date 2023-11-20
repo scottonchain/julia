3d297013d80618d4e057eae1a78df74d62669d56
Scott <scottimig@gmail.com>
2023-11-20 15:35:53 -0700

    update

--- julia_render.py
+++ julia_render.py
@@ -1,12 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.colors import hsv_to_rgb
+import matplotlib.cm as cm
 
 width, height = 1600, 1600
-x_range = (-1.41, 1.41)
-y_range = (-1.41, 1.41)
-c = complex(0.32, 0.33)
-max_iter = 360
+# Zoomed in region for detail
+x_range = (-1.51, 1.51)
+y_range = (-1.56, 1.56)
+c = complex(0.48, 0.11)
+max_iter = 1200
 
 x = np.linspace(x_range[0], x_range[1], width)
 y = np.linspace(y_range[0], y_range[1], height)
@@ -21,30 +22,23 @@ for i in range(max_iter):
     iteration[mask & ~mask_new] = i
     mask = mask_new
 
+# Smooth coloring for better detail
 with np.errstate(divide='ignore', invalid='ignore'):
     smooth = iteration + 1 - np.log(np.log2(np.abs(Z)))
     smooth = np.nan_to_num(smooth)
-smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
-hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (0.2 * smooth_norm + 0.5) % 1
-hsv[..., 1] = 0.95
-hsv[..., 2] = smooth_norm ** 0.6
-rgb = hsv_to_rgb(hsv)
-
+# Use bright, warm prismatic colormap
 fig, ax = plt.subplots(figsize=(8, 8), dpi=112)
-im = ax.imshow(rgb, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), origin='lower')
-ax.set_title('Julia Set (c = 0.355 + 0.355i)', fontsize=14)
+im = ax.imshow(smooth, extent=(x_range[0], x_range[1], y_range[0], y_range[1]), 
+               origin='lower', cmap='autumn', interpolation='bilinear')
+ax.set_title('Julia Set Detail (c = 0.45 + 0.1428i)', fontsize=14)
 ax.set_xlabel('Re(z)', fontsize=12)
 ax.set_ylabel('Im(z)', fontsize=12)
-ax.grid(True, color='white', alpha=0.2, linestyle='--', linewidth=0.5)
+ax.grid(True, color='white', alpha=0.3, linestyle='--', linewidth=0.5)
 
-from matplotlib.cm import ScalarMappable
-from matplotlib.colors import Normalize
-sm = ScalarMappable(cmap='hsv', norm=Normalize(vmin=smooth_norm.min(), vmax=smooth_norm.max()))
-sm.set_array([])
-cbar = plt.colorbar(sm, ax=ax, fraction=0.046, pad=0.04)
-cbar.set_label('Normalized Iteration (Smooth)', fontsize=12)
+# Add colorbar
+cbar = plt.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
+cbar.set_label('Iteration Count (Smooth)', fontsize=12)
 
 plt.tight_layout()
 plt.savefig('julia_output.jpg', dpi=112, bbox_inches='tight')
