260cfd9482eea9099b87c724b2782b82fcef6fb7
Scott <scottimig@gmail.com>
2023-11-29 20:01:00 -0700

    fixes

--- julia.py
+++ julia.py
@@ -1,21 +1,54 @@
 
 import numpy as np
-import matplotlib.pyplot as plt
+from pyopencl import clcreate, clprogram
 
-def compute_julia(width, height, c, max_iter):
-    x, y = np.meshgrid(np.linspace(-2, 2, width), np.linspace(-2, 2, height))
-    z = x + 1j * y
-    julia = np.zeros(z.shape, dtype=int)
-    
-    for i in range(max_iter):
-        mask = (np.abs(z) < 2) & ((x >= -2.01) | (y <= 2.01))  # boundary handling
-        z[mask] = z[mask]**2 + c
-        julia[mask] = i
-    
+def compute_julia_opencl(width, height, c, max_iter):
+    # Create a CL context and command queue
+    ctx = cl.create_context()
+    queue = ctx.get_command_queue()
+
+    # Define the kernel code for computing the Julia set
+    kernel_code = """
+        __kernel void julia_set(__global float *x, __global float *y,
+                                 __global int *julia, const int max_iter) {
+            int idx = get_global_id(0);
+            int idy = get_global_id(1);
+
+            // Compute the Julia set value for this pixel
+            double z_re = x[idx] + y[idy];
+            double z_im = 0;
+            int iter = 0;
+
+            while (iter < max_iter && np.abs(z_re) <= 2.0) {
+                z_re *= z_re - c.re;
+                z_im *= z_im - c.im;
+                iter++;
+            }
+
+            // Store the Julia set value in the output array
+            julia[idx * height + idy] = iter;
+        }
+    """
+
+    # Create a CL program and kernel from the kernel code
+    prg = clprogram.Program(ctx, kernel_code)
+    prg.build()
+
+    # Allocate memory for the input arrays (x, y) and output array (julia)
+    x = np.random.rand(width * height).astype(np.float32)
+    y = np.random.rand(height * width).astype(np.float32)
+    julia = np.zeros((width, height), dtype=np.int)
+
+    # Enqueue a kernel execution for the Julia set computation
+    prg.julia_set(queue, (width, height), None,
+                  x.astype(np.float32),
+                  y.astype(np.float32),
+                  julia.astype(np.int))
+
     return julia
 
-def plot_julia(julia, cmap='viridis'):
-    plt.imshow(julia, cmap=cmap)
+def plot_julia(julia):
+    plt.imshow(julia)
     plt.axis('off')
     plt.show()
 
@@ -23,7 +56,7 @@ if __name__ == '__main__':
     width, height = 500, 500
     c = complex(-0.8, 0.156)
     max_iter = 255
-    
-    julia = compute_julia(width, height, c, max_iter)
+
+    julia = compute_julia_opencl(width, height, c, max_iter)
     plot_julia(julia)
 
