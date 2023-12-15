038c3b9ff9d9efd5ba65e2703c326fe55295d524
Scott <scottimig@gmail.com>
2023-12-15 14:43:17 -0700

    sync files

--- julia_set_gen.py
+++ julia_set_gen.py
@@ -5,8 +5,8 @@ from matplotlib.colors import hsv_to_rgb
 
 # Artistic Julia set parameters
 width, height = 800, 800
-x_range = (-0.4 + 0.2j, -0.3 + 0.156j)  # Changed center of the julia set
-y_range = (-1.5, 1.5)
+x_range = (-0.5 + 0j, -0.3 + 0.156j)  # Changed center of the julia set
+y_range = (-1.8, 1.2)
 c = complex(-0.7, 0.15)  # Tweaked for different shapes
 max_iter = 300
 
@@ -35,19 +35,19 @@ smooth_norm = (smooth - smooth.min()) / (smooth.max() - smooth.min())
 
 # Build HSV image
 hsv = np.zeros((height, width, 3), dtype=float)
-hsv[..., 0] = (smooth_norm + 0.6) % 1  # Hue
-hsv[..., 1] = 0.8 + 0.2 * smooth_norm  # Saturation
-hsv[..., 2] = smooth_norm ** 0.3  # Value
+hsv[..., 0] = (smooth_norm + 0.4) % 1  # Hue
+hsv[..., 1] = 0.6 + 0.2 * smooth_norm  # Saturation
+hsv[..., 2] = smooth_norm ** 0.5  # Value
 
 # Convert to RGB
 rgb = (hsv_to_rgb(hsv) * 255).astype(np.uint8)
 img = Image.fromarray(rgb)
 
 # Artistic postprocessing: glow and enhancement
-blur = img.filter(ImageFilter.GaussianBlur(radius=7))  # Changed radius for blur effect
-glow = Image.blend(img, blur, alpha=0.5)  # Changed alpha value for glow effect
-enhanced = ImageEnhance.Contrast(glow).enhance(1.8)
-enhanced = ImageEnhance.Color(enhanced).enhance(1.2)
+blur = img.filter(ImageFilter.GaussianBlur(radius=10))  # Changed radius for blur effect
+glow = Image.blend(img, blur, alpha=0.7)  # Changed alpha value for glow effect
+enhanced = ImageEnhance.Contrast(glow).enhance(1.5)
+enhanced = ImageEnhance.Color(enhanced).enhance(1.3)
 
 # Display
 plt.figure(figsize=(6, 6))
