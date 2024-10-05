7cb68d4bc2fc0811096c869ded1efe7ffc84dabd
Scott <scottimig@gmail.com>
2024-10-04 20:00:00 -0600

    bug fix

--- julia_gen.py
+++ julia_gen.py
@@ -1,7 +1,7 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.ndimage import gaussian_filter
+from scipy.ndimage import gaussian_filter, filters
+import random
 
 # Define the Julia set parameters
 x_min, x_max = -1.5, 1.5
