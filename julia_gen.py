2f7741c17065a083c0fca30eb717de9b1b5af985
Scott <scottimig@gmail.com>
2024-11-13 20:00:00 -0700

    fix

--- julia_gen.py
+++ julia_gen.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.ndimage import gaussian_filter, filters
@@ -55,5 +54,6 @@ plt.figure(figsize=(6, 6))
 plt.axis('off')
 plt.imshow(enhanced.astype(np.uint8), cmap='gray', vmin=0, vmax=1)  # Convert to uint8 for display
 plt.show()
+
 import os; print("Current directory:", os.getcwd())
 
