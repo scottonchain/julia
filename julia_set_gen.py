608886496f9927d0742960e090324945ac37df71
Scott <scottimig@gmail.com>
2025-01-09 11:50:14 -0700

    update script

--- julia_set_gen.py
+++ julia_set_gen.py
@@ -1,11 +1,11 @@
 import numpy as np
-from matplotlib.pyplot import figure, axis, imshow
+from matplotlib import pyplot as plt
 
 # Artistic Julia set parameters
 image_size = 800
-x_range = (-0.75 + 1j * 0.25, -0.5 + 1j * 0.2)   # Change the center of the julia set
+x_range = (-0.75 + 1j * 0.25, -0.5 + 1j * 0.2)  # Change the center of the julia set
 y_range = (-1.8, 1.8)
-c = complex(-0.7, 0.3)   # Tweak this for different shapes
+c = complex(-0.7, 0.3)  # Tweak this for different shapes
 max_iterations = 300
 
 # Generate grid of complex points
@@ -30,15 +30,14 @@ smooth = (div_iterations - smooth.min()) / (smooth.max() - smooth.min())
 
 # Build HSV image
 hsv = np.zeros((image_size, image_size, 3), dtype=float)
-hsv[..., 0] = (smooth + 0.6) % 1   # Hue
-hsv[..., 1] = 0.8 + 0.2 * smooth   # Saturation
-hsv[..., 2] = smooth ** 0.5   # Value
+hsv[..., 0] = (smooth + 0.6) % 1  # Hue
+hsv[..., 1] = 0.8 + 0.2 * smooth  # Saturation
+hsv[..., 2] = smooth ** 0.5  # Value
 
 # Convert to RGB and display
 rgb = hsv_to_rgb(hsv) * 255
 img = rgb.astype(np.uint8)
-fig, ax = figure(figsize=(6, 6)), axis('off')
+fig, ax = plt.subplots(figsize=(6, 6))
 ax.imshow(img)
-
 plt.show()
 
