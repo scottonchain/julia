3a9cd579197c803064f90e3089b18e2b382c65ce
Scott <scottimig@gmail.com>
2025-05-02 18:12:44 -0600

    update

--- julia_set_gen.py
+++ julia_set_gen.py
@@ -1,47 +1,18 @@
 import numpy as np
 from PIL import Image, ImageFilter, ImageEnhance
-import matplotlib.pyplot as plt
-
-def hsv_to_rgb(hsv: np.ndarray) -> np.ndarray:
-    """Convert HSV to RGB"""
-    h = (hsv[..., 0] * 6).astype(int)
-    s = hsv[..., 1]
-    v = hsv[..., 2]
-
-    def hue_to_rgb(p, q, t):
-        if t < 0: t += 1
-        elif t > 1: t -= 1
-
-        if t < 1/6:
-            return v * (1 - s) + p * s
-        elif t < 1/2:
-            return v * (1 - s) + q * s
-        elif t < 2/3:
-            return v * (1 - s) + p * s
-        else:
-            return v
-
-    r = hue_to_rgb(v, h % 6 == 0, s)
-    g = hue_to_rgb(v, h % 6 == 1 or h % 6 == 5, s)
-    b = hue_to_rgb(0, h % 2 == 0, s)
-
-    return np.dstack((r, g, b)).astype(np.float32)
-
 
 def render_julia_set(width: int, height: int) -> Image:
     """Render the Julia set"""
+    
     x_range = (-1.5 + 0.3j, -0.8 + 0.156j)
     y_range = (-1.4, 1.6)
 
-    c = complex(0.2, 0.5)
-
-    # Adjust center of Julia set
-    # c = complex(-0.7, 0.2)  # Original value
+    c = complex(-0.7, 0.2)  # Adjust center of Julia set
 
     x = np.linspace(x_range[0].real, x_range[1].real, height)
     y = np.linspace(y_range[0], y_range[1], width)
     X, Y = np.meshgrid((x + 1j * (y - y_range[0])), y)
-    Z = X + 1j * Y
+    Z = c + (X - x_range[0].real) * 1j + (Y - y_range[0])
 
     div_iter = np.zeros(Z.shape, dtype=int)
     mask = np.ones(Z.shape, dtype=bool)
@@ -55,11 +26,11 @@ def render_julia_set(width: int, height: int) -> Image:
 
     return Image.fromarray((div_iter / max_iter * 255).astype(np.uint8))
 
-
 def main():
     img = render_julia_set(800, 600)
-    img.show()
-
+    enhancer = ImageEnhance.Color(img)
+    enhanced_img = enhancer.enhance(1.5)  # Adjust color scheme
+    enhanced_img.show()
 
 if __name__ == "__main__":
     main()
