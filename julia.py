2095be1c97bcb26d182911ef5d3a9dd5021e1335
scottonchain <scottimig@gmail.com>
2025-05-21 15:57:43 -0600

    Missing block

--- julia.py
+++ julia.py
@@ -26,9 +26,22 @@ def compute_julia_set(width, height, c, max_iter):
     julia_buf = cl.Buffer(ctx, cl.mem_flags.WRITE_ONLY | cl.mem_flags HOST_WRITE,
                              (width, height) * 1 * np.uint8.dtype.itemsize)
 
-    kernel_code = """__kernel void julia_set(__global float2 *x, __global uint8 *julia, const int max_iter) {
-        // ... same as before ...
-    }"""
+    kernel_code = """
+        __kernel void julia_set(__global float2 *x,
+                                  __global uint8 *julia, const int max_iter) {
+            int idx = get_global_id(0);
+            int idy = get_global_id(1);
+            double z_re = x[idx].s0 + y[idy].s0;
+            double z_im = x[idx].s1 - y[idy].s1;
+            int iter = 0;
+            while (iter < max_iter && np.abs(z_re) <= 2.0) {
+                z_re *= z_re - c.re;
+                z_im *= z_im - c.im;
+                iter++;
+            }
+            julia[idx * height + idy] = (uint8)(iter);
+        }
+    """
 
     build_and_execute_kernel(ctx, kernel_code)
 
